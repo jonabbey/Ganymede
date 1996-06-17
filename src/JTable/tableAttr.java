ced9cfc77080cd40f1f731da4238a4e6afec95f0
Jonathan Abbey <jonabbey@arlut.utexas.edu>
1996-06-17 20:41:43 +0000

    it compiles now -- Jon

--- src/JTable/tableAttr.java
+++ src/JTable/tableAttr.java
@@ -5,13 +5,13 @@
    A GUI component
 
    Created: 4 June 1996
-   Version: $Revision: 1.1 $ %D%
+   Version: $Revision: 1.2 $ %D%
    Module By: Jonathan Abbey
    Applied Research Laboratories, The University of Texas at Austin
 
 */
 
-package csd;
+package csd.Table;
 
 import java.awt.*;
 import java.util.*;
