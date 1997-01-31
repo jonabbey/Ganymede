318ff5224357ccf28d044060f97a2049e69c99f2
Jonathan Abbey <jonabbey@arlut.utexas.edu>
1997-01-31 16:03:56 +0000

    Fixed up.  Moved the table inserts into the init() method instead of the start() method.  testTable now puts the rowTable into a randomly sized border container to show off the scroll bar handling. -- Jon

--- src/JTable/testTable.java
+++ src/JTable/testTable.java
@@ -4,13 +4,14 @@
   A test framework for the baseTable GUI component
 
   Created: 5 June 1996
-  Version: 1.1 96/06/17
+  Version: $Revision: 1.3 $ %D%
   Module By: Jonathan Abbey -- jonabbey@arlut.utexas.edu
   Applied Research Laboratories, The University of Texas at Austin
 
 */
 
 import java.awt.*;
+import java.awt.event.*;
 import java.applet.*;
 import java.util.*;
 import csd.Table.*;
@@ -21,18 +22,24 @@ import csd.Table.*;
 
 ------------------------------------------------------------------------------*/
 
-public class testTable extends Applet {
+public class testTable extends Applet implements rowSelectCallback, ActionListener {
 
   static final boolean debug = false;
   static testTable applet = null;
   static Frame frame = null;
 
+  static final int Xsize = 600;
+  static final int Ysize = 300;
+
   /* - */
 
-  csd.Table.gridTable table;
+  csd.Table.rowTable table;
 
-  Panel p1 = null;
+  testBorder tBorder = null;
   TextField statusField = null;
+  Panel southPanel = null;
+  TextField labelField = null;
+  Button resize = null;
 
   String headers[] = {"User", "Status", "Random 1", "Random 2"};
   int colWidths[] = {100,100,100,100};
@@ -41,11 +48,62 @@ public class testTable extends Applet {
   
   public void init() 
   {
+    if (debug)
+      {
+	System.err.println("testTable.init()");
+      }
+
+    table.newRow("jonabbey");
+    table.setCellText("jonabbey", 0, "jonabbey",false);
+    table.setCellText("jonabbey", 1, "okay",false);
+    table.setCellText("jonabbey", 2, "3.14159",false);
+    table.setCellText("jonabbey", 3, "Pineapples in summer",true);
+    table.newRow("root");
+    table.setCellText("root", 0, "root",false);
+    table.setCellText("root", 1, "anonymous",false);
+    table.setCellText("root", 2, "csdsun1.arlut.utexas.edu",false);
+    table.setCellText("root", 3, "Alaska",true);
+    table.newRow("navin");
+    table.setCellText("navin", 0, "navin",false);
+    table.setCellText("navin", 1, "student",false);
+    table.setCellText("navin", 2, "Java",false);
+    table.setCellText("navin", 3, "Computer Science Dept.",true);
+    table.newRow("imkris");
+    table.setCellText("imkris", 0, "imkris",false);
+    table.setCellText("imkris", 1, "full time",false);
+    table.setCellText("imkris", 2, "Accounting",false);
+    table.setCellText("imkris" ,3, "Texas A&M",true);
+    table.newRow("jonabbey2");
+    table.setCellText("jonabbey2", 0, "jonabbey2",false);
+    table.setCellText("jonabbey2", 1, "okay",false);
+    table.setCellText("jonabbey2", 2, "3.14159",false);
+    table.setCellText("jonabbey2", 3, "Pineapples in summer",true);
+    table.newRow("root2");
+    table.setCellText("root2", 0, "root2",false);
+    table.setCellText("root2", 1, "anonymous",false);
+    table.setCellText("root2", 2, "csdsun1.arlut.utexas.edu",false);
+    table.setCellText("root2", 3, "Alaska",true);
+    table.newRow("navin2");
+    table.setCellText("navin2", 0, "navin2",false);
+    table.setCellText("navin2", 1, "student",false);
+    table.setCellText("navin2", 2, "Java",false);
+    table.setCellText("navin2", 3, "Computer Science Dept.",true);
+    table.newRow("imkris2");
+    table.setCellText("imkris2", 0, "imkris2",false);
+    table.setCellText("imkris2", 1, "full time",false);
+    table.setCellText("imkris2", 2, "Accounting",false);
+    table.setCellText("imkris2" ,3, "Texas A&M",true);
+
+
+    if (debug)
+      {
+	System.err.println("exiting testTable.init()");
+      }
   }
 
   public void resize() 
   {
-    resize(600,300);
+    resize(Xsize, Ysize);
   }
 
   // Our primary constructor.  This will always be called, either
@@ -55,7 +113,7 @@ public class testTable extends Applet {
   {
     setLayout(new BorderLayout());
 
-    statusField = new TextField("baseTable Testing", 40);
+    statusField = new TextField("rowTable Testing", 40);
     statusField.setEditable(false);
     statusField.setBackground(Color.red);
     statusField.setForeground(Color.white);
@@ -66,65 +124,71 @@ public class testTable extends Applet {
 	System.err.println("testTable constructor: constructing gridTable");
       }
 
-    table = new csd.Table.gridTable(colWidths, headers);
+    table = new csd.Table.rowTable(colWidths, headers, this);
 
     if (debug)
       {
 	System.err.println("testTable constructor: constructed gridTable");
       }
 
-    add("Center", table);
+    tBorder = new testBorder(Xsize, Ysize, table);
+
+    add("Center", tBorder);
 
     if (debug)
       {
 	System.err.println("testTable constructor: table added to applet");
       }
+    
+    southPanel = new Panel();
+    southPanel.setLayout(new BorderLayout());
+    
+    labelField = new TextField("boop", 30);
+    labelField.setEditable(false);
+    labelField.setBackground(SystemColor.text);
+    labelField.setForeground(SystemColor.textText);
+
+    resize = new Button("resize");
+    resize.addActionListener(this);
+
+    southPanel.add("West", labelField);
+    southPanel.add("East", resize);
+
+    add("South", southPanel);
+    
   }
 
   public void start()
   {
-    if (debug)
-      {
-	System.err.println("testTable.start()");
-      }
 
-    table.setCellText(0,0, "jonabbey",false);
-    table.setCellText(1,0, "okay",false);
-    table.setCellText(2,0, "3.14159",false);
-    table.setCellText(3,0, "Pineapples in summer",true);
-    table.setCellText(0,1, "root",false);
-    table.setCellText(1,1, "anonymous",false);
-    table.setCellText(2,1, "csdsun1.arlut.utexas.edu",false);
-    table.setCellText(3,1, "Alaska",true);
-    table.setCellText(0,2, "navin",false);
-    table.setCellText(1,2, "student",false);
-    table.setCellText(2,2, "Java",false);
-    table.setCellText(3,2, "Computer Science Dept.",true);
-    table.setCellText(0,3, "imkris",false);
-    table.setCellText(1,3, "full time",false);
-    table.setCellText(2,3, "Accounting",false);
-    table.setCellText(3,3, "Texas A&M",true);
-    table.setCellText(0,4, "jonabbey",false);
-    table.setCellText(1,4, "okay",false);
-    table.setCellText(2,4, "3.14159",false);
-    table.setCellText(3,4, "Pineapples in summer",true);
-    table.setCellText(0,5, "root",false);
-    table.setCellText(1,5, "anonymous",false);
-    table.setCellText(2,5, "csdsun1.arlut.utexas.edu",false);
-    table.setCellText(3,5, "Alaska",true);
-    table.setCellText(0,6, "navin",false);
-    table.setCellText(1,6, "student",false);
-    table.setCellText(2,6, "Java",false);
-    table.setCellText(3,6, "Computer Science Dept.",true);
-    table.setCellText(0,7, "imkris",false);
-    table.setCellText(1,7, "full time",false);
-    table.setCellText(2,7, "Accounting",false);
-    table.setCellText(3,7, "Texas A&M",true);
+  }
 
-    if (debug)
-      {
-	System.err.println("exiting testTable.start()");
-      }
+  // ActionListener methods
+
+  public void actionPerformed(java.awt.event.ActionEvent event)
+  {
+    // we want to resize the table
+
+    tBorder.randomize();
+    this.invalidate();
+    this.validate();
+  }
+
+  // rowSelectCallback methods
+
+  public void rowSelected(Object key)
+  {
+    labelField.setText(key + " selected");
+  }
+
+  public void rowDoubleSelected(Object key)
+  {
+    labelField.setText(key + " double selected");
+  }
+
+  public void rowUnSelected(Object key, boolean endSelected)
+  {
+    labelField.setText(key + " unselected");
   }
 
   public static void main(String[] argv)
