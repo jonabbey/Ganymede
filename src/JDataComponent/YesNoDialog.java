ec4b8c0568956059adf73581d817f08f5831d79d
Jonathan Abbey <jonabbey@arlut.utexas.edu>
1997-02-06 20:39:17 +0000

    Initial revision

---
+++ src/JDataComponent/YesNoDialog.java
@@ -0,0 +1,89 @@
+/*
+
+   YesNoDialog.java
+
+   A 1.1 compatible YesNoDialog box
+   
+   Created: 6 February 1997
+   Version: $Revision: 1.1 $ %D%
+   Module By: Jonathan Abbey
+   Applied Research Laboratories, The University of Texas at Austin
+
+*/
+
+package csd.Dialog;
+
+import java.awt.*;
+import java.awt.event.*;
+
+import gjt.ButtonPanel;
+
+public class YesNoDialog extends Dialog implements ActionListener {
+
+  Button yesButton;
+  Button noButton;
+  ButtonPanel buttonPanel;
+  ActionListener listener;
+
+  boolean answer = false;
+
+  public YesNoDialog(Frame frame, String title, String message, ActionListener listener)
+  {
+    super(frame, title, true);
+
+    this.listener = listener;
+
+    buttonPanel = new ButtonPanel();
+    yesButton = buttonPanel.add("Yes");
+    noButton = buttonPanel.add("No");
+    
+    yesButton.addActionListener(this);
+    noButton.addActionListener(this);
+
+    setLayout(new BorderLayout());
+    add("Center", new MessagePanel(message));
+    add("South", buttonPanel);
+    pack();
+  }
+
+  public void show()
+  {
+    answer = false;
+    yesButton.requestFocus();
+    super.show();
+  }
+  
+  public boolean answeredYes()
+  {
+    return answer;
+  }
+
+  public void actionPerformed(ActionEvent e)
+  {
+    if (e.getSource() == yesButton)
+      {
+	answer = true;
+      }
+    else
+      {
+	answer = false;
+      }
+
+    hide();
+    listener.actionPerformed(new ActionEvent(this, ActionEvent.ACTION_PERFORMED, ""));
+  }
+
+}
+
+class MessagePanel extends Panel {
+
+  public MessagePanel(String message)
+  {
+    add("Center", new Label(message, Label.CENTER));
+  }
+
+  public Insets insets()
+  {
+    return new Insets(10,10,10,10);
+  }
+}
