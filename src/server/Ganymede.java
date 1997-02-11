4c06d8a1b60c97d42795949ec434072c201db086
Jonathan Abbey <jonabbey@arlut.utexas.edu>
1997-02-11 20:20:11 +0000

    Created debug() method to centralize all debug output. -- Jon

--- src/server/Ganymede.java
+++ src/server/Ganymede.java
@@ -5,7 +5,7 @@
    Server main module
 
    Created: 17 January 1997
-   Version: $Revision: 1.1 $ %D%
+   Version: $Revision: 1.2 $ %D%
    Module By: Jonathan Abbey
    Applied Research Laboratories, The University of Texas at Austin
 
@@ -16,39 +16,89 @@ package gash;
 import java.rmi.*;
 import java.rmi.registry.*;
 import java.rmi.server.*;
+import java.io.*;
+import csd.DBStore.*;
 
 public class Ganymede {
   
-  public static RegistryImpl registry;
-  
+  public static GanymedeServer server;
+  public static DBStore db;
+  public static GanymedeSchema schema;
+  public static String dbFilename;
+  public static String schemaFilename;
+  public static final boolean debug = true;
+
+  /* -- */
+
   public static void main(String argv[]) 
   {
+    if (argv.length != 2)
+      {
+	System.out.println("Error: invalid command line parameters");
+	System.out.println("Usage: java Ganymede <dbfile> <schemafile>");
+	return;
+      }
+    else
+      {
+	dbFilename = argv[0];
+	schemaFilename = argv[1];
+      }
 
-    System.setSecurityManager(new StubSecurityManager());
+    debug("Creating DBStore structures");
+
+    db = new DBStore();
+
+    debug("Loading DBStore contents");
+    
+    db.load(dbFilename);
+
+    debug("Creating Ganymede Schema");
 
     try
       {
-	registry = new RegistryImpl(7211);
+	schema = new GanymedeSchema(schemaFilename);
       }
-    catch (RemoteException ex)
+    catch (IOException ex)
       {
-	System.err.println("Couldn't establish registry: " + ex);
-	return;
+	debug("Ganymede Schema creation failed.");
       }
+
+    debug("Initializing Security Manager");
+
+    System.setSecurityManager(new RMISecurityManager());
 
     // Create a Server object
 
     try
       {
-	GanymedeServer server = new GanymedeServer(10);
-	registry.bind("ganymede.server", server);
+	debug("Creating GanymedeServer object");
+
+	server = new GanymedeServer(10);
+
+	debug("Binding GanymedeServer in RMI Registry");
+
+	Naming.rebind("ganymede.server", server);
       }
     catch (Exception ex)
       {
-	System.err.println("Couldn't establish server binding: " + ex);
+	debug("Couldn't establish server binding: " + ex);
 	return;
       }
 
-    System.err.println("Setup and bound server object OK");
+    if (debug)
+      {
+	debug("Setup and bound server object OK");
+      }
+  }
+
+  // debug routine
+
+  static public void debug(String string)
+  {
+    if (debug)
+      {
+	System.err.println(string);
+      }
+    GanymedeAdmin.setStatus(string);
   }
 }
