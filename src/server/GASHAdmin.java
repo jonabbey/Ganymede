344aea2abd4932f36ce6922595dc13fc6ace16a6
Jonathan Abbey <jonabbey@arlut.utexas.edu>
1997-02-11 20:23:57 +0000

    Lots of changes, now have a textarea for status logging. -- Jon

--- src/server/GASHAdmin.java
+++ src/server/GASHAdmin.java
@@ -5,7 +5,7 @@
    Admin console for the Java RMI Gash Server
 
    Created: 28 May 1996
-   Version: $Revision: 1.3 $ %D%
+   Version: $Revision: 1.4 $ %D%
    Module By: Jonathan Abbey
    Applied Research Laboratories, The University of Texas at Austin
 
@@ -16,10 +16,14 @@ package gash;
 import java.rmi.*;
 import java.rmi.server.*;
 import java.awt.*;
+import java.awt.event.*;
 import java.applet.*;
 import java.util.*;
-import ReportTable;
-import ReportTableAttr;
+
+import gjt.Box;
+
+import csd.Table.*;
+import csd.Dialog.YesNoDialog;
 
 /*------------------------------------------------------------------------------
                                                                            class
@@ -31,19 +35,19 @@ happens.
 
 ------------------------------------------------------------------------------*/
 
-class iAdmin extends UnicastRemoteServer implements Admin {
+class iAdmin extends UnicastRemoteObject implements Admin {
 
-  private GASHAdmin applet = null;
+  private GASHAdminFrame frame = null;
   private Server server = null;
   private adminSession aSession = null;
 
   /* -- */
 
-  public iAdmin(GASHAdmin applet, Server server) throws RemoteException
+  public iAdmin(GASHAdminFrame frame, Server server) throws RemoteException
   {
     // UnicastRemoteServer can throw RemoteException 
 
-    this.applet = applet;
+    this.frame = frame;
     this.server = server;
 
     try
@@ -68,63 +72,217 @@ class iAdmin extends UnicastRemoteServer implements Admin {
   {
     return "Dilbert";
   }
-  
+
+  public void changeStatus(String status)
+  {
+    frame.statusArea.append(new Date() + " " + status + "\n");
+    frame.statusArea.setCaretPosition(frame.statusArea.getText().length());
+  }
+
+  public void changeUsers(Vector entries)
+  {
+    AdminEntry e;
+
+    /* -- */
+
+    frame.table.clearCells();
+    
+    // Process entries from the server
+
+    for (int i = 0; i < entries.size(); i++)
+      {
+	e = (AdminEntry) entries.elementAt(i);
+
+	frame.table.newRow(e.username);
+	frame.table.setCellText(e.username, 0, e.username, false);
+	frame.table.setCellText(e.username, 1, e.hostname, false);
+	frame.table.setCellText(e.username, 2, e.status, false);
+	frame.table.setCellText(e.username, 3, e.connecttime, false);
+	frame.table.setCellText(e.username, 4, e.event, false);
+      }
+
+    // And refresh our table
+
+    frame.table.refreshTable();
+  }
+
   public void disconnect() throws RemoteException
   {
     aSession.logout();
   }
 
-  public void changeStatus(String status)
+  /**
+   *
+   * Callback: The server can tell us to disconnect if the server is 
+   * going down.
+   *
+   */
+
+  public void forceDisconnect(String reason)
   {
-    applet.statusField.setText(status);
+    changeStatus("Disconnected: " + reason + "\n");
+    server = null;
   }
 
-  public void changeUsers(Vector users, Vector status)
+  // ------------------------------------------------------------
+  // convenience methods for our GASHAdminFrame
+  // ------------------------------------------------------------
+
+  void kill(String username) throws RemoteException
   {
-    applet.table.clearCells();
+    aSession.kill(username);
+  }
 
-    for (int i = 0; i < users.size(); i++)
-      {
-	applet.table.setCellText(0, i, (String)users.elementAt(i), false);
-	applet.table.setCellText(1, i, (String)status.elementAt(i), false);
-      }
+  void shutdown() throws RemoteException
+  {
+    aSession.shutdown();
+  }
 
-    applet.table.refreshTable();
+  void dumpDB() throws RemoteException
+  {
+    aSession.dumpDB();
   }
 
 }
 
 /*------------------------------------------------------------------------------
                                                                            class
-                                                                       GASHAdmin
+                                                                  GASHAdminFrame
 
 ------------------------------------------------------------------------------*/
-public class GASHAdmin extends Applet {
 
-  static GASHAdmin applet = null;
+class GASHAdminFrame extends Frame implements ActionListener, rowSelectCallback {
+
   static iAdmin admin = null;
-  static Frame frame = null;
+  static boolean WeAreApplet = true;
 
-  Panel p1 = null;
-  Button quitButton = null;
-  TextField statusField = null;
-  ReportTable table = null;
+  /* - */
 
-  String headers[] = {"User", "Status"};
-  int colWidths[] = {100,100};
+  Server server = null;
 
-  /* -- */
+  MenuBar mbar = null;
+  Menu controlMenu = null;
+  MenuItem quitMI = null;
+  MenuItem dumpMI = null;
+  MenuItem shutdownMI = null;
+
+  PopupMenu popMenu = null;
+  MenuItem killUserMI = null;
+
+  Panel topPanel = null;
+
+  YesNoDialog shutdownDialog = null;
+  YesNoDialog dumpDialog = null;
+  YesNoDialog killDialog = null;
+
+  String killVictim = null;
+
+  Label hostLabel = null;
+  TextField hostField = null;
+  TextArea statusArea = null;
   
-  public void init() 
-  {
-    String url = "rmi://csdsun1.arlut.utexas.edu:7211/gash.server";
-    Server server = null;
+  rowTable table = null;
 
-    /* -- */
+  String url = "rmi://www.arlut.utexas.edu/ganymede.server";
+  String headers[] = {"User", "System", "Status", "Connect Time", "Last Event"};
+  int colWidths[] = {100,100,100,100,100};
+  
+  /* -- */
+
+  public GASHAdminFrame(String title, boolean WeAreApplet)
+  {
+    super(title);
+
+    this.WeAreApplet = WeAreApplet;
+
+    mbar = new MenuBar();
+    controlMenu = new Menu("Control", false);
+
+    dumpMI = new MenuItem("Dump Database");
+    dumpMI.addActionListener(this);
+
+    shutdownMI = new MenuItem("Shutdown Ganymede");
+    shutdownMI.addActionListener(this);
+
+    quitMI = new MenuItem("Close Console");
+    quitMI.addActionListener(this);
+
+    controlMenu.add(dumpMI);
+    controlMenu.add(shutdownMI);
+    controlMenu.addSeparator();
+    controlMenu.add(quitMI);
+
+    mbar.add(controlMenu);
+
+    setMenuBar(mbar);
+
+    popMenu = new PopupMenu();
+
+    killUserMI = new MenuItem("Kill User");
+    popMenu.add(killUserMI);
+
+    shutdownDialog = new YesNoDialog(this,
+				     "Confirm Ganymede Server Shutdown", 
+				     "Are you sure you want to shutdown the Ganymede server?", 
+				     this);
+
+    dumpDialog = new YesNoDialog(this,
+				 "Ganymede Server Dump",
+				 "Are you sure you want to schedule a full dump of the Ganymede database?", 
+				 this);
+
+    //    setBackground(Color.white);
+
+    setLayout(new BorderLayout());
+
+    table = new rowTable(colWidths, headers, this, popMenu);
+    add("South", new Box(table, "Users Connected"));
+
+    hostLabel = new Label("Ganymede Server Host:");
+
+    hostField = new TextField(url, 40);
+    hostField.setEditable(false);
+    hostField.setBackground(SystemColor.text);
+    hostField.setForeground(SystemColor.textText);
+
+    topPanel = new Panel();
+
+    GridBagLayout gbl = new GridBagLayout();
+    GridBagConstraints gbc = new GridBagConstraints();
+
+    topPanel.setLayout(gbl);
+
+    gbc.anchor = GridBagConstraints.EAST;
+    gbc.weightx = 0;
+    gbc.fill = GridBagConstraints.NONE;
+    gbc.gridwidth = 1;
+    gbc.gridheight = 1;
+    gbl.setConstraints(hostLabel, gbc);
+    topPanel.add(hostLabel);
+
+    gbc.anchor = GridBagConstraints.WEST;
+    gbc.weightx = 100;
+    gbc.fill = GridBagConstraints.HORIZONTAL;
+    gbc.gridwidth = GridBagConstraints.REMAINDER;
+    gbc.gridheight = 1;
+    gbl.setConstraints(hostField, gbc);
+    topPanel.add(hostField);
+
+    add("North", new Box(topPanel, "Server"));
+
+    statusArea = new TextArea("Admin Console Testing\n", 6, 50);
+    statusArea.setEditable(false);
+    statusArea.setBackground(SystemColor.text);
+    statusArea.setForeground(SystemColor.textText);
+
+    add("Center", new Box(statusArea, "Server Log"));
+
+    pack();
+    show();
 
     /* RMI initialization stuff. We do this for our iClient object. */
 
-    System.setSecurityManager(new StubSecurityManager());
+    System.setSecurityManager(new RMISecurityManager());
 
     /* Get a reference to the server */
 
@@ -141,7 +299,7 @@ public class GASHAdmin extends Applet {
       {
 	System.err.println("RMI: Couldn't bind to server object\n" + ex );
       }
-    catch (java.net.UnknownHostException ex)
+    catch (java.rmi.UnknownHostException ex)
       {
 	System.err.println("RMI: Couldn't find server\n" + url );
       }
@@ -176,76 +334,154 @@ public class GASHAdmin extends Applet {
       }
   }
 
+  // our button / dialog handler
 
-  public void resize() 
+  public void actionPerformed(ActionEvent event)
   {
-    resize(600,300);
-  }
-
-  public boolean action(Event event, Object obj)
-  {
-    if (event.target instanceof Button)
+    if (event.getSource() == quitMI)
       {
-	if (event.target == quitButton)
+	System.err.println("Quitting");
+	try
 	  {
-	    System.err.println("Quitting");
-	    try
-	      {
-		admin.disconnect();
-	      }
-	    catch (RemoteException ex)
-	      {
-		System.err.println("Couldn't logout cleanly: " + ex);
-	      }
-	    finally
-	      {
-		System.exit(0);
-	      }
-
-	    return true;	// we handled the event
+	    admin.disconnect();
+	  }
+	catch (RemoteException ex)
+	  {
+	    System.err.println("Couldn't logout cleanly: " + ex);
+	  }
+	finally
+	  {
+	    System.exit(0);
 	  }
       }
-    return false;
+    else if (event.getSource() == dumpMI)
+      {
+	dumpDialog.show();
+      }
+    else if (event.getSource() == shutdownMI)
+      {
+	shutdownDialog.show();
+      }
+    else if (event.getSource() == shutdownDialog)
+      {
+	if (shutdownDialog.answeredYes())
+	  {
+	    System.err.println("Affirmative shutdown request");
+	    try
+	      {
+		admin.shutdown();
+	      }
+	    catch (RemoteException e)
+	      {
+		admin.forceDisconnect("Couldn't talk to server");
+	      }
+	  }
+      }
+    else if (event.getSource() == dumpDialog)
+      {
+	if (dumpDialog.answeredYes())
+	  {
+	    System.err.println("Affirmative dump request");
+	    try
+	      {
+		admin.dumpDB();
+	      }
+	    catch (RemoteException e)
+	      {
+		admin.forceDisconnect("Couldn't talk to server");
+	      }
+	  }
+      }
+    else if (event.getSource() == killDialog)
+      {
+	if (killDialog.answeredYes())
+	  {
+	    System.err.println("Affirmative kill request");
+	    if (killVictim != null)
+	      {
+		try
+		  {
+		    admin.kill(killVictim);
+		  }
+		catch (RemoteException e)
+		  {
+		    admin.forceDisconnect("Couldn't talk to server");
+		  }
+	      }
+	    killVictim = null;
+	  }
+	else
+	  {
+	    System.err.println("Negative dump request");
+	    killVictim = null;
+	  }
+      }
   }
+
+  // methods for the rowSelectCallback
+
+  public void rowSelected(Object key)
+  {
+  }
+
+  public void rowDoubleSelected(Object key)
+  {
+  }
+
+  public void rowUnSelected(Object key, boolean finalState)
+  {
+  }
+
+  public void rowMenuPerformed(Object key, ActionEvent e)
+  {
+
+    System.err.println("rowMenuPerformed");
+
+    if (e.getSource() == killUserMI)
+      {
+	System.err.println("kill " + key + " selected");
+      }
+
+    killVictim = (String) key;
+
+    killDialog = new YesNoDialog(this,
+				 "Confirm User Kill",
+				 "Are you sure you want to disconnect user " + key + "?",
+				 this);
+
+    killDialog.show();
+  }
+
+}
+
+/*------------------------------------------------------------------------------
+                                                                          class
+                                                                       GASHAdmin
+
+------------------------------------------------------------------------------*/
+public class GASHAdmin extends Applet {
+
+  static GASHAdmin applet = null;
+  static GASHAdminFrame frame = null;
+
+  /* -- */
 
   // Our primary constructor.  This will always be called, either
   // from main(), below, or by the environment building our applet.
 
   public GASHAdmin() 
   {
-    setLayout(new BorderLayout());
 
-    table = new ReportTable(colWidths, headers);
-    add("Center", table);
-
-    statusField = new TextField("Admin Console Testing", 40);
-    statusField.setEditable(false);
-    statusField.setBackground(Color.red);
-    statusField.setForeground(Color.white);
-    add("North", statusField);
-
-    p1 = new Panel();
-    p1.setLayout(new FlowLayout());
-    add("South", p1);
-
-    quitButton = new Button("Quit");
-    p1.add(quitButton);
+  }
+  
+  public void init() 
+  {
+    frame = new GASHAdminFrame("Ganymede Admin Console", true);
   }
 
   public static void main(String[] argv)
   {
-    /* Define the applet */
-
-    Frame frame = new Frame("GASH2 Admin Console");
-    applet = new GASHAdmin();
-
-    /* present the applet */
-
-    frame.add("Center", applet);
-    frame.resize(300, 300);
-    frame.show();  
-    applet.init();
-    applet.start();
+    frame = new GASHAdminFrame("Ganymede Admin Console", false);
   }
 }
  
