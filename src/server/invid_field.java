12c85c11c39487efae3d8cee71fc42b9232080af
Jonathan Abbey <jonabbey@arlut.utexas.edu>
1997-04-02 13:10:04 +0000

    Added limited(), getAllowedTarget() method definitions. -- Jon

--- src/server/invid_field.java
+++ src/server/invid_field.java
@@ -5,7 +5,7 @@
    Remote interface definition.
 
    Created: 14 November 1996
-   Version: $Revision: 1.1 $ %D%
+   Version: $Revision: 1.2 $ %D%
    Module By: Jonathan Abbey
    Applied Research Laboratories, The University of Texas at Austin
 
@@ -16,5 +16,7 @@ import java.util.*;
 import java.rmi.RemoteException;
 
 public interface invid_field extends db_field {
+  boolean limited() throws RemoteException;
+  int getAllowedTarget() throws RemoteException;
   Vector choices() throws RemoteException;
 }
