3728eda293eadf63bc57b229652c15d30cd9dbcf
Jonathan Abbey <jonabbey@arlut.utexas.edu>
1997-04-11 16:25:29 +0000

    Reworked to take advantage of the beefed up DBField base class. -- Jon

--- src/server/BooleanDBField.java
+++ src/server/BooleanDBField.java
@@ -6,7 +6,7 @@
    The GANYMEDE object storage system.
 
    Created: 2 July 1996
-   Version: $Revision: 1.4 $ %D%
+   Version: $Revision: 1.5 $ %D%
    Module By: Jonathan Abbey
    Applied Research Laboratories, The University of Texas at Austin
 
@@ -23,42 +23,273 @@ import java.util.*;
 
 ------------------------------------------------------------------------------*/
 
-public class BooleanDBField extends DBField {
+public class BooleanDBField extends DBField implements boolean_field {
+  
+  /**
+   *
+   * Receive constructor.  Used to create a BooleanDBField from a DBStore/DBJournal
+   * DataInput stream.
+   *
+   */
 
-  boolean value;
-
-  /* -- */
-
-  BooleanDBField(DataInput in, DBObjectBaseField definition) throws IOException
+  BooleanDBField(DBObject owner, DataInput in, DBObjectBaseField definition) throws IOException
   {
+    defined = true;
+    value = null;
+    values = null;
+    this.owner = owner;
     this.definition = definition;
     receive(in);
   }
 
-  public BooleanDBField(boolean value)
+  /**
+   *
+   * No-value constructor.  Allows the construction of a
+   * 'non-initialized' field, for use where the DBObjectBase
+   * definition indicates that a given field may be present,
+   * but for which no value has been stored in the DBStore.
+   *
+   * Used to provide the client a template for 'creating' this
+   * field if so desired.
+   *
+   */
+
+  BooleanDBField(DBObject owner, DBObjectBaseField definition)
   {
-    this.definition = null;
-    this.value = value;
+    this.owner = owner;
+    this.definition = definition;
+    
+    defined = false;
+    value = null;
+    values = null;
+  }
+
+  /**
+   *
+   * Copy constructor.
+   *
+   */
+
+  public BooleanDBField(DBObject owner, BooleanDBField field)
+  {
+    this.owner = owner;
+    definition = field.definition;
+    
+    if (isVector())
+      {
+	values = (Vector) field.values.clone();
+	value = null;
+      }
+    else
+      {
+	value = field.value;
+	values = null;
+      }
+
+    defined = true;
+  }
+
+
+  /**
+   *
+   * Scalar value constructor.
+   *
+   */
+
+  public BooleanDBField(DBObject owner, boolean value, DBObjectBaseField definition)
+  {
+    if (definition.isArray())
+      {
+	throw new IllegalArgumentException("scalar constructor called on vector field");
+      }
+
+    this.owner = owner;
+    this.definition = definition;
+    this.value = new Boolean(value);
+
+    values = null;
+    defined = true;
+  }
+
+  /**
+   *
+   * Vector value constructor.
+   *
+   */
+
+  public BooleanDBField(DBObject owner, Vector values, DBObjectBaseField definition)
+  {
+    if (!definition.isArray())
+      {
+	throw new IllegalArgumentException("vector constructor called on scalar field");
+      }
+
+    this.owner = owner;
+    this.definition = definition;
+    
+    if (values == null)
+      {
+	this.values = new Vector();
+	defined = false;
+      }
+    else
+      {
+	this.values = (Vector) values.clone();
+	defined = true;
+      }
+
+    value = false;
+    defined = true;
+  }
+
+  protected Object clone()
+  {
+    return new BooleanDBField(owner, this);
   }
 
   void emit(DataOutput out) throws IOException
   {
-    out.writeBoolean(value);
+    if (isVector())
+      {
+	out.writeShort(values.size());
+	for (int i = 0; i < values.size(); i++)
+	  {
+	    out.writeBoolean(value(i));
+	  }
+      }
+    else
+      {
+	out.writeBoolean(value());
+      }
   }
 
   void receive(DataInput in) throws IOException
   {
-    value = in.readBoolean();
+    int count;
+
+    /* -- */
+
+    if (isVector())
+      {
+	count = in.readShort();
+	values = new Vector(count);
+	for (int i = 0; i < count; i++)
+	  {
+	    values.addElement(new Boolean(in.readBoolean()));
+	  }
+      }
+    else
+      {
+	value = new Boolean(in.readBoolean());
+      }
   }
+
+  // ****
+  //
+  // type-specific accessor methods
+  //
+  // ****
 
   public boolean value()
   {
-    return value;
+    if (isVector())
+      {
+	throw new IllegalArgumentException("scalar accessor called on vector");
+      }
+
+    return ((Boolean) value).booleanValue();
   }
 
-  public Object key()
+  public boolean value(int index)
   {
-    return new Boolean(value);
+    if (!isVector())
+      {
+	throw new IllegalArgumentException("vector accessor called on scalar");
+      }
+
+    return ((Boolean) values.elementAt(index)).booleanValue();
   }
+
+
+  // ****
+  //
+  // boolean_field methods
+  //
+  // ****
+  
+  /**
+   *
+   * Returns true if this field is defined to have the true/false
+   * values associated with labels.
+   *
+   * @see arlut.csd.ganymede.boolean_field
+   */
+
+  public boolean labeled()
+  {
+    return definition.isLabeled();
+  }
+
+  /**
+   *
+   * Returns the true label if this field is defined to have the true/false
+   * values associated with labels.
+   *
+   * @see arlut.csd.ganymede.boolean_field
+   */
+
+  public String trueLabel()
+  {
+    return definition.getTrueLabel();
+  }
+
+  /**
+   *
+   * Returns the false label if this field is defined to have the true/false
+   * values associated with labels.
+   *
+   * @see arlut.csd.ganymede.boolean_field
+   */
+
+  public String falseLabel()
+  {
+    return definition.getFalseLabel();
+  }
+
+  // ****
+  //
+  // Overridable methods for implementing intelligent behavior
+  //
+  // ****
+
+  public boolean verifyTypeMatch(Object o)
+  {
+    return (o instanceof Boolean);
+  }
+
+  public boolean verifyNewValue(Object o)
+  {
+    DBEditObject eObj;
+
+    /* -- */
+
+    if (!isEditable())
+      {
+	return false;
+      }
+
+    eObj = (DBEditObject) owner;
+
+    if (!verifyTypeMatch(o))
+      {
+	setLastError("type mismatch");
+	return false;
+      }
+
+    // have our parent make the final ok on the value
+
+    return eObj.verifyNewValue(this, o);
+  }
+
 }
 
