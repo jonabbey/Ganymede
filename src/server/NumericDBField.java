0d59c257491d6964e5c49fd61dac89f73773c086
Jonathan Abbey <jonabbey@arlut.utexas.edu>
1997-04-11 17:12:19 +0000

    Reworked to take advantage of the beefed up DBField base class. -- Jon

--- src/server/NumericDBField.java
+++ src/server/NumericDBField.java
@@ -6,7 +6,7 @@
    The GANYMEDE object storage system.
 
    Created: 2 July 1996
-   Version: $Revision: 1.4 $ %D%
+   Version: $Revision: 1.5 $ %D%
    Module By: Jonathan Abbey
    Applied Research Laboratories, The University of Texas at Austin
 
@@ -23,41 +23,329 @@ import java.util.*;
 
 ------------------------------------------------------------------------------*/
 
-public class NumericDBField extends DBField {
+public class NumericDBField extends DBField implements num_field {
 
-  int value;
+  /**
+   *
+   * Receive constructor.  Used to create a NumericDBField from a DBStore/DBJournal
+   * DataInput stream.
+   *
+   */
 
-  /* -- */
-
-  NumericDBField(DataInput in, DBObjectBaseField definition) throws IOException
+  NumericDBField(DBObject owner, DataInput in, DBObjectBaseField definition) throws IOException
   {
+    value = values = null;
+    this.owner = owner;
     this.definition = definition;
     receive(in);
   }
 
-  public NumericDBField(int value)
+  /**
+   *
+   * No-value constructor.  Allows the construction of a
+   * 'non-initialized' field, for use where the DBObjectBase
+   * definition indicates that a given field may be present,
+   * but for which no value has been stored in the DBStore.
+   *
+   * Used to provide the client a template for 'creating' this
+   * field if so desired.
+   *
+   */
+
+  NumericDBField(DBObject owner, DBObjectBaseField definition)
   {
-    this.definition = null;
-    this.value = value;
+    this.owner = owner;
+    this.definition = definition;
+    
+    defined = false;
+    value = values = null;
+  }
+
+  /**
+   *
+   * Copy constructor.
+   *
+   */
+
+  public NumericDBField(DBObject owner, NumericDBField field)
+  {
+    this.owner = owner;
+    definition = field.definition;
+    
+    if (isVector())
+      {
+	values = (Vector) field.values.clone();
+	value = null;
+      }
+    else
+      {
+	value = field.value;
+	values = null;
+      }
+
+    defined = true;
+  }
+
+  /**
+   *
+   * Scalar value constructor.
+   *
+   */
+
+  public NumericDBField(DBObject owner, int value, DBObjectBaseField definition)
+  {
+    if (definition.isArray())
+      {
+	throw new IllegalArgumentException("scalar constructor called on vector field");
+      }
+
+    this.owner = owner;
+    this.definition = definition;
+    this.value = new Integer(value);
+
+    defined = true;
+    values = null;
+  }
+
+  /**
+   *
+   * Vector value constructor.
+   *
+   */
+
+  public NumericDBField(DBObject owner, Vector values, DBObjectBaseField definition)
+  {
+    if (!definition.isArray())
+      {
+	throw new IllegalArgumentException("vector constructor called on scalar field");
+      }
+
+    this.owner = owner;
+    this.definition = definition;
+
+    if (values == null)
+      {
+	this.values = new Vector();
+	defined = false;
+      }
+    else
+      {
+	this.values = (Vector) values.clone();
+	defined = true;
+      }
+
+    value = null;
+  }
+
+  protected Object clone()
+  {
+    return new DateDBField(owner, this);
   }
 
   void emit(DataOutput out) throws IOException
   {
-    out.writeInt(value);
+    if (isVector())
+      {
+	out.writeShort(values.size());
+	for (int i = 0; i < values.size(); i++)
+	  {
+	    out.writeInt(((Integer) values.elementAt(i)).intValue());
+	  }
+      }
+    else
+      {
+	out.writeInt(((Integer) value).intValue());
+      }
   }
 
   void receive(DataInput in) throws IOException
   {
-    value = in.readInt();
+    int count;
+
+    /* - */
+
+    if (isVector())
+      {
+	count = in.readShort();
+	values = new Vector(count);
+	for (int i = 0; i < count; i++)
+	  {
+	    values.addElement(new Integer(in.readInt()));
+	  }
+      }
+    else
+      {
+	value = in.readInt();
+      }
+
+    defined = true;
   }
+
+  // ****
+  //
+  // type-specific accessor methods
+  //
+  // ****
 
   public int value()
   {
+    if (isVector())
+      {
+	throw new IllegalArgumentException("scalar accessor called on vector field");
+      }
+
     return value;
   }
 
-  public Object key()
+  public int value(int index)
   {
-    return new Integer(value);
+    if (!isVector())
+      {
+	throw new IllegalArgumentException("vector accessor called on scalar field");
+      }
+
+    return ((Integer) values.elementAt(index)).intValue();
   }
+
+  // ****
+  //
+  // num_field specific methods
+  //
+  // ****
+
+  /**
+   *
+   * Returns true if this field has max/min
+   * limitations.  This is not set in the
+   * DBStore schema, so you'll need to subclass
+   * NumericDBField and override this method to
+   * implement a restriction.
+   *
+   * @see arlut.csd.ganymede.num_field
+   *
+   */
+
+  public boolean limited()
+  {
+    DBEditObject eObj;
+
+    /* -- */
+
+    if (!isEditable())
+      {
+	throw new IllegalArgumentException("not applicable to a non-editable field/object");
+      }
+
+    eObj = (DBEditObject) owner;
+
+    return eObj.isIntLimited(this);
+  }
+
+  /**
+   *
+   * Returns the minimum acceptable value for this field if this field
+   * has max/min limitations.  This is not set in the DBStore schema,
+   * so you'll need to subclass NumericDBField and override this
+   * method to implement a restriction.
+   *
+   * @see arlut.csd.ganymede.num_field
+   * 
+   */
+
+  public int getMinValue()
+  {
+    DBEditObject eObj;
+
+    /* -- */
+
+    if (!isEditable())
+      {
+	throw new IllegalArgumentException("not applicable to a non-editable field/object");
+      }
+
+    eObj = (DBEditObject) owner;
+
+    return eObj.minInt(this);
+  }
+
+  /**
+   *
+   * Returns the maximum acceptable value for this field if this field
+   * has max/min limitations.  This is not set in the DBStore schema,
+   * so you'll need to subclass NumericDBField and override this
+   * method to implement a restriction.
+   *
+   * @see arlut.csd.ganymede.num_field
+   * 
+   */
+
+  public int getMaxValue()
+  {
+    DBEditObject eObj;
+
+    /* -- */
+
+    if (!isEditable())
+      {
+	throw new IllegalArgumentException("not applicable to a non-editable field/object");
+      }
+
+    eObj = (DBEditObject) owner;
+
+    return eObj.maxInt(this);
+  }
+
+  // ****
+  //
+  // Overridable methods for implementing intelligent behavior
+  //
+  // ****
+
+  public boolean verifyTypeMatch(Object o)
+  {
+    return (o instanceof Integer);
+  }
+
+  public boolean verifyNewValue(Object o)
+  {
+    DBEditObject eObj;
+    Integer i;
+
+    /* -- */
+
+    if (!isEditable())
+      {
+	return false;
+      }
+
+    eObj = (DBEditObject) owner;
+
+    if (!verifyTypeMatch(o))
+      {
+	setLastError("type mismatch");
+	return false;
+      }
+
+    i = (Integer) o;
+
+    if (limited())
+      {
+	if (getMinValue().intValue() > i.intValue())
+	  {
+	    setLastError("value out of range (underflow)");
+	    return false;
+	  }
+
+	if (getMaxValue().intValue() < i.intValue())
+	  {
+	    setLastError("value out of range (overflow)");
+	    return false;
+	  }
+      }
+
+    // have our parent make the final ok on the value
+
+    return eObj.verifyNewValue(this, s);
+  }
+
 }
