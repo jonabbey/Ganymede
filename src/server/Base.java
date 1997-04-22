da78c98bcfe8cf2bb24a64b6125823673970bab5
Jonathan Abbey <jonabbey@arlut.utexas.edu>
1997-04-22 16:30:42 +0000

    Added createNewField() -- Jon

--- src/server/Base.java
+++ src/server/Base.java
@@ -5,7 +5,7 @@
    Client side interface to the object type dictionary
    
    Created: 17 April 1997
-   Version: $Revision: 1.1 $ %D%
+   Version: $Revision: 1.2 $ %D%
    Module By: Jonathan Abbey
    Applied Research Laboratories, The University of Texas at Austin
 
@@ -29,11 +29,17 @@ import java.rmi.*;
  */
 
 public interface Base extends Remote {
-
   public String getName() throws RemoteException;
   public String getClassName() throws RemoteException;
   public boolean canCreate(Session session) throws RemoteException;
   public boolean canInactivate() throws RemoteException;
   public short getTypeID() throws RemoteException;
   public Vector getFields() throws RemoteException;
+
+  // the following methods are only valid when the Base reference
+  // is obtained from a SchemaEdit reference.
+
+  public void setName(String newName) throws RemoteException;
+  public void setClassName(String newName) throws RemoteException;
+  public BaseField createNewField() throws RemoteException;
 }
