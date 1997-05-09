ef1e5c7f4f3f523afb74b5511c55b87508c197ca
Jonathan Abbey <jonabbey@arlut.utexas.edu>
1997-05-09 15:12:46 +0000

    Initial revision

---
+++ src/server/FieldType.java
@@ -0,0 +1,24 @@
+/*
+
+   FieldType.java
+
+   Hackified enumeration of defined field types
+   
+   Created: 15 April 1997
+   Version: $Revision: 1.1 $ %D%
+   Module By: Jonathan Abbey
+   Applied Research Laboratories, The University of Texas at Austin
+
+*/
+
+package arlut.csd.ganymede;
+
+public interface FieldType {
+  static final short FIRSTFIELD = 0;
+  static final short BOOLEAN = 0;
+  static final short NUMERIC = 1;
+  static final short DATE = 2;
+  static final short STRING = 3;
+  static final short INVID = 4;
+  static final short LASTFIELD = 4;
+}
