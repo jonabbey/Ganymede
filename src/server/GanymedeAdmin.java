6e2fd032aba6676b7154e274263f4ff6af60c967
Jonathan Abbey <jonabbey@arlut.utexas.edu>
1997-05-13 15:22:50 +0000

    Fixed console detach msg. -- Jon

--- src/server/GanymedeAdmin.java
+++ src/server/GanymedeAdmin.java
@@ -8,7 +8,7 @@
    status monitoring and administrative activities.
    
    Created: 17 January 1997
-   Version: $Revision: 1.4 $ %D%
+   Version: $Revision: 1.5 $ %D%
    Module By: Jonathan Abbey
    Applied Research Laboratories, The University of Texas at Austin
 
@@ -162,7 +162,7 @@ class GanymedeAdmin extends UnicastRemoteObject implements adminSession {
   {
     consoles.removeElement(admin);
     this.admin = null;
-    Ganymede.debug("Admin console detached" + new Date());
+    Ganymede.debug("Admin console detached " + new Date());
   }
 
   /**
