4231f1fa432510e6a6b50b820dffea7198106732
Jonathan Abbey <jonabbey@arlut.utexas.edu>
1997-05-14 16:43:29 +0000

    establish() can now be interrupted with abort(). -- Jon

--- src/server/DBReadLock.java
+++ src/server/DBReadLock.java
@@ -6,7 +6,7 @@
    The GANYMEDE object storage system.
 
    Created: 2 July 1996
-   Version: $Revision: 1.2 $ %D%
+   Version: $Revision: 1.3 $ %D%
    Module By: Jonathan Abbey
    Applied Research Laboratories, The University of Texas at Austin
 
@@ -27,13 +27,12 @@ import java.util.*;
 public class DBReadLock extends DBLock {
 
   Enumeration enum;
-  boolean okay, done;
   DBStore lockManager;
   DBObjectBase base;
   Vector baseSet;
   Object key;
   
-  private boolean locked;
+  private boolean locked = false, abort = false, inEstablish = false;
 
   /* -- */
 
@@ -43,15 +42,16 @@ public class DBReadLock extends DBLock {
     this.lockManager = lockManager;
     baseSet = new Vector();
 
-    enum = lockManager.objectBases.elements();
-	    
-    while (enum.hasMoreElements())
+    synchronized (lockManager)
       {
-	base = (DBObjectBase) enum.nextElement();
-	baseSet.addElement(base);
+	enum = lockManager.objectBases.elements();
+	    
+	while (enum.hasMoreElements())
+	  {
+	    base = (DBObjectBase) enum.nextElement();
+	    baseSet.addElement(base);
+	  }
       }
-
-    locked = false;
   }
 
   // constructor to get a read lock on a subset of the
@@ -62,43 +62,61 @@ public class DBReadLock extends DBLock {
     key = null;
     this.lockManager = lockManager;
     this.baseSet = baseSet;
-    locked = false;
   }
 
-  public void establish(Object key)
+  public void establish(Object key) throws InterruptedException
   {
-    done = false;
+    boolean done, okay;
 
-    if (lockManager.lockHash.containsKey(key))
-      {
-	throw new RuntimeException("Error: lock sought by owner of existing lockset.");
-      }
-
-    lockManager.lockHash.put(key, this);
-    this.key = key;
+    /* -- */
 
     synchronized (lockManager)
       {
-	// wait until there are no writers blocking our access
+	if (lockManager.lockHash.containsKey(key))
+	  {
+	    throw new RuntimeException("Error: lock sought by owner of existing lockset.");
+	  }
+	
+	lockManager.lockHash.put(key, this);
+	this.key = key;
+
+	inEstablish = true;
+
+	done = false;
 
 	while (!done)
 	  {
+	    // if we've received an abort notification, bail.
+
+	    if (abort)
+	      {
+		inEstablish = false;
+		lockManager.notifyAll();
+		throw new InterruptedException();
+	      }
+
+	    // assume we can proceed to get our lock until we find out
+	    // otherwise
+
 	    okay = true;
+
+	    // if the schema is being edited, we can't proceed
 
 	    if (lockManager.schemaEditInProgress)
 	      {
 		okay = false;
 	      }
-	    else
+
+	    // if there are any writers queued, we have to wait
+	    // for them to finish before we can proceed
+
+	    for (int i = 0; okay && (i < baseSet.size()); i++)
 	      {
-		for (int i = 0; okay && (i < baseSet.size()); i++)
-		  {
-		    base = (DBObjectBase) baseSet.elementAt(i);
+		base = (DBObjectBase) baseSet.elementAt(i);
 		    
-		    if (base.writerList.size() > 0)
-		      {
-			okay = false;
-		      }
+		if (base.writerList.size() > 0)
+		  {
+		    okay = false;
 		  }
 	      }
 
@@ -108,51 +126,66 @@ public class DBReadLock extends DBLock {
 		  {
 		    base = (DBObjectBase) baseSet.elementAt(i);
 		    base.readerList.addElement(this);
-		    // we don't need to set currentLock
-		    // since readers are shared
 		  }
 
 		done = true;
+		inEstablish = false;
 	      }
 	    else
 	      {
-		try
-		  {
-		    lockManager.wait();
-		  }
-		catch (InterruptedException ex)
-		  {
-		  }
+		lockManager.wait(); // an InterruptedException here gets propagated up
 	      }
-	  }
-      }
+	  } // while (!done)
 
-    locked = true;
+	locked = true;
+	lockManager.notifyAll(); // let a thread trying to release this lock proceed
+
+      }	// synchronized (lockManager)
   }
 
   public void release()
   {
-    if (!locked)
-      {
-	return;
-      }
-
     synchronized (lockManager)
       {
+	while (inEstablish)
+	  {
+	    try
+	      {
+		lockManager.wait();
+	      } 
+	    catch (InterruptedException ex)
+	      {
+	      }
+	  }
+
+	if (!locked)
+	  {
+	    return;
+	  }
+
 	for (int i = 0; i < baseSet.size(); i++)
 	  {
 	    base = (DBObjectBase) baseSet.elementAt(i);
 	    base.readerList.removeElement(this);
-	    // we don't need to clear currentLock
-	    // since readers are shared
 	  }
 
 	locked = false;
 	lockManager.lockHash.remove(key);
 	key = null;
 
-	lockManager.notify();
+	lockManager.notifyAll();
       }
+  }
+
+  public void abort()
+  {
+    synchronized (lockManager)
+      {
+	abort = true;
+	lockManager.notifyAll();
+      }
+
+    release();
   }
 
   boolean isLocked(DBObjectBase base)
