6024757482f3b8a09ad4fbaa783f98c2aa17ad52
Jonathan Abbey <jonabbey@arlut.utexas.edu>
1997-06-09 11:50:52 +0000

    Put in GNU license info. -- Jon

--- src/JTable/testTable.java
+++ src/JTable/testTable.java
@@ -3,8 +3,24 @@
 
   A test framework for the baseTable GUI component
 
+  Copyright (C) 1997  The University of Texas at Austin.
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
   Created: 5 June 1996
-  Version: $Revision: 1.6 $ %D%
+  Version: $Revision: 1.7 $ %D%
   Module By: Jonathan Abbey -- jonabbey@arlut.utexas.edu
   Applied Research Laboratories, The University of Texas at Austin
 
@@ -22,7 +38,7 @@ import arlut.csd.Table.*;
 
 ------------------------------------------------------------------------------*/
 
-public class testTable extends Applet implements rowSelectCallback, ActionListener {
+public class testTable extends Applet implements arlut.csd.Table.rowSelectCallback, ActionListener {
 
   static final boolean debug = false;
   static testTable applet = null;
