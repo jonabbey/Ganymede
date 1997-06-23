43f1b916a2ad8cec5d1b14b8b36ee2c9e5af0535
Jonathan Abbey <jonabbey@arlut.utexas.edu>
1997-06-23 13:17:24 +0000

    Added LabelField() methods. -- Jon

--- src/server/Base.java
+++ src/server/Base.java
@@ -5,7 +5,7 @@
    Client side interface to the object type dictionary
    
    Created: 17 April 1997
-   Version: $Revision: 1.5 $ %D%
+   Version: $Revision: 1.6 $ %D%
    Module By: Jonathan Abbey
    Applied Research Laboratories, The University of Texas at Austin
 
@@ -33,6 +33,9 @@ public interface Base extends Remote {
   public String getName() throws RemoteException;
   public String getClassName() throws RemoteException;
   public short getTypeID() throws RemoteException;
+  public short getLabelField() throws RemoteException;
+  public String getLabelFieldName() throws RemoteException;
+
   public Vector getFields() throws RemoteException;
   public BaseField getField(short id) throws RemoteException;
   public BaseField getField(String name) throws RemoteException;
@@ -45,6 +48,10 @@ public interface Base extends Remote {
 
   public void setName(String newName) throws RemoteException;
   public void setClassName(String newName) throws RemoteException;
+
+  public void setLabelField(String fieldName) throws RemoteException;
+  public void setLabelField(short fieldID) throws RemoteException;
+
   public BaseField createNewField() throws RemoteException;
   public boolean deleteField(BaseField bF) throws RemoteException;
   public boolean fieldInUse(BaseField bF) throws RemoteException;
