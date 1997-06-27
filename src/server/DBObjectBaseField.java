3fbc0af785623e6466f59280f76ec4edc06f0875
Jonathan Abbey <jonabbey@arlut.utexas.edu>
1997-06-27 16:33:37 +0000

    Fixed fall through PERMISSIONMATRIX case. -- Jon

--- src/server/DBObjectBaseField.java
+++ src/server/DBObjectBaseField.java
@@ -6,7 +6,7 @@
    The GANYMEDE object storage system.
 
    Created: 27 August 1996
-   Version: $Revision: 1.16 $ %D%
+   Version: $Revision: 1.17 $ %D%
    Module By: Jonathan Abbey
    Applied Research Laboratories, The University of Texas at Austin
 
@@ -1542,6 +1542,7 @@ public class DBObjectBaseField extends UnicastRemoteObject implements BaseField,
 
       case PERMISSIONMATRIX:
 	result = "permission matrix";
+	break;
 
       default:
 	result = "<<bad type code>>";
