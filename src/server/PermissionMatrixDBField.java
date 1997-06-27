105bf1de9557908c6b4674382d1c67b18e595ca9
Jonathan Abbey <jonabbey@arlut.utexas.edu>
1997-06-27 16:41:22 +0000

    Initial revision

---
+++ src/server/PermissionMatrixDBField.java
@@ -0,0 +1,391 @@
+/*
+
+   PermissionMatrixDBField.java
+
+   This class defines the permission matrix field used in the
+   'Admin' DBObjectBase class.
+   
+   Created: 27 June 1997
+   Version: $Revision: 1.1 $ %D%
+   Module By: Jonathan Abbey
+   Applied Research Laboratories, The University of Texas at Austin
+
+*/
+
+package arlut.csd.ganymede;
+
+import java.io.*;
+import java.util.*;
+import java.rmi.*;
+
+/*------------------------------------------------------------------------------
+                                                                           class
+                                                         PermissionMatrixDBField
+
+------------------------------------------------------------------------------*/
+
+public class PermissionMatrixDBField extends DBField implements perm_field {
+
+  private Hashtable matrix;
+
+  /* -- */
+
+  /**
+   *
+   * Receive constructor.  Used to create a PermissionMatrixDBField
+   * from a DBStore/DBJournal DataInput stream.
+   * 
+   */
+
+  PermissionMatrixDBField(DBObject owner, DataInput in, DBObjectBaseField definition) throws IOException, RemoteException
+  {
+    value = values = null;
+    this.owner = owner;
+    this.definition = definition;
+    receive(in);
+  }
+
+  /**
+   *
+   * No-value constructor.  Allows the construction of a
+   * 'non-initialized' field, for use where the DBObjectBase
+   * definition indicates that a given field may be present,
+   * but for which no value has been stored in the DBStore.
+   *
+   * Used to provide the client a template for 'creating' this
+   * field if so desired.
+   *
+   */
+
+  PermissionMatrixDBField(DBObject owner, DBObjectBaseField definition) throws RemoteException
+  {
+    this.owner = owner;
+    this.definition = definition;
+    
+    matrix = new Hashtable();
+    defined = false;
+    value = null;
+    values = null;
+  }
+
+  /**
+   *
+   * Copy constructor.
+   *
+   */
+
+  public PermissionMatrixDBField(DBObject owner, PermissionMatrixDBField field) throws RemoteException
+  {
+    value = values = null;
+
+    this.definition = field.definition;
+    this.owner = owner;
+    this.matrix = field.matrix.clone();
+
+    defined = true;
+  }
+
+  // fancy equals method really does check for value equality
+
+  public synchronized boolean equals(Object obj)
+  {
+    PermissionMatrixDBField pmdb;
+    Enumeration keys;
+    String key;
+    
+    /* -- */
+
+    if (!(obj.getClass().equals(this.getClass())))
+      {
+	return false;
+      }
+
+    pmdb = (PermissionMatrixDBField) obj;
+
+    if (matrix.size() != pmdb.matrix.size())
+      {
+	return false;
+      }
+
+    keys = matrix.keys();
+
+    while (keys.hasMoreElements())
+      {
+	key = (String) keys.nextElement();
+
+	try
+	  {
+	    if (!(matrix.get(key).equals(pmdb.matrix.get(key))))
+	      {
+		return false;
+	      }
+	  }
+	catch (NullPointerException ex)
+	  {
+	    return false;
+	  }
+      }
+    
+    return true;
+  }
+
+  // we don't really want to hash according to our permission
+  // contents, so just hash according to our containing object's
+  // i.d.
+
+  public Object key()
+  {
+    return new Integer(owner.getID());
+  }
+
+  // we don't allow setValue.. PermissionMatrixDBField doesn't allow
+  // direct setting of the entire matrix.. just use the get() and set()
+  // methods below.
+
+  public boolean setValue(Object value)
+  {
+    return false;
+  }
+
+  public Object clone()
+  {
+    try
+      {
+	return new PermissionMatrixDBField(owner, this);
+      }
+    catch (RemoteException ex)
+      {
+	throw new RuntimeException("Couldn't create PermissionMatrixDBField: " + ex);
+      }
+  }
+
+  void synchronized emit(DataOutput out) throws IOException
+  {
+    Enumeration keys;
+    PermEntry pe;
+    String key;
+
+    /* -- */
+
+    out.writeInt(matrix.size());
+
+    keys = matrix.keys();
+
+    while (keys.hasMoreElements())
+      {
+	key = (String) keys.nextElement();
+	pe = (PermEntry) keys.get(key);
+
+	out.writeUTF(key);
+	pe.emit(out);
+      }
+  }
+
+  void synchronized receive(DataInput in) throws IOException
+  {
+    int tableSize;
+    PermEntry pe;
+    String key;
+
+    /* -- */
+
+    tableSize = in.readInt();
+    matrix = new Hashtable(tableSize);
+    
+    for (int i = 0; i < tableSize; i++)
+      {
+	key = in.readUTF();
+	pe = new PermEntry(in);
+	matrix.put(key, pe);
+      }
+  }
+
+  /**
+   *
+   * Returns a PermEntry object representing this PermMatrix's 
+   * permissions on the field <fieldID> in base <baseID>
+   *
+   * @see arlut.csd.ganymede.PermMatrix
+   */
+
+  public PermEntry getPerm(short baseID, short fieldID)
+  {
+    return (PermEntry) matrix.get(matrixEntry(baseID, fieldID));
+  }
+
+  /**
+   *
+   * Returns a PermEntry object representing this PermMatrix's 
+   * permissions on the base <baseID>
+   *
+   * @see arlut.csd.ganymede.PermMatrix
+   */
+
+  public PermEntry getPerm(short baseID)
+  {
+    return (PermEntry) matrix.get(matrixEntry(baseID));
+  }
+
+  /**
+   *
+   * Returns a PermEntry object representing this PermMatrix's 
+   * permissions on the field <field> in base <base>
+   *
+   * @see arlut.csd.ganymede.PermMatrix
+   */
+
+  public PermEntry getPerm(Base base, BaseField field)
+  {
+    try
+      {
+	return (PermEntry) matrix.get(matrixEntry(base.getTypeID(), field.getID()));
+      }
+    catch (RemoteException ex)
+      {
+	throw new RuntimeException("caught remote: " + ex);
+      }
+
+    return null;
+  }
+
+  /**
+   *
+   * Returns a PermEntry object representing this PermMatrix's 
+   * permissions on the base <base>
+   *
+   * @see arlut.csd.ganymede.PermMatrix
+   */
+
+  public PermEntry getPerm(Base base)
+  {
+    try
+      {
+	return (PermEntry) matrix.get(matrixEntry(base.getTypeID()));
+      }
+    catch (RemoteException ex)
+      {
+	throw new RuntimeException("caught remote: " + ex);
+      }
+
+    return null;
+  }
+
+  /**
+   *
+   * Sets the permission entry for this matrix for base <baseID>,
+   * field <fieldID> to PermEntry <entry>.
+   *
+   * This operation will fail if this permission matrix is not
+   * associated with a currently checked-out-for-editing
+   * PermissionMatrixDBField.
+   *
+   * @see arlut.csd.ganymede.PermMatrix 
+   */
+
+  public synchronized void setPerm(short baseID, short fieldID, PermEntry entry)
+  {
+    if (isEditable())
+      {
+	matrix.put(matrixEntry(baseID, fieldID), entry);
+      }
+    else
+      {
+	throw new IllegalArgumentException("not an editable field");
+      }
+  }
+
+  /**
+   *
+   * Sets the permission entry for this matrix for base <baseID>
+   * to PermEntry <entry>
+   *
+   * This operation will fail if this permission matrix is not
+   * associated with a currently checked-out-for-editing
+   * PermissionMatrixDBField.
+   *
+   * @see arlut.csd.ganymede.PermMatrix
+   */
+
+  public synchronized void setPerm(short baseID, PermEntry entry)
+  {
+    if (isEditable())
+      {
+	matrix.put(matrixEntry(baseID), entry);
+      }
+    else
+      {
+	throw new IllegalArgumentException("not an editable field");
+      }
+  }
+
+  /**
+   *
+   * Sets the permission entry for this matrix for base <base>,
+   * field <field> to PermEntry <entry>
+   *
+   * This operation will fail if this permission matrix is not
+   * associated with a currently checked-out-for-editing
+   * PermissionMatrixDBField.
+   *
+   * @see arlut.csd.ganymede.PermMatrix
+   */
+
+  public synchronized void setPerm(Base base, BaseField field, PermEntry entry)
+  {
+    if (isEditable())
+      {
+	try
+	  {
+	    matrix.put(matrixEntry(base.getTypeID(), field.getID()), entry);
+	  }
+	catch (RemoteException ex)
+	  {
+	  }
+      }
+    else
+      {
+	throw new IllegalArgumentException("not an editable field");
+      }
+
+  }
+
+  /**
+   *
+   * Sets the permission entry for this matrix for base <baseID>
+   * to PermEntry <entry>.
+   *
+   * This operation will fail if this permission matrix is not
+   * associated with a currently checked-out-for-editing
+   * PermissionMatrixDBField.
+   *
+   * @see arlut.csd.ganymede.PermMatrix
+   */
+
+  public synchronized void setPerm(Base base, PermEntry entry)
+  {
+    if (isEditable())
+      {
+	try
+	  {
+	    matrix.put(matrixEntry(base.getTypeID()), entry);
+	  }
+	catch (RemoteException ex)
+	  {
+	  }
+      }
+    else
+      {
+	throw new IllegalArgumentException("not an editable field");
+      }
+  }
+
+  private String matrixEntry(short baseID, short fieldID)
+  {
+    return (String) baseID + ":" + fieldID;
+  }
+  
+  private String matrixEntry(short baseID)
+  {
+    return (String) baseID + "::";
+  }
+}
---
+++ src/server/perm_field.java
@@ -0,0 +1,33 @@
+/*
+
+   perm_field.java
+
+   Client-side interface to the PermissionMatrixDBField class.
+   
+   Created: 27 June 1997
+   Version: $Revision: 1.1 $ %D%
+   Module By: Jonathan Abbey
+   Applied Research Laboratories, The University of Texas at Austin
+
+*/
+
+package arlut.csd.ganymede;
+
+import java.rmi.*;
+
+/*------------------------------------------------------------------------------
+                                                                       interface
+                                                                      perm_field
+
+------------------------------------------------------------------------------*/
+
+interface perm_field extends db_field {
+  public PermEntry getPerm(short baseID, short fieldID) throws RemoteException;
+  public PermEntry getPerm(short baseID) throws RemoteException;
+  public PermEntry getPerm(Base base, BaseField field) throws RemoteException;
+  public PermEntry getPerm(Base base) throws RemoteException;
+  public void setPerm(short baseID, short fieldID, PermEntry entry) throws RemoteException;
+  public void setPerm(short baseID, PermEntry entry) throws RemoteException;
+  public void setPerm(Base base, BaseField field, PermEntry entry) throws RemoteException;
+  public void setPerm(Base base, PermEntry entry) throws RemoteException;
+}
