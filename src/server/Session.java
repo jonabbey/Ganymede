c57c0c2e13cc417344e551c89303f0f4a3a44ed0
Jonathan Abbey <jonabbey@arlut.utexas.edu>
1997-07-09 14:47:57 +0000

    Added explicit transaction management methods, remove_db_object(). -- Jon

--- src/server/Session.java
+++ src/server/Session.java
@@ -10,7 +10,7 @@
    primary interface for accessing ganymede db objects.
 
    Created: 1 April 1996
-   Version: $Revision: 1.2 $ %D%
+   Version: $Revision: 1.3 $ %D%
    Module By: Jonathan Abbey
    Applied Research Laboratories, The University of Texas at Austin
 
@@ -54,11 +54,58 @@ public interface Session extends Remote {
    *
    */
 
-  Enumeration getTypes() throws RemoteException;
+  Vector      getTypes() throws RemoteException;
 
   /**
    *
-   * List objects in the database meeting the given query criteria
+   * This method call initiates a transaction on the server.  This
+   * call must be executed before any objects are modified (created,
+   * edited, inactivated, removed).
+   *
+   * Currently each client can only have one transaction open.. it
+   * is an error to call openTransaction() while another transaction
+   * is still open, and an exception will be thrown.
+   * 
+   */
+
+  void     openTransaction() throws RemoteException;
+
+  /**
+   *
+   * This method causes all changes made by the client to be 'locked in'
+   * to the database.  When commitTransaction() is called, the changes
+   * made by the client during this transaction is logged to a journal
+   * file on the server, and the changes will become visible to other
+   * clients, or to subsequent queries and view_db_object() calls by
+   * this client.
+   *
+   * If the transaction cannot be committed for some reason,
+   * commitTransaction() will instead abort the transaction.  In any
+   * case, calling commitTransaction() will close the transaction.
+   *
+   * @returns false if the transaction could not be committed.  
+   *                getLastError() can be called to obtain an explanation
+   *                of commit failure.
+   * 
+   */
+
+  boolean  commitTransaction() throws RemoteException;
+
+  /**
+   *
+   * This method causes all changes made by the client to be thrown out
+   * by the database, and the transaction is closed.
+   *
+   */
+  
+  void     abortTransaction() throws RemoteException;
+
+  /**
+   *
+   * List objects in the database meeting the given query criteria.
+   *
+   * The database will be read-locked during the query, assuring
+   * a transaction-consistent view of the database.
    *
    */
 
@@ -67,7 +114,12 @@ public interface Session extends Remote {
   /**
    *
    * View an object from the database.  If the return value is null,
-   * getLastError() should be called for a description of the problem. 
+   * getLastError() should be called for a description of the problem.
+   *
+   * view_db_object() can be done at any time, outside of the bounds of
+   * any transaction.  view_db_object() returns a snapshot of the object's
+   * state at the time the view_db_object() call is processed, and will
+   * be transaction-consistent internally.
    *
    */
 
@@ -81,7 +133,7 @@ public interface Session extends Remote {
    *
    */
 
-  storable_object   edit_db_object(Invid invid) throws RemoteException;
+  db_object   edit_db_object(Invid invid) throws RemoteException;
 
   /**
    *
@@ -91,7 +143,8 @@ public interface Session extends Remote {
    * @return the newly created object for editing
    *
    */
-  storable_object   create_db_object(short type) throws RemoteException;
+
+  db_object   create_db_object(short type) throws RemoteException;
 
   /**
    *
@@ -109,5 +162,20 @@ public interface Session extends Remote {
    */
 
   boolean     inactivate_db_object(Invid invid) throws RemoteException;
+
+  /**
+   *
+   * Remove an object from the database
+   *
+   * Certain objects cannot be inactivated, but must instead be
+   * simply removed on demand.  The active permissions for the client
+   * may determine whether a particular type of object may be removed.
+   *
+   * @return true if the object was removed, if false, check
+   * getLastError()
+   *
+   */
+
+  boolean     remove_db_object(Invid invid) throws RemoteException;
 }
 
