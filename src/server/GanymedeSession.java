dd26d3d1a26a85b36f74c5ebd6a8b6d0683e66ec
Jonathan Abbey <jonabbey@arlut.utexas.edu>
1997-07-10 18:58:01 +0000

    Added viewObjectLabel(), removed DBReadLock locking for view_db_object() and edit_db_object() -- Jon

--- src/server/GanymedeSession.java
+++ src/server/GanymedeSession.java
@@ -7,7 +7,7 @@
    the Ganymede server.
    
    Created: 17 January 1997
-   Version: $Revision: 1.2 $ %D%
+   Version: $Revision: 1.3 $ %D%
    Module By: Jonathan Abbey
    Applied Research Laboratories, The University of Texas at Austin
 
@@ -105,6 +105,10 @@ class GanymedeSession extends UnicastRemoteObject implements Session {
 
     connecttime = new Date();
 
+    // construct our DBSession
+
+    session = new DBSession(Ganymede.db, client);
+
     GanymedeServer.sessions.addElement(this);
     GanymedeAdmin.refreshUsers();
   }
@@ -154,6 +158,8 @@ class GanymedeSession extends UnicastRemoteObject implements Session {
   /** 
    * getLastError() returns text explaining the last
    * error condition 
+   *
+   * @see arlut.csd.ganymede.Session
    */
   
   synchronized public String getLastError()
@@ -165,6 +171,13 @@ class GanymedeSession extends UnicastRemoteObject implements Session {
   {
     return true;
   }
+
+  /**
+   * 
+   * Log out this session.
+   *
+   * @see arlut.csd.ganymede.Session
+   */
 
   synchronized public void logout()
   {
@@ -187,45 +200,324 @@ class GanymedeSession extends UnicastRemoteObject implements Session {
   /**
    *
    * This method returns a list of remote references to the Ganymede
-   * object type definitions.
+   * object type definitions.  This method will throws a RuntimeException
+   * if it is called when the server is in schemaEditMode.
    * 
    * @see arlut.csd.ganymede.Session
    */
 
-  public Enumeration getTypes()
+  public Vector getTypes()
   {
-    // how do we manage this so that we don't have to
-    // worry about concurrency issues?  We basically
-    // just want to make sure that the client can't
-    // retain access to this enumeration after
-    // the system goes into schema editing mode..
-    // Is the schema edit establish code sufficiently
-    // rigorous to permit this?
+    Enumeration enum;
+    Vector result = new Vector();
 
-    return Ganymede.db.objectBases.elements();
+    /* -- */
+
+    synchronized (Ganymede.db)
+      {
+	if (Ganymede.db.schemaEditInProgress)
+	  {
+	    throw new RuntimeException("schemaEditInProgress");
+	  }
+
+	enum = Ganymede.db.objectBases.elements();
+
+	while (enum.hasMoreElements())
+	  {
+	    result.addElement(enum.nextElement());
+	  }
+      }
+
+    return result;
   }
 
-  public Result[] query(Query query)
+  /**
+   *
+   * This method call initiates a transaction on the server.  This
+   * call must be executed before any objects are modified (created,
+   * edited, inactivated, removed).
+   *
+   * Currently each client can only have one transaction open.. it
+   * is an error to call openTransaction() while another transaction
+   * is still open, and an exception will be thrown.
+   * 
+   * @see arlut.csd.ganymede.Session
+   */
+
+  public void openTransaction()
   {
-    return null;
+    if (session.editSet != null)
+      {
+	throw new IllegalArgumentException("transaction already opened");
+      }
+
+    session.openTransaction();
   }
 
-  public db_object view_db_object(Invid invid)
+  /**
+   *
+   * This method causes all changes made by the client to be 'locked in'
+   * to the database.  When commitTransaction() is called, the changes
+   * made by the client during this transaction is logged to a journal
+   * file on the server, and the changes will become visible to other
+   * clients, or to subsequent queries and view_db_object() calls by
+   * this client.
+   *
+   * If the transaction cannot be committed for some reason,
+   * commitTransaction() will instead abort the transaction.  In any
+   * case, calling commitTransaction() will close the transaction.
+   *
+   * @returns false if the transaction could not be committed.  
+   *                getLastError() can be called to obtain an explanation
+   *                of commit failure.
+   * 
+   * @see arlut.csd.ganymede.Session
+   */
+
+  public synchronized boolean commitTransaction()
   {
-    return session.viewDBObject(invid);
+    if (session.editSet == null)
+      {
+	throw new IllegalArgumentException("no transaction in progress");
+      }
+
+    return session.commitTransaction();
   }
 
-  public storable_object edit_db_object(Invid invid)
+  /**
+   *
+   * This method causes all changes made by the client to be thrown out
+   * by the database, and the transaction is closed.
+   *
+   * @see arlut.csd.ganymede.Session
+   */
+
+  public void abortTransaction()
   {
-    return session.editDBObject(invid);
+    if (session.editSet == null)
+      {
+	throw new IllegalArgumentException("no transaction in progress");
+      }
+
+    session.abortTransaction();
   }
 
-  public storable_object create_db_object(short type) 
+  /**
+   *
+   * This method provides the hook for doing all
+   * manner of object listing for the Ganymede
+   * database.
+   *
+   * @see arlut.csd.ganymede.Query
+   * @see arlut.csd.ganymede.Result
+   *
+   * @see arlut.csd.ganymede.Session
+   */
+
+  public synchronized Vector query(Query query)
+  {
+    DBObjectBase base = null;
+    Vector baseLock = new Vector();
+    Vector result = new Vector();
+    Enumeration enum;
+    Integer key;
+    DBObject obj;
+
+    /* -- */
+
+    if (query == null)
+      {
+	setLastError("null query");
+	return null;
+      }
+
+    if (query.objectType != -1)
+      {
+	base = Ganymede.db.getObjectBase(query.objectType);
+      }
+    else if (query.objectName != null)
+      {
+	base = Ganymede.db.getObjectBase(query.objectName);
+      }
+
+    if (base == null)
+      {
+	setLastError("No such base");
+	return null;
+      }
+
+    baseLock.addElement(base);
+
+    //    Ganymede.debug("Query: opening read lock");
+
+    try
+      {
+	session.openReadLock(baseLock);	// wait for it
+      }
+    catch (InterruptedException ex)
+      {
+	setLastError("lock interrupted");
+	return null;		// we're probably being booted off
+      }
+
+    // Ganymede.debug("Query: got read lock");
+
+    enum = base.objectHash.keys();
+
+    // need to check in here to see if we've had the lock yanked
+
+    while (enum.hasMoreElements())
+      {
+	key = (Integer) enum.nextElement();
+	obj = (DBObject) base.objectHash.get(key);
+
+	if (DBQueryHandler.matches(query, obj))
+	  {
+	    // Ganymede.debug("Adding element");
+	    result.addElement(new Result(obj));
+	  }
+      }
+    
+    session.releaseReadLock();
+
+    // Ganymede.debug("Query: released read lock");
+
+    return result;
+  }
+
+  /**
+   *
+   *
+   * @see arlut.csd.ganymede.Session
+   */ 
+
+  public String viewObjectLabel(Invid invid)
+  {
+    return session.viewDBObject(invid).getLabel();
+  }
+
+  /**
+   * View an object from the database.  If the return value is null,
+   * getLastError() should be called for a description of the problem.
+   *
+   * view_db_object() can be done at any time, outside of the bounds of
+   * any transaction.  view_db_object() returns a snapshot of the object's
+   * state at the time the view_db_object() call is processed, and will
+   * be transaction-consistent internally.
+   *
+   * If view_db_object() is called during a transaction, the object
+   * will be returned as it stands during the transaction.. that is,
+   * if the object has been changed during the transaction, that
+   * changed object will be returned, even if the transaction has
+   * not yet been committed, and other clients would not be able to
+   * see that version of the object.
+   *
+   * @see arlut.csd.ganymede.Session
+   */
+
+  public synchronized db_object view_db_object(Invid invid)
+  {
+    db_object result;
+    Vector baseLock;
+
+    /* -- */
+
+    //    baseLock = new Vector();
+    //    baseLock.addElement(session.store.getObjectBase(invid.getType()));
+    //
+    //    try
+    //      {
+    //	session.openReadLock(baseLock);	// wait for it
+    //      }
+    //    catch (InterruptedException ex)
+    //      {
+    //	return null;		// we're probably being booted off
+    //      }
+    
+    result = session.viewDBObject(invid);
+
+    // session.releaseReadLock();
+
+    return result;
+  }
+
+  /**
+   * This method provides a handle to an editable
+   * object from the Ganymede database.
+   *
+   * @see arlut.csd.ganymede.Session
+   */
+
+  public synchronized db_object edit_db_object(Invid invid)
+  {
+    db_object result;
+    Vector baseLock;
+
+    /* -- */
+
+    //    baseLock = new Vector();
+    //    baseLock.addElement(session.store.getObjectBase(invid.getType()));
+    //
+    //    try
+    //      {
+    //	session.openReadLock(baseLock);	// wait for it
+    //      }
+    //    catch (InterruptedException ex)
+    //      {
+    //	return null;		// we're probably being booted off
+    //      }
+    
+    result = session.editDBObject(invid);
+
+    //    session.releaseReadLock();
+
+    return result;
+  }
+
+  /**
+   * This method provides a handle to a newly
+   * created object of the specified type in the
+   * Ganymede database.
+   *
+   * @see arlut.csd.ganymede.Session
+   */
+
+  public synchronized db_object create_db_object(short type) 
   {
     return session.createDBObject(type);
   }
+
+  /**
+   * Inactivate an object in the database
+   *
+   * Objects inactivated will typically be altered to reflect their inactive
+   * status, but the object itself might not be purged from the Ganymede
+   * server for a defined period of time, to allow other network systems
+   * to have time to do accounting, clean up, etc., before a user id or
+   * network address is re-used.
+   *
+   * @see arlut.csd.ganymede.Session
+   */
   
-  public boolean inactivate_db_object(Invid invid) 
+  public synchronized boolean inactivate_db_object(Invid invid) 
+  {
+    return false;
+  }
+
+  /**
+   * Remove an object from the database
+   *
+   * Certain objects cannot be inactivated, but must instead be
+   * simply removed on demand.  The active permissions for the client
+   * may determine whether a particular type of object may be removed.
+   *
+   * @return true if the object was removed, if false, check
+   * getLastError()
+   *
+   * @see arlut.csd.ganymede.Session
+   */
+  
+  public synchronized boolean remove_db_object(Invid invid) 
   {
     return false;
   }
