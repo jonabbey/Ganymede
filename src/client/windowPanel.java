945957b310f4c68c3a8eff804b527bc69cd8e9ae
Michael Mulvaney <mulvaney@arlut.utexas.edu>
1997-07-14 11:31:21 +0000

    Still has table layout, not working well. Mike

--- src/client/windowPanel.java
+++ src/client/windowPanel.java
@@ -15,10 +15,13 @@ import java.rmi.*;
 import arlut.csd.ganymede.*;
 import arlut.csd.ganymede.client.*;
 
-public class windowPanel extends JPanel implements ActionListener{
+import arlut.csd.JDataComponent.*;
+
+public class windowPanel extends JPanel implements ActionListener, InternalFrameListener, JsetValueCallback{
 
   JLayeredPane lc;
-  
+  int topLayer = 0;
+  JTitledPane panel;
 
   public windowPanel()
     {
@@ -26,8 +29,6 @@ public class windowPanel extends JPanel implements ActionListener{
       setLayout(new BorderLayout());
       lc = new JLayeredPane();
 
-      Label test = new Label("There should be a window under this!");
-      add("North", test);
       add("Center", lc);
 
 
@@ -45,11 +46,12 @@ public class windowPanel extends JPanel implements ActionListener{
       w.setMaxable(true);
       w.setTitle("This is the title");
       w.setResizable(true);      
+      w.addFrameListener(this);
+      w.setLayout(new BorderLayout());
 
-      Panel panel = new Panel();
+      //      OpaqueJPanel panel = new OpaqueJPanel();
+      panel = new JTitledPane();
       panel.setLayout(new TableLayout());
-
-      addRow(panel, new Label("test"), "this is a", 0);
 
       // Get the list of fields
       db_field[] fields = null;
@@ -69,8 +71,24 @@ public class windowPanel extends JPanel implements ActionListener{
 	      for (int i = 0; i < fields.length ; i++)
 		{
 		  System.out.println("Adding " + fields[i].getName());
-		  Label asdf = new Label(fields[i].getTypeDesc());
-		  addRow(panel, asdf, fields[i].getName(), i+1);
+		  //JstringField asdf = new JstringField();
+		  JTextField asdf = new JTextField();
+
+		  //asdf.setCallback(this);
+		  
+		  //asdf.setEditable(true);
+		  //asdf.setText(fields[i].getTypeDesc());
+		  /*if (fields[i].getTypeDesc().equals("string"))
+		    {
+		      //asdf.setText((String)fields[i].getValue());
+		      asdf.setText("Heya");
+		    }
+		  else
+		    {
+		      asdf.setText(fields[i].getTypeDesc());
+		    }
+		    */
+		  addRow(panel, asdf, fields[i].getName(), i);
 		}
 	    }
 	  catch (RemoteException rx)
@@ -78,30 +96,127 @@ public class windowPanel extends JPanel implements ActionListener{
 	      throw new RuntimeException("Could not get field info: " + rx);
 	    }
 	}
+
+      //w.setContentView(panel);
       w.add(panel);
-      w.add(new Label("adding this lable"));
       //w.setBounds(20,20, panel.getPreferredSize().width, panel.getPreferredSize().height);
       w.setBounds(5,5,300,300);
-      w.setLayer(0);
+      w.setLayer(topLayer);
+      //lc.setLayer(w, topLayer);
+      System.out.println("Setting layer to " + topLayer);
+
       lc.add(w);
+      lc.repaint();
     }
 
   // Event handlers
+  public boolean setValuePerformed(JValueObject v)
+    {
+      System.out.println("setValuePerformed");
+      return true;
+    }
+
   public void actionPerformed(ActionEvent e)
     {
       System.out.println("Action performed");
     }
 
+  public  void frameDidClose(InternalFrameEvent e)
+    {
+      System.out.println("frameDidClose");
+    }
+   
+
+  public  void frameDidMaximize(InternalFrameEvent e)
+    {
+      System.out.println("frameDidMaximize");
+    }
+
+  public  void frameDidMinimize(InternalFrameEvent e)
+    {
+      System.out.println("frameDidMinimize");
+    }
+
+  public  void frameDidIconify(InternalFrameEvent e)
+    {
+      System.out.println("frameDidIconify");
+    }
+
+  public  void frameDidDeiconify(InternalFrameEvent e)
+    {
+      System.out.println("frameDidDeiconify");
+    }
+   
+ public  void frameDidBecomeMain(InternalFrameEvent e)
+    {
+      System.out.println("frameDidBecomeMain");
+      /*      if (e.getInternalFrame().getLayer() < topLayer)
+	{
+	  e.getInternalFrame().setLayer(++topLayer);
+	}
+       */
+    }
+   
+ public  void frameDidLoseMain(InternalFrameEvent e)
+    {
+      System.out.println("frameDidLoseMain");
+    }
+   
+ public  void frameDidSize(InternalFrameEvent e)
+    {
+      System.out.println("frameDidSize");
+    }
+   
+
+ public  void frameDidMove(InternalFrameEvent e)
+    {
+      System.out.println("frameDidMove");
+
+    }
+
+
   // Convenience methods
   void addRow(Panel parent, Component comp,  String label, int row)
     {
-      Label l = new Label(label);
+       System.out.println("Adding a line to a Panel");
+      JLabel l = new JLabel(label);
       
       parent.add("0 " + row + " lhwHW", l);
       parent.add("1 " + row + " lhwHW", comp);
       
     }
 
+  void addRow(JPanel parent, Component comp,  String label, int row)
+    {
+       System.out.println("Adding a line to a JPanel");
+      JLabel l = new JLabel(label);
+      
+      parent.add("0 " + row + " lhwHW", l);
+      parent.add("1 " + row + " lhwHW", comp);
+      
+    }
 
+  void addRow(OpaqueJPanel parent, Component comp,  String label, int row)
+    {
+      System.out.println("Adding a line to a OpaqueJPanel");
+      JLabel l = new JLabel(label);
+      
+      parent.add("0 " + row + " lhwHW", l);
+      parent.add("1 " + row + " lhwHW", comp);
+      
+    }
+
+ void addRow(JComponent parent, Component comp,  String label, int row)
+    {
+      System.out.println("Adding a line to a JComponent");
+      JLabel l = new JLabel(label);
+      
+      parent.add("0 " + row + " lhwHW", l);
+      parent.add("1 " + row + " lhwHW", comp);
+      
+    }
+  
 
 }
+
+
