b26c0fa14664c4cc674463706e24979adf0cc163
Jonathan Abbey <jonabbey@arlut.utexas.edu>
1997-07-18 10:47:00 +0000

    Checking in. Mike.

--- src/JDataComponent/JcheckboxField.java
+++ src/JDataComponent/JcheckboxField.java
@@ -4,7 +4,7 @@
 
    
    Created: 12 Jul 1996
-   Version: $Revision: 1.1 $ %D%
+   Version: $Revision: 1.2 $ %D%
    Module By: Navin Manohar
    Applied Research Laboratories, The University of Texas at Austin
 
