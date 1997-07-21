fe3888c267f99efc7ba8f3c3d45e66c9057a628e
Jonathan Abbey <jonabbey@arlut.utexas.edu>
1997-07-21 18:37:14 +0000

    Added no-parmeter isLocked() method -- Jon

--- src/server/DBReadLock.java
+++ src/server/DBReadLock.java
@@ -6,7 +6,7 @@
    The GANYMEDE object storage system.
 
    Created: 2 July 1996
-   Version: $Revision: 1.5 $ %D%
+   Version: $Revision: 1.6 $ %D%
    Module By: Jonathan Abbey
    Applied Research Laboratories, The University of Texas at Austin
 
@@ -244,6 +244,18 @@ public class DBReadLock extends DBLock {
 	lockManager.notifyAll();
 	release();
       }
+  }
+
+  /**
+   *
+   * Returns true if the lock has been established and not
+   * yet aborted / released.
+   *
+   */
+
+  boolean isLocked()
+  {
+    return locked;
   }
 
   /**
--- src/server/DBWriteLock.java
+++ src/server/DBWriteLock.java
@@ -6,7 +6,7 @@
    The GANYMEDE object storage system.
 
    Created: 2 July 1996
-   Version: $Revision: 1.7 $ %D%
+   Version: $Revision: 1.8 $ %D%
    Module By: Jonathan Abbey
    Applied Research Laboratories, The University of Texas at Austin
 
@@ -390,6 +390,18 @@ public class DBWriteLock extends DBLock {
 	lockManager.notifyAll();
 	release();
       }
+  }
+
+  /**
+   *
+   * Returns true if the lock has been established and not
+   * yet aborted / released.
+   *
+   */
+
+  boolean isLocked()
+  {
+    return locked;
   }
 
   /**
