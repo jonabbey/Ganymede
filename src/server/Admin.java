feec3e554ff5f50a6af1f30dc27d78d77bfb59a7
Jonathan Abbey <jonabbey@arlut.utexas.edu>
1997-07-22 20:02:14 +0000

    Added getName() -- Jon

--- src/server/Admin.java
+++ src/server/Admin.java
@@ -9,7 +9,7 @@
    appropriate.
 
    Created: 28 May 1996
-   Version: $Revision: 1.2 $ %D%
+   Version: $Revision: 1.3 $ %D%
    Module By: Jonathan Abbey
    Applied Research Laboratories, The University of Texas at Austin
 
@@ -21,6 +21,7 @@ import java.rmi.*;
 import java.util.*;
 
 public interface Admin extends Remote {
+  String getName() throws RemoteException;
   String getPassword() throws RemoteException;
   void forceDisconnect(String reason) throws RemoteException;
 
