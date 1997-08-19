3326bd0a6b3262325b3960c1bbed86b5121fe56f
Michael Mulvaney <mulvaney@arlut.utexas.edu>
1997-08-19 13:07:36 +0000

    Initial revision

---
+++ src/server/CatTreeNode.java
@@ -0,0 +1,47 @@
+/*
+
+   CatTreeNode.java
+
+   Category tree node for GASHSchema
+   
+   Created: 14 August 1997
+   Version: $Revision: 1.1 $ %D%
+   Module By: Jonathan Abbey
+   Applied Research Laboratories, The University of Texas at Austin
+
+*/
+
+package arlut.csd.ganymede;
+
+import arlut.csd.Tree.*;
+import java.awt.*;
+
+/*------------------------------------------------------------------------------
+                                                                           class
+                                                                     CatTreeNode
+
+------------------------------------------------------------------------------*/
+
+class CatTreeNode extends arlut.csd.Tree.treeNode {
+
+  private Category category;
+
+  /* -- */
+
+  CatTreeNode(treeNode parent, String text, Category category, treeNode insertAfter,
+	      boolean expandable, int openImage, int closedImage, PopupMenu menu)
+  {
+    super(parent, text, insertAfter, expandable, openImage, closedImage, menu);
+    this.category = category;
+  }
+
+  public Category getCategory()
+  {
+    return category;
+  }
+
+  public void setCategory(Category category)
+  {
+    this.category = category;
+  }
+}
