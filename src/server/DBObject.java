1d80cc7b88218bebc789571cdc00dd2045f754fc
Jonathan Abbey <jonabbey@arlut.utexas.edu>
1997-09-02 17:26:51 +0000

    No change ? -- Jon

--- src/server/DBObject.java
+++ src/server/DBObject.java
@@ -6,7 +6,7 @@
    The GANYMEDE object storage system.
 
    Created: 2 July 1996
-   Version: $Revision: 1.22 $ %D%
+   Version: $Revision: 1.23 $ %D%
    Module By: Jonathan Abbey
    Applied Research Laboratories, The University of Texas at Austin
 
@@ -51,7 +51,7 @@ import arlut.csd.Util.*;
  * <p>The constructors of this object can throw RemoteException because of the
  * UnicastRemoteObject superclass' constructor.</p>
  *
- * @version $Revision: 1.22 $ %D% (Created 2 July 1996)
+ * @version $Revision: 1.23 $ %D% (Created 2 July 1996)
  * @author Jonathan Abbey, jonabbey@arlut.utexas.edu, ARL:UT
  *
  */
@@ -185,6 +185,10 @@ public class DBObject extends UnicastRemoteObject implements db_object, FieldTyp
 	  {
 	    field.setOwner(this); // this will make the field non-editable
 	    fields.put(key, field);
+	  }
+	else
+	  {
+	    Ganymede.debug("DBObject check-in: rejecting undefined field " + field.getName());
 	  }
       }
   }
