4c6d1e2948d7bf3eaef04b0961de40d7b39ce520
Jonathan Abbey <jonabbey@arlut.utexas.edu>
1997-09-04 12:36:38 +0000

    Added IP -- Jon

--- src/server/FieldType.java
+++ src/server/FieldType.java
@@ -5,7 +5,7 @@
    Hackified enumeration of defined field types
    
    Created: 15 April 1997
-   Version: $Revision: 1.3 $ %D%
+   Version: $Revision: 1.4 $ %D%
    Module By: Jonathan Abbey
    Applied Research Laboratories, The University of Texas at Austin
 
@@ -22,5 +22,6 @@ public interface FieldType {
   static final short INVID = 4;
   static final short PERMISSIONMATRIX = 5;
   static final short PASSWORD = 6;
-  static final short LASTFIELD = 6;
+  static final short IP = 7;
+  static final short LASTFIELD = 7;
 }
