8cad36f0761264ebe5a215436f479d155283a892
Jonathan Abbey <jonabbey@arlut.utexas.edu>
1997-09-10 14:19:18 +0000

    No change, I don't think -- Jon

--- src/server/InvidDBField.java
+++ src/server/InvidDBField.java
@@ -6,7 +6,7 @@
    The GANYMEDE object storage system.
 
    Created: 2 July 1996
-   Version: $Revision: 1.15 $ %D%
+   Version: $Revision: 1.16 $ %D%
    Module By: Jonathan Abbey
    Applied Research Laboratories, The University of Texas at Austin
 
