bc4643eb04d676e5668d8cb0cfa37a8485f008b0
Jonathan Abbey <jonabbey@arlut.utexas.edu>
1997-09-10 18:26:37 +0000

    Added   Invid createNewEmbedded() throws RemoteException; -- Jon

--- src/server/invid_field.java
+++ src/server/invid_field.java
@@ -5,7 +5,7 @@
    Remote interface definition.
 
    Created: 14 November 1996
-   Version: $Revision: 1.3 $ %D%
+   Version: $Revision: 1.4 $ %D%
    Module By: Jonathan Abbey
    Applied Research Laboratories, The University of Texas at Austin
 
@@ -19,6 +19,14 @@ public interface invid_field extends db_field {
   boolean limited() throws RemoteException;
   int getAllowedTarget() throws RemoteException;
 
+  // the following methods provided stringbuffer-encoded
+  // information on the values and choices present
+  // for this field.
+
   StringBuffer encodedValues() throws RemoteException;
   StringBuffer choices() throws RemoteException;
+
+  // the following methods apply if this is an edit-in-place vector
+
+  Invid createNewEmbedded() throws RemoteException;
 }
