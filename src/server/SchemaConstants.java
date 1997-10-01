2bcd24334a39d4f5d94d401ef7525b7800d50ee9
Jonathan Abbey <jonabbey@arlut.utexas.edu>
1997-10-01 20:17:49 +0000

    Moved to an owner group / admin persona / user architecture. -- Jon

--- src/server/SchemaConstants.java
+++ src/server/SchemaConstants.java
@@ -6,7 +6,7 @@
    types and fields.
    
    Created: 21 July 1997
-   Version: $Revision: 1.3 $ %D%
+   Version: $Revision: 1.4 $ %D%
    Module By: Jonathan Abbey
    Applied Research Laboratories, The University of Texas at Austin
 
@@ -44,32 +44,40 @@ public interface SchemaConstants {
 
   final static short ContainerField = 0; // Invid pointer to object 'containing' us.
 
-  /* administrators have a defined set of fields */
+  /* owner base */
 
-  final static short AdminBase = 0; // an administrator account
+  final static short OwnerBase = 0; // all objects are owned by objects of type OwnerBase
 
-  final static short AdminGroupField = 100; // boolean, true if this is an admin group 
-  final static short AdminNameField = 101; // name of this admin account, null if associated with a user
-  final static short AdminPasswordField = 102; // password for this admin account
-  final static short AdminMembersField = 103; // members of this admin account if an admin group
-  final static short AdminGroupsField = 104; // list of admin groups that this admin is a member of
-  final static short AdminAssocUser = 105; // if associated with a user, here's the reference.
-  final static short AdminPrivs = 106; // vector of permission invids for this admin account
-  final static short AdminObjectsOwned = 107; // list of objects owned by this admin account
+  final static short OwnerNameField = 100; // name of this owner set
+  final static short OwnerMembersField = 101; // what admin:role entities have privileges in this OwnerBase?
+  final static short OwnerObjectsOwned = 102; // what objects does this owner set own?
+
+  /* administrator roles have a defined set of fields */
+
+  final static short PersonaBase = 1; // an administrator privilege record
+
+  final static short PersonaNameField = 100; // what is this persona called?
+  final static short PersonaPasswordField = 101; // password for this admin account
+  final static short PersonaGroupsField = 102; // list of admin groups that this admin is a member of
+  final static short PersonaAssocUser = 103; // if associated with a user, here's the reference.
+  final static short PersonaPrivs = 104; // vector of permission invids for this admin account
+  final static short PersonaAdminConsole = 105; // boolean, does this role have access to the admin console?
+  final static short PersonaAdminPower = 106; // boolean, does this role have *full* access to the admin console?
 
   /* permission */
 
-  final static short PermBase = 1;
+  final static short PermBase = 2; // this base contains a set of permission bits constraining the admin personae's power
 
   final static short PermName = 100;
   final static short PermMatrix = 101;
+  final static short PermPersonae = 102; // what admin personae are using this priv matrix?
 
   /* users have a defined set of fields */
 
-  final static short UserBase = 2;
+  final static short UserBase = 3;
 
   final static short UserUserName = 100; // username string
   final static short UserPassword = 101; // password
-  final static short UserAdminRole = 102; // pointer to associated admin account, if any
+  final static short UserAdminPersonae = 102; // pointer to zero or more associated admin personae
 
 }
