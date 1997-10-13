8eb483ac9fafb29ef1c8c9a79d2cdd2471819b4a
Jonathan Abbey <jonabbey@arlut.utexas.edu>
1997-10-13 14:47:19 +0000

    Initial revision

---
+++ src/JDataComponent/JIPField.java
@@ -0,0 +1,424 @@
+/*
+
+   JIPField.java
+
+   An IPv4/IPv6 data display / entry widget for Ganymede
+   
+   Created: 13 October 1997
+   Version: $Revision: 1.1 $ %D%
+   Module By: Jonathan Abbey
+   Applied Research Laboratories, The University of Texas at Austin
+
+*/
+
+package arlut.csd.JDataComponent;
+
+import java.awt.*;
+import java.awt.event.*;
+
+import com.sun.java.swing.*;
+import com.sun.java.swing.text.*;
+
+/*------------------------------------------------------------------------------
+                                                                           class
+                                                                        JIPField
+
+------------------------------------------------------------------------------*/
+
+/**
+ *
+ * This class is an IPv4/IPv6 data display/entry widget for Ganymede.  Its purpose
+ * is to allow the viewing and editing of either 4 or 16 byte Internet addresses
+ * and subnet masks.
+ *
+ */
+
+public class JIPField extends JentryField {
+
+  public static final boolean debug = false;
+
+  public static int DEFAULT_COLS = 15;
+  public static int DEFAULT_SIZE = 4096;
+  
+  private int size = DEFAULT_SIZE;
+
+  private String value = null;
+  private String allowedChars = null;
+  private String disallowedChars = null;
+
+  /**  Constructors ***/
+
+ /**
+   * Base constructor for JIPField
+   * 
+   * @param columns number of colums in the JIPField
+   * @param valueAttr used to determine the foregoudn/background/font for this JIPField
+   * @param is_editable true if this JIPField is editable
+   */
+  public JIPField(JcomponentAttr valueAttr,
+		  boolean is_editable)
+  {
+    super(DEFAULT_COLS);
+    
+    if (valueAttr == null)
+      {
+	throw new IllegalArgumentException("Invalid Paramter: valueAttr is null");
+      }
+
+    this.valueAttr = valueAttr;
+    
+    //    setText(null);
+
+    setEditable(is_editable);  // will this JIPField be editable or not?
+
+    setEnabled(true);
+
+    JcomponentAttr.setAttr(this,valueAttr);
+
+    enableEvents(AWTEvent.FOCUS_EVENT_MASK);
+    enableEvents(AWTEvent.KEY_EVENT_MASK); 
+  }
+  
+  /**
+   * Constructor which uses default fonts,no parent,
+   * default column size, and default foregound/background
+   * colors.
+   */
+
+  public JIPField()
+  {
+    this(new JcomponentAttr(null,new Font("Courier",Font.PLAIN,12),
+			    Color.black,Color.white),
+	 true);
+  }
+
+  /**
+    * Constructor that allows for the creation of a JIPField
+    * that knows about its parent.
+    *
+    * @param cols number of colums in the JIPField
+    * @param valueAttr used to determine the foregoudn/background/font for this JIPField
+    * @param parent the container within which this JIPField is contained
+    *        (This container will implement an interface that will utilize the
+    *         data contained within this JIPField.)
+    *
+    */
+
+  public JIPField(JcomponentAttr valueAttr,
+		  boolean is_editable,
+		  JsetValueCallback callback)
+  {
+    this(valueAttr,is_editable);
+
+    setCallback(callback);
+  }
+  
+ /************************************************************/
+ // JIPField methods
+
+  /**
+   *  sets the JIPField to a specific value
+   *
+   * @param str value to which the JIPField is set
+   */
+
+  public void setText(String str)
+  {
+    if (str == null) 
+      {
+	value = new String("");
+	
+	super.setText("");
+      
+	changed = true;
+      }
+    else 
+      {
+	if (str.length() > size)
+	  {
+	    throw new IllegalArgumentException("string too long");
+	  }
+
+	for (int i = 0; i < str.length(); i++)
+	  {
+	    if (!isAllowed(str.charAt(i)))
+	      {
+		throw new IllegalArgumentException("invalid char in string: " + 
+						   str.charAt(i));
+	      }
+	  }
+
+	super.setText(str); 
+
+	value = str;
+	
+	changed = true;
+      }
+  }
+
+  /**
+   *
+   *  returns the value of the member variable value
+   *
+   */
+
+  public String getValue() 
+  {
+    return value;
+  }
+
+  /**
+   *  returns the character located at position n in the JIPField value
+   *
+   * @param n position in the JIPField value from which to retrieve character
+   */
+
+  public char getCharAt(int n)
+  {
+    return this.getText().charAt(n);
+  }
+
+  /**
+   *  assigns a set of characters which are valid within the JIPField
+   *
+   * @param s each character in this string will be considered an allowed character
+   */
+
+  public void setAllowedChars(String s)
+  {
+     if (s != null)
+       {
+ 	this.allowedChars = s;
+       }
+     else 
+       {
+ 	this.allowedChars = null;
+       }
+
+     if (s == null || s.equals(""))
+       {
+ 	return;
+       }
+     else
+       {
+	 //	 Keymap map = getKeymap();
+	 //	
+	 //	 Action insert = new DefaultAction(JTextComponent.insertContentAction);
+	 //
+	 //	 KeyStroke[] strokes = map.getKeyStrokesForAction(insert);
+	 //
+	 //	 map.removeBindings();
+	 //	 
+	 //	 for (int i = 0; i < s.length(); i++)
+	 //	   {
+	 //	     map.addActionForKeyStroke(KeyStroke.getKeyStroke(s.charAt(i), 0), insert);
+	 //	   }
+       }
+  }
+ 
+  /**
+   *  assigns a set of characters which are invalid within the JIPField
+   *
+   * @param s each character in this string will be considered a disallowed character
+   */
+  public void setDisallowedChars(String s)
+  {
+    //    Keymap map;
+
+    /* -- */
+
+    //    map = getKeymap();
+
+    if (s != null && !s.equals(""))
+      {
+	// 	for (int i = 0; i < s.length(); i++)
+	// 	  {
+	//	    //	    map.removeKeyStrokeBinding(KeyStroke.getKeyStroke(s.charAt(i), 0));
+	//	    map.addActionForKeyStroke(KeyStroke.getKeyStroke(s.charAt(i)),
+	//			      new DefaultAction("null action"));
+	// 	  }
+      }
+
+    if (s!= null)
+      {
+ 	this.disallowedChars = s;
+      }
+    else 
+      {
+ 	this.disallowedChars = null;
+      }
+  }
+
+  /**
+   *   returns the set of allowed characters as a String object
+   */
+
+  public String getAllowedChars()
+  {
+    return this.allowedChars;
+  }
+
+  /**
+   *  returns the set of disallowed characters as a String object
+   */
+
+  public String getDisallowedChars()
+  {
+    return this.disallowedChars;
+  }
+
+  /**
+   * returns the maximum size of the string that can be placed in this JIPField
+   */
+
+  public int getMaxStringSize()
+  {
+    return this.size;
+  }
+
+
+  /**
+   *  determines whether a given character is valid or invalid for a JIPField
+   *
+   * @param ch the character which is being tested for its validity
+   */
+
+  private boolean isAllowed(char ch)
+  {
+    if (disallowedChars != null)
+      {
+	if (disallowedChars.indexOf(ch) != -1)
+	  {
+	    return false;
+	  }
+      }
+    
+    if (allowedChars != null)
+
+      {
+	if (allowedChars.indexOf(ch) == -1)
+	  {
+	    return false;
+	  }
+      }
+    
+    return true;
+  }
+
+  /**
+   * When the JIPField looses focus, any changes made to 
+   * the value in the JIPField need to be propogated to the
+   * server.  This method will handle that functionality.
+   *
+   * @param e the FocusEvent that needs to be process
+   */
+
+  public void processFocusEvent(FocusEvent e)
+  {
+    String str;
+
+    /* -- */
+
+    super.processFocusEvent(e);
+
+    if (debug)
+      {
+	System.err.println("JIPField.processFocusEvent: entering");
+      }
+
+    if (e.getID() == FocusEvent.FOCUS_LOST)
+      {
+	// if nothing in the JIPField has changed,
+	// we don't need to worry about this event.
+
+	str = getText();
+
+	if (value != null)
+	  {
+	    if (debug)
+	      {
+		System.err.println("JIPField.processFocusEvent: old value != null");
+	      }
+
+	    changed = !value.equals(str);
+	  }
+	else
+	  {
+	    if (debug)
+	      {
+		System.err.println("JIPField.processFocusEvent: old value == null");
+	      }
+
+	    changed = true;
+	  }
+
+	if (!changed)
+	  {
+	    if (debug)
+	      {
+		System.err.println("JIPField.processFocusEvent: no change, ignoring");
+	      }
+
+	    return;
+	  }
+	
+	if (allowCallback) 
+	  {
+	    boolean b = false;
+	  
+	    try 
+	      {
+		if (debug)
+		  {
+		    System.err.println("JIPField.processFocusEvent: making callback");
+		  }
+
+		b = my_parent.setValuePerformed(new JValueObject(this, str));
+	      }
+	    catch (RemoteException re)
+	      {
+	      }
+	    
+	    if (b==false) 
+	      {
+		if (debug)
+		  {
+		    System.err.println("JIPField.processFocusEvent: setValue rejected");
+
+		    if (value == null)
+		      {
+			System.err.println("JIPField.processFocusEvent: resetting to empty string");
+		      }
+		    else
+		      {
+			System.err.println("JIPField.processFocusEvent: resetting to " + value);
+		      }
+		  }
+		
+		if (value == null)
+		  {
+		    super.setText("");
+		  }
+		else
+		  {
+		    super.setText(value);
+		  }
+
+		changed = false;
+	      }
+	    else 
+	      {
+		if (debug)
+		  {
+		    System.err.println("JIPField.processFocusEvent: setValue accepted");
+		  }
+
+		value = str;
+		
+		changed = true;
+	      }
+	  }
+      }
+  }
+
+}
