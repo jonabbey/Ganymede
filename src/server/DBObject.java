177df842cb9a908a8325d6b1975f5ef203af35ca
Jonathan Abbey <jonabbey@arlut.utexas.edu>
1997-10-16 12:25:02 +0000

    Added support for reading in IP fields in receive(). -- Jon

--- src/server/DBObject.java
+++ src/server/DBObject.java
@@ -6,7 +6,7 @@
    The GANYMEDE object storage system.
 
    Created: 2 July 1996
-   Version: $Revision: 1.28 $ %D%
+   Version: $Revision: 1.29 $ %D%
    Module By: Jonathan Abbey
    Applied Research Laboratories, The University of Texas at Austin
 
@@ -51,7 +51,7 @@ import arlut.csd.Util.*;
  * <p>The constructors of this object can throw RemoteException because of the
  * UnicastRemoteObject superclass' constructor.</p>
  *
- * @version $Revision: 1.28 $ %D% (Created 2 July 1996)
+ * @version $Revision: 1.29 $ %D% (Created 2 July 1996)
  * @author Jonathan Abbey, jonabbey@arlut.utexas.edu, ARL:UT
  *
  */
@@ -438,6 +438,9 @@ public class DBObject extends UnicastRemoteObject implements db_object, FieldTyp
 	    tmp = new PasswordDBField(this, in, definition);
 	    break;
 
+	  case IP:
+	    tmp = new IPDBField(this, in, definition);
+	    break;
 	  }
 
 	if (tmp == null)
