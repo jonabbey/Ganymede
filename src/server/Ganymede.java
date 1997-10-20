f254d39ed157e73ceff61adc846d412848bb59e2
Jonathan Abbey <jonabbey@arlut.utexas.edu>
1997-10-20 18:46:11 +0000

    Added directInit() to allow the Ganymede server to be melded with the directLoader code. -- Jon

--- src/server/Ganymede.java
+++ src/server/Ganymede.java
@@ -5,7 +5,7 @@
    Server main module
 
    Created: 17 January 1997
-   Version: $Revision: 1.14 $ %D%
+   Version: $Revision: 1.15 $ %D%
    Module By: Jonathan Abbey
    Applied Research Laboratories, The University of Texas at Austin
 
@@ -164,6 +164,86 @@ public class Ganymede {
       {
 	debug("Setup and bound server object OK");
       }
+  }
+
+  public static GanymedeServer directInit(String dbFilename) 
+  {
+    File dataFile;
+
+    /* -- */
+
+    Ganymede.dbFilename = dbFilename;
+
+    boolean stop = true;
+
+    debug("Creating DBStore structures");
+
+    db = new DBStore();
+
+    dataFile = new File(dbFilename);
+    
+    if (dataFile.exists())
+      {
+	debug("Loading DBStore contents");
+	db.load(dbFilename);
+      }
+    else
+      {
+	debug("No DBStore exists under filename " + dbFilename + ", not loading");
+	debug("Initializing new schema");
+	db.initializeSchema();
+	debug("Template schema created.");
+
+	try 
+	  {
+	    db.journal = new DBJournal(db, GanymedeConfig.journal);
+	  }
+	catch (IOException ex)
+	  {
+	    // what do we really want to do here?
+	    
+	    throw new RuntimeException("couldn't initialize journal");
+	  }
+
+	debug("Creating supergash object");
+	db.initializeObjects();
+	debug("supergash object created");
+      }
+
+    debug("Initializing Security Manager");
+
+    // Create a Server object
+
+    try
+      {
+	debug("Creating GanymedeServer object");
+
+	server = new GanymedeServer(10);
+
+	debug("Binding GanymedeServer in RMI Registry");
+      }
+    catch (Exception ex)
+      {
+	debug("Couldn't establish server binding: " + ex);
+	return null;
+      }
+
+    try
+      {
+	debug("Creating internal Ganymede Session");
+	internalSession = new GanymedeSession();
+      }
+    catch (RemoteException ex)
+      {
+	throw new RuntimeException("Couldn't establish internal session: " + ex);
+      }
+
+    if (debug)
+      {
+	debug("Setup and bound server object OK");
+      }
+
+    return server;
   }
 
   // debug routine
