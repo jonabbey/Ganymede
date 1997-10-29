6cc10772c51b7223340eb062507bc7b0c2a595a2
Jonathan Abbey <jonabbey@arlut.utexas.edu>
1997-10-29 12:10:56 +0000

    Various things to optimize the OwnerGroup creation. -- Jon

--- src/schemas/gash/loader/source/OwnerGroup.java
+++ src/schemas/gash/loader/source/OwnerGroup.java
@@ -6,7 +6,7 @@
    GASH user_info file
    
    Created: 30 September 1997
-   Version: $Revision: 1.1 $ %D%
+   Version: $Revision: 1.2 $ %D%
    Module By: Jonathan Abbey
    Applied Research Laboratories, The University of Texas at Austin
 
@@ -59,13 +59,50 @@ public class OwnerGroup {
     admins = new Vector();
   }
 
+  public OwnerGroup(Admin entry)
+  {
+    this(entry.lowuid, entry.highuid, entry.lowgid, entry.highgid, entry.mask);
+  }
+
   public boolean equals(int lowuid, int highuid, int lowgid, int highgid, String prefix)
   {
+    String temp;
+
+    /* -- */
+
+    if (prefix == null)
+      {
+	temp = "";
+      }
+    else
+      {
+	if (prefix.indexOf('*') != -1)
+	  {
+	    if (prefix.length() > 1)
+	      {
+		temp = prefix.substring(0, prefix.length() - 1);
+	      }
+	    else
+	      {
+		temp = "";
+	      }
+	  }
+	else
+	  {
+	    temp = prefix;
+	  }
+      }
+
     return (lowuid == this.lowuid &&
 	    highuid == this.highuid &&
 	    lowgid == this.lowgid &&
 	    highgid == this.highgid &&
-	    prefix.equals(this.prefix));
+	    temp.equals(this.prefix));
+  }
+
+  public boolean compatible(Admin entry)
+  {
+    return this.equals(entry.lowuid, entry.highuid, entry.lowgid, entry.highgid, entry.mask);
   }
 
   public synchronized void addAdmin(String adminName, String password)
@@ -105,6 +142,23 @@ public class OwnerGroup {
     return false;
   }
 
+  public synchronized String password(String name)
+  {
+    adminRec a;
+
+    for (int i = 0; i < admins.size(); i++)
+      {
+	a = (adminRec) admins.elementAt(i);
+
+	if (a.adminName.equals(name))
+	  {
+	    return a.password;
+	  }
+      }
+
+    throw new IllegalArgumentException(name + " is not an admin in this group!");
+  }
+
   public void setInvid(Invid inv)
   {
     this.objInvid = inv;
@@ -114,6 +168,28 @@ public class OwnerGroup {
   {
     return objInvid;
   }
+
+  public String toString()
+  {
+    String result = objInvid == null ? "null " : (objInvid.toString() + " ");
+    result += "[" + lowuid + "," + highuid + "], [" + lowgid + "," + highgid + "], [" + prefix + "], (";
+
+    for (int i = 0; i < admins.size(); i++)
+      {
+	if (i > 0)
+	  {
+	    result += ", ";
+	  }
+
+	result += admins.elementAt(i);
+      }
+
+    result += ")";
+
+    return result;
+  }
+
+
 
 }
 
@@ -126,6 +202,11 @@ class adminRec {
   {
     this.adminName = name;
     this.password = pass;
+  }
+
+  public String toString()
+  {
+    return adminName;
   }
 
 }
