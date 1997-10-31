9d7decb0b1d9d081e7a881a4f8a6a31cbbe2124d
Jonathan Abbey <jonabbey@arlut.utexas.edu>
1997-10-31 13:48:42 +0000

    Should be no changes. -- Jon

--- src/server/PermMatrix.java
+++ src/server/PermMatrix.java
@@ -6,7 +6,7 @@
    object type and field id's.
    
    Created: 3 October 1997
-   Version: $Revision: 1.3 $ %D%
+   Version: $Revision: 1.4 $ %D%
    Module By: Jonathan Abbey
    Applied Research Laboratories, The University of Texas at Austin
 
