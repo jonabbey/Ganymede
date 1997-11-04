b28984fee5d9a47a9c16ba198605d2d7e7cf653d
Jonathan Abbey <jonabbey@arlut.utexas.edu>
1997-11-04 16:50:40 +0000

    Modified choicesKey() to simply return null if owner is not a DBEditObject. -- Jon

--- src/server/StringDBField.java
+++ src/server/StringDBField.java
@@ -6,7 +6,7 @@
    The GANYMEDE object storage system.
 
    Created: 2 July 1996
-   Version: $Revision: 1.14 $ %D%
+   Version: $Revision: 1.15 $ %D%
    Module By: Jonathan Abbey
    Applied Research Laboratories, The University of Texas at Austin
 
@@ -429,7 +429,14 @@ public class StringDBField extends DBField implements string_field {
 
   public Object choicesKey()
   {
-    return ((DBEditObject) owner).obtainChoicesKey(this);
+    if (owner instanceof DBEditObject)
+      {
+	return ((DBEditObject) owner).obtainChoicesKey(this);
+      }
+    else
+      {
+	return null;
+      }
   }
 
   /**
