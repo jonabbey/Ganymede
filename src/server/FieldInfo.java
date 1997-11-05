d988c96e8e9ee67a954ad230122100d9b345b65f
Jonathan Abbey <jonabbey@arlut.utexas.edu>
1997-11-05 12:48:11 +0000

    Converted FieldInfo to only carry the dynamically updated field information. -- Jon

--- src/server/FieldInfo.java
+++ src/server/FieldInfo.java
@@ -6,7 +6,7 @@
    the container panel needs to render a field.
    
    Created: 4 November 1997
-   Version: $Revision: 1.1 $ %D%
+   Version: $Revision: 1.2 $ %D%
    Module By: Jonathan Abbey
    Applied Research Laboratories, The University of Texas at Austin
 
@@ -22,51 +22,35 @@ package arlut.csd.ganymede;
 
 public class FieldInfo implements java.io.Serializable {
 
-  short type;
-  String name;
-  short field_code;
-  String comment;
+  db_field
+    field;
 
   boolean
     defined,
     editable,
-    visible,
-    builtIn,
-    editInPlace,
-    vector;
+    visible;
+
+  Object
+    value;
+
+  /* -- */
 
   public FieldInfo(DBField field)
   {
-    field_code = field.getID();
-    name = field.getName();
-    type = field.getType();
-    comment = field.getComment();
+    this.field = (db_field) field;
+
+    if (!field.isVector())
+      {
+	value = field.getValue();
+      }
+    else
+      {
+	value = field.getValues();
+      }
+
     defined = field.isDefined();
     editable = field.isEditable();
     visible = field.isVisible();
-    builtIn = field.isBuiltIn();
-    editInPlace = field.isEditInPlace();
-    vector = field.isVector();
-  }
-
-  public short getFieldID()
-  {
-    return field_code;
-  }
-
-  public String getName()
-  {
-    return name;
-  }
-
-  public short getType()
-  {
-    return type;
-  }
-
-  public String getComment()
-  {
-    return comment;
   }
 
   public boolean isDefined()
@@ -83,20 +67,4 @@ public class FieldInfo implements java.io.Serializable {
   {
     return visible;
   }
-
-  public boolean isBuiltIn()
-  {
-    return builtIn;
-  }
-
-  public boolean isEditInPlace()
-  {
-    return editInPlace;
-  }
-
-  public boolean isVector()
-  {
-    return vector;
-  }
-
 }
