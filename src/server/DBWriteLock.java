5732b0d6fa16d318473587d8c84c1daf47c43426
Jonathan Abbey <jonabbey@arlut.utexas.edu>
1997-11-20 18:21:07 +0000

    Made inEstablish not be private, so that DBEditSet can check this flag to see whether it is safe to abort and disregard an old lock. -- Jon

--- src/server/DBWriteLock.java
+++ src/server/DBWriteLock.java
@@ -6,7 +6,7 @@
    The GANYMEDE object storage system.
 
    Created: 2 July 1996
-   Version: $Revision: 1.11 $ %D%
+   Version: $Revision: 1.12 $ %D%
    Module By: Jonathan Abbey
    Applied Research Laboratories, The University of Texas at Austin
 
@@ -51,8 +51,9 @@ public class DBWriteLock extends DBLock {
   private Vector baseSet;
   private boolean 
     locked = false,
-    abort = false,
-    inEstablish = false;
+    abort = false;
+
+  boolean inEstablish = false;
 
   /* -- */
 
