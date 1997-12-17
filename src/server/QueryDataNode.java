5a31762eec32d0f5e8325639dfc8e48303185e5a
Jonathan Abbey <jonabbey@arlut.utexas.edu>
1997-12-17 16:02:37 +0000

    Added vector ops, new constructors to pass 'em in. -- Jon

--- src/server/QueryDataNode.java
+++ src/server/QueryDataNode.java
@@ -3,7 +3,7 @@
    QueryDataNode.java
 
    Created: 10 July 1997
-   Version: $Revision: 1.4 $ %D%
+   Version: $Revision: 1.5 $ %D%
    Module By: Jonathan Abbey
    Applied Research Laboratories, The University of Texas at Austin
 
@@ -32,14 +32,56 @@ public class QueryDataNode extends QueryNode {
   static public final byte UNDEFINED = 9;
   static public final byte LAST = 9;
 
+  static public final byte FIRSTVECOP = 0;
+  
+  static public final byte NONE = 0;
+  static public final byte CONTAINSANY = 1;
+  static public final byte CONTAINSALL = 2;
+  static public final byte CONTAINSNONE = 3;
+  static public final byte LENGTHEQ = 4;
+  static public final byte LENGTHGR = 5;
+  static public final byte LENGTHLE = 6;
+
+  static public final byte LASTVECOP = 6;
+  
   /* - */
 
   String fieldname;
   short fieldId;
   byte comparator;
+  byte arrayOp;
   Object value;
 
   /* -- */
+
+  /**
+   * Field comparison node constructor.
+   *
+   * This constructor creates a query node that will be matched
+   * against a field in an object.
+   *
+   */
+
+  public QueryDataNode(String fieldname, byte comparator, byte vecOp, Object value)
+  {
+    this.fieldname = fieldname;
+    this.fieldId = -1;
+
+    if (comparator < FIRST || comparator > LAST)
+      {
+	throw new IllegalArgumentException("bad comparator value: " + comparator);
+      }
+
+    this.comparator = comparator;
+    this.value = value;
+
+    if (vecOp < FIRSTVECOP || vecOp > LASTVECOP)
+      {
+	throw new IllegalArgumentException("bad vector operator value: " + vecOp);
+      }
+
+    this.arrayOp = vecOp;
+  }
 
   /**
    * Field comparison node constructor.
@@ -61,6 +103,43 @@ public class QueryDataNode extends QueryNode {
 
     this.comparator = comparator;
     this.value = value;
+
+    this.arrayOp = NONE;
+  }
+
+  /**
+   * Field comparison node constructor.
+   *
+   * This constructor creates a query node that will be matched
+   * against a field in an object.
+   *
+   * If fieldID == -1, the labels of objects in the database will be
+   * taken as the field for comparison's sake.
+   *
+   * If fieldID == -2, the Invid of objects in the database will be
+   * taken as the field for comparison's sake.
+   * 
+   */
+
+  public QueryDataNode(short fieldId, byte comparator, byte vecOp, Object value)
+  {
+    this.fieldname = null;
+    this.fieldId = fieldId;
+
+    if (comparator < FIRST || comparator > LAST)
+      {
+	throw new IllegalArgumentException("bad comparator value: " + comparator);
+      }
+
+    this.comparator = comparator;
+    this.value = value;
+
+    if (vecOp < FIRSTVECOP || vecOp > LASTVECOP)
+      {
+	throw new IllegalArgumentException("bad vector operator value: " + vecOp);
+      }
+
+    this.arrayOp = vecOp;
   }
 
   /**
@@ -89,6 +168,8 @@ public class QueryDataNode extends QueryNode {
 
     this.comparator = comparator;
     this.value = value;
+
+    this.arrayOp = NONE;
   }
 
   /**
@@ -111,8 +192,9 @@ public class QueryDataNode extends QueryNode {
 
     this.comparator = comparator;
     this.value = value;
-  }
 
+    this.arrayOp = NONE;
+  }
 
   /** 
    * This method dumps the value of the data node to a string in the form of:
@@ -122,76 +204,75 @@ public class QueryDataNode extends QueryNode {
    */
   
   public String dumpToString ()
-    {
+  {
+    String operator, returnVal;
 
-      String operator, returnVal;
+    /* -- */
 
-      // The following is designed to make the query string
-      // more readable by using the operator instad of its
-      // mapped numeric value
+    // The following is designed to make the query string
+    // more readable by using the operator instad of its
+    // mapped numeric value
 
-      if (this.comparator == 1)
-	{
-	  operator = "=";
-	}
-      else if (this.comparator == 2)
-	{
-	  operator = "<"; 
-	} 
-      else if (this.comparator == 3)
-	  {
-	    operator = "<=";
-	  }
-      else if (this.comparator == 4)
-	{
-	  operator = ">";
-	}
-      else if (this.comparator == 5)
-	{
-	  operator = ">=";
-	}
-      else
-	{
-	  operator = "Error: Operator undefined";
-	}
+    if (this.comparator == 1)
+      {
+	operator = "=";
+      }
+    else if (this.comparator == 2)
+      {
+	operator = "<"; 
+      } 
+    else if (this.comparator == 3)
+      {
+	operator = "<=";
+      }
+    else if (this.comparator == 4)
+      {
+	operator = ">";
+      }
+    else if (this.comparator == 5)
+      {
+	operator = ">=";
+      }
+    else
+      {
+	operator = "Error: Operator undefined";
+      }
+    
+    returnVal = "(" + operator;
 
-
-      returnVal = "(" + operator;
-
-      // add fieldname
+    // add fieldname
+    
+    if (fieldname != null)
+      {
+	returnVal = returnVal + "(fieldname " + fieldname + ")"; 
+      }
+    else if (fieldId != -1)
+      {
+	returnVal = returnVal + "(fieldname #" + fieldId +")"; 
+      }
+    else
+      {
+	returnVal = returnVal + "(fieldname null)";
+      }
       
-      if (fieldname != null)
-	{
-	  returnVal = returnVal + "(fieldname " + fieldname + ")"; 
-	}
-      else if (fieldId != -1)
-	{
-	  returnVal = returnVal + "(fieldname #" + fieldId +")"; 
-	}
-      else
-	{
-	  returnVal = returnVal + "(fieldname null)";
-	}
+    // add value
       
-      // add value
-      
-      if (this.value == null)
-	{
-	  System.err.println("Error: Value undefined in QueryDataNode");
-	  returnVal = returnVal + "(value undefined)";
-	}
-      else
-	{
-      returnVal = returnVal + "(value " + this.value + ")";
-	}
-      
-      // close parenthesis
-
-      returnVal = returnVal + ")";
-      
-      // all done
-
-      return returnVal;
-      
-    }
+    if (this.value == null)
+      {
+	System.err.println("Error: Value undefined in QueryDataNode");
+	returnVal = returnVal + "(value undefined)";
+      }
+    else
+      {
+	returnVal = returnVal + "(value " + this.value + ")";
+      }
+    
+    // close parenthesis
+    
+    returnVal = returnVal + ")";
+    
+    // all done
+    
+    return returnVal;
+  }
 }
