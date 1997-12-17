762fb5bd1bb3d7bfe26cece393b1629699f3338b
Erik Grostic <butcher@arlut.utexas.edu>
1997-12-17 15:06:48 +0000

    Added dumpToString method - Erik

--- src/server/QueryNotNode.java
+++ src/server/QueryNotNode.java
@@ -3,7 +3,7 @@
    QueryNotNode.java
 
    Created: 10 July 1997
-   Version: $Revision: 1.1 $ %D%
+   Version: $Revision: 1.2 $ %D%
    Module By: Jonathan Abbey
    Applied Research Laboratories, The University of Texas at Austin
 
@@ -27,4 +27,23 @@ public class QueryNotNode extends QueryNode {
   {
     this.child = child;
   }
+
+  
+  /**
+   *
+   * This method returns a string in the form of:
+   * 
+   * not("value child 1")
+   *
+   */
+  
+  public String dumpToString()
+    {
+      System.out.println("NOT DUMP");
+      
+
+      String returnVal = ("not(" + child.dumpToString());
+      return returnVal;
+    }
+  
 }
