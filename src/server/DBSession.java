295e32294580c31c8690bfa5ab864f3ca18c8090
Jonathan Abbey <jonabbey@arlut.utexas.edu>
1998-01-22 20:40:55 +0000

    Added a releaseAllReadLocks() to commitTransaction().  This is so that the server can't get hung up if the client tried to initiate a transaction commit before running queries completed. -- Jon

--- src/server/DBSession.java
+++ src/server/DBSession.java
@@ -5,7 +5,7 @@
    The GANYMEDE object storage system.
 
    Created: 26 August 1996
-   Version: $Revision: 1.27 $ %D%
+   Version: $Revision: 1.28 $ %D%
    Module By: Jonathan Abbey
    Applied Research Laboratories, The University of Texas at Austin
 
@@ -671,6 +671,8 @@ final public class DBSession {
       {
 	throw new RuntimeException(key + ": commitTransaction called outside of a transaction");
       }
+
+    releaseAllReadLocks();
 
     while (lockVect.size() != 0)
       {
