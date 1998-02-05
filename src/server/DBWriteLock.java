cd8fa857b949f741b4d79407e9284aae1f890b8c
Jonathan Abbey <jonabbey@arlut.utexas.edu>
1998-02-05 15:06:51 +0000

    Turned off debug mode. -- Jon

--- src/server/DBWriteLock.java
+++ src/server/DBWriteLock.java
@@ -6,7 +6,7 @@
    The GANYMEDE object storage system.
 
    Created: 2 July 1996
-   Version: $Revision: 1.13 $ %D%
+   Version: $Revision: 1.14 $ %D%
    Module By: Jonathan Abbey
    Applied Research Laboratories, The University of Texas at Austin
 
@@ -44,7 +44,7 @@ import java.util.*;
 
 public class DBWriteLock extends DBLock {
 
-  static final boolean debug = true;
+  static final boolean debug = false;
 
   private Object key;
   private DBStore lockManager;
