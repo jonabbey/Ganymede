c19311d754b416f01c64fe989dbb0f6c20589168
Jonathan Abbey <jonabbey@arlut.utexas.edu>
1998-02-09 14:17:01 +0000

    Added getFieldValueLocal(), willBeRemoved(), and added selectivity to the isInactivated() method. -- Jon

--- src/server/DBObject.java
+++ src/server/DBObject.java
@@ -6,7 +6,7 @@
    The GANYMEDE object storage system.
 
    Created: 2 July 1996
-   Version: $Revision: 1.42 $ %D%
+   Version: $Revision: 1.43 $ %D%
    Module By: Jonathan Abbey
    Applied Research Laboratories, The University of Texas at Austin
 
@@ -53,7 +53,7 @@ import arlut.csd.JDialog.*;
  * <p>The constructors of this object can throw RemoteException because of the
  * UnicastRemoteObject superclass' constructor.</p>
  *
- * @version $Revision: 1.42 $ %D% (Created 2 July 1996)
+ * @version $Revision: 1.43 $ %D% (Created 2 July 1996)
  * @author Jonathan Abbey, jonabbey@arlut.utexas.edu, ARL:UT
  *
  */
@@ -1198,7 +1198,7 @@ public class DBObject extends UnicastRemoteObject implements db_object, FieldTyp
 
   public boolean isInactivated()
   {
-    return (getField(SchemaConstants.RemovalField) != null);
+    return (objectBase.canInactivate() && (getFieldValueLocal(SchemaConstants.RemovalField) != null));
   }
 
   /**
@@ -1230,7 +1230,19 @@ public class DBObject extends UnicastRemoteObject implements db_object, FieldTyp
 
   public boolean willExpire()
   {
-    return (getField(SchemaConstants.ExpirationField) != null);
+    return (getFieldValueLocal(SchemaConstants.ExpirationField) != null);
+  }
+
+  /**
+   *
+   * <p>Returns true if this object has a removal date set.</p>
+   *
+   * @see arlut.csd.ganymede.db_object
+   */
+
+  public boolean willBeRemoved()
+  {
+    return (getFieldValueLocal(SchemaConstants.RemovalField) != null);
   }
 
   /**
@@ -1285,10 +1297,10 @@ public class DBObject extends UnicastRemoteObject implements db_object, FieldTyp
     
     if (f == null)
       {
-	if (gSession != null)
-	  {
-	    gSession.setLastError("couldn't find field " + fieldID);
-	  }
+	//	if (gSession != null)
+	//	  {
+	//	    gSession.setLastError("couldn't find field " + fieldID);
+	//	  }
 
 	return null;
       }
@@ -1304,6 +1316,40 @@ public class DBObject extends UnicastRemoteObject implements db_object, FieldTyp
       }
 
     return f.getValue();
+  }
+
+  /**
+   *
+   * Shortcut method to get a field's value.  Used only
+   * on the server, as permissions are not checked.
+   *
+   */
+
+  public Object getFieldValueLocal(short fieldID)
+  {
+    DBField f = (DBField) getField(fieldID);
+    
+    if (f == null)
+      {
+	//	if (gSession != null)
+	//	  {
+	//	    gSession.setLastError("couldn't find field " + fieldID);
+	//	  }
+
+	return null;
+      }
+
+    if (f.isVector())
+      {
+	if (gSession != null)
+	  {
+	    gSession.setLastError("couldn't get scalar value on vector field " + fieldID);
+	  }
+
+	return null;
+      }
+
+    return f.getValueLocal();
   }
 
   /**
