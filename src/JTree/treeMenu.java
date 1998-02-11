442b3151ce781ab30da6b5f7cd21b08a1d301d18
Jonathan Abbey <jonabbey@arlut.utexas.edu>
1998-02-11 16:55:18 +0000

    Changed registerItems() to return a boolean so that the treeControl can be more selective about what it adds to its canvas. -- Jon

--- src/JTree/treeMenu.java
+++ src/JTree/treeMenu.java
@@ -7,7 +7,7 @@
    by the tree.
    
    Created: 5 September 1997
-   Version: $Revision: 1.1 $ %D%
+   Version: $Revision: 1.2 $ %D%
    Module By: Jonathan Abbey
    Applied Research Laboratories, The University of Texas at Austin
 
@@ -40,7 +40,20 @@ public class treeMenu extends PopupMenu {
     super(label);
   }
 
-  public void registerItems(treeControl listener)
+  /**
+   * This method registers all menu items in this menu with the
+   * appropriate listener.  If this menu has already been registered
+   * with anything, it won't add items to the listener.<p>
+   *
+   * This means that a treeMenu cannot be used in multiple trees
+   * simultaneously, as written.<p>
+   *
+   * @return a boolean, true if this menu is being registered
+   * for the first time.
+   * 
+   */
+
+  public boolean registerItems(treeControl listener)
   {
     MenuItem temp;
 
@@ -48,7 +61,7 @@ public class treeMenu extends PopupMenu {
 
     if (registered)
       {
-	return;
+	return false;
       }
 
     for (int i = 0; i < getItemCount(); i++)
@@ -58,5 +71,7 @@ public class treeMenu extends PopupMenu {
       }
 
     registered = true;
+
+    return true;
   }
 }
