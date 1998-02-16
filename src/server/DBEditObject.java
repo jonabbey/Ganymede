8b43b1d3ce4f9eb3bd9f559fee3f476ab419767b
Jonathan Abbey <jonabbey@arlut.utexas.edu>
1998-02-16 19:36:51 +0000

    Nothin'. -- Jon

--- src/server/DBEditObject.java
+++ src/server/DBEditObject.java
@@ -6,7 +6,7 @@
    The GANYMEDE object storage system.
 
    Created: 2 July 1996
-   Version: $Revision: 1.45 $ %D%
+   Version: $Revision: 1.46 $ %D%
    Module By: Jonathan Abbey
    Applied Research Laboratories, The University of Texas at Austin
 
