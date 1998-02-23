1ce57d5dbf664f9d0178f64f6a1ecfed360834d8
Jonathan Abbey <jonabbey@arlut.utexas.edu>
1998-02-23 19:34:42 +0000

    Added getFieldValuesLocal() -- Jon

--- src/server/DBObject.java
+++ src/server/DBObject.java
@@ -6,7 +6,7 @@
    The GANYMEDE object storage system.
 
    Created: 2 July 1996
-   Version: $Revision: 1.44 $ %D%
+   Version: $Revision: 1.45 $ %D%
    Module By: Jonathan Abbey
    Applied Research Laboratories, The University of Texas at Austin
 
@@ -53,7 +53,7 @@ import arlut.csd.JDialog.*;
  * <p>The constructors of this object can throw RemoteException because of the
  * UnicastRemoteObject superclass' constructor.</p>
  *
- * @version $Revision: 1.44 $ %D% (Created 2 July 1996)
+ * @version $Revision: 1.45 $ %D% (Created 2 July 1996)
  * @author Jonathan Abbey, jonabbey@arlut.utexas.edu, ARL:UT
  *
  */
@@ -1389,6 +1389,42 @@ public class DBObject extends UnicastRemoteObject implements db_object, FieldTyp
       }
 
     return f.getValues();
+  }
+
+  /**
+   *
+   * Shortcut method to set a field's value.  Using this
+   * method saves a roundtrip to the server, which is
+   * particularly useful in database loading.
+   *
+   * @see arlut.csd.ganymede.db_object
+   */
+
+  public Vector getFieldValuesLocal(short fieldID)
+  {
+    DBField f = (DBField) getField(fieldID);
+    
+    if (f == null)
+      {
+	if (gSession != null)
+	  {
+	    gSession.setLastError("couldn't find field " + fieldID);
+	  }
+
+	return null;
+      }
+
+    if (!f.isVector())
+      {
+	if (gSession != null)
+	  {
+	    gSession.setLastError("couldn't get vector value on scalar field " + fieldID);
+	  }
+
+	return null;
+      }
+
+    return f.getValuesLocal();
   }
 
   /**
