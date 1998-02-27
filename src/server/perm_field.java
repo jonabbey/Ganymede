3812051ca94dd8c24cd1caa3077e9f5e4397315b
Jonathan Abbey <jonabbey@arlut.utexas.edu>
1998-02-27 13:10:04 +0000

    Added getMatrix(). -- Jon

--- src/server/perm_field.java
+++ src/server/perm_field.java
@@ -5,7 +5,7 @@
    Client-side interface to the PermissionMatrixDBField class.
    
    Created: 27 June 1997
-   Version: $Revision: 1.2 $ %D%
+   Version: $Revision: 1.3 $ %D%
    Module By: Jonathan Abbey
    Applied Research Laboratories, The University of Texas at Austin
 
@@ -22,6 +22,7 @@ import java.rmi.*;
 ------------------------------------------------------------------------------*/
 
 public interface perm_field extends db_field {
+  public PermMatrix getMatrix() throws RemoteException;
   public PermEntry getPerm(short baseID, short fieldID) throws RemoteException;
   public PermEntry getPerm(short baseID) throws RemoteException;
   public PermEntry getPerm(Base base, BaseField field) throws RemoteException;
