8bd65a8b85f77b978d049b5283938766282b490e
Jonathan Abbey <jonabbey@arlut.utexas.edu>
1998-03-12 11:48:47 +0000

    Defensive coding. -- Jon

--- src/Util/PathComplete.java
+++ src/Util/PathComplete.java
@@ -5,7 +5,7 @@
    A utility class to fix up a path string.
    
    Created: 12 March 1998
-   Version: $Revision: 1.1 $ %D%
+   Version: $Revision: 1.2 $ %D%
    Module By: Jonathan Abbey
    Applied Research Laboratories, The University of Texas at Austin
 
@@ -33,6 +33,13 @@ public class PathComplete {
     if (separator == null)
       {
 	separator = System.getProperty("file.separator");
+
+	if (separator == null)
+	  {
+	    System.err.println("ERROR! PathComplete couldn't read file.separator system property.");
+	    System.err.println("PathComplete defaulting to UNIX separator");
+	    separator = "/";
+	  }
       }
 
     if (!path.endsWith(separator))
