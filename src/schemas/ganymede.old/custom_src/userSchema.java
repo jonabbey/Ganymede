cfb156723149d48cc0b3197c2503522b8e56a870
Jonathan Abbey <jonabbey@arlut.utexas.edu>
1998-03-27 17:28:44 +0000

    Added UNIX and UNIXENABLED. -- Jon

--- src/schemas/ganymede.old/custom_src/userSchema.java
+++ src/schemas/ganymede.old/custom_src/userSchema.java
@@ -5,7 +5,7 @@
    An interface defining constants to be used by the user code.
    
    Created: 12 March 1998
-   Version: $Revision: 1.2 $ %D%
+   Version: $Revision: 1.3 $ %D%
    Module By: Jonathan Abbey
    Applied Research Laboratories, The University of Texas at Austin
 
@@ -36,16 +36,18 @@ public interface userSchema {
   final static short PERSON=272;
   final static short PRIVILEGED=273;
   final static short PURPOSE=274;
-  final static short PASSWORD=101;
-  final static short UID=256;
-  final static short HOMEGROUP=265;
-  final static short GROUPLIST=264;
-  final static short LOGINSHELL=263;
-  final static short HOMEDIR=262;
   final static short PERSONAE=102;
-  final static short NETGROUPS=266;
-  final static short VOLUMES=271;
+  final static short PASSWORD=101;
   final static short ALIASES=267;
   final static short SIGNATURE=268;
   final static short EMAILTARGET=269;
+  final static short UNIX=275;
+  final static short UNIXENABLED=276;
+  final static short UID=256;
+  final static short LOGINSHELL=263;
+  final static short HOMEGROUP=265;
+  final static short GROUPLIST=264;
+  final static short NETGROUPS=266;
+  final static short HOMEDIR=262;
+  final static short VOLUMES=271;
 }
