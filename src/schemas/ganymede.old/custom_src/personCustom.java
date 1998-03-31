4b27e7d11df1b129d063b0b6d6087cceeaba82b7
Jonathan Abbey <jonabbey@arlut.utexas.edu>
1998-03-31 16:50:18 +0000

    Got things working in permExpand(), turned off debug. -- Jon

--- src/schemas/ganymede.old/custom_src/personCustom.java
+++ src/schemas/ganymede.old/custom_src/personCustom.java
@@ -5,7 +5,7 @@
    This file is a management class for person objects in Ganymede.
    
    Created: 25 March 1998
-   Version: $Revision: 1.1 $ %D%
+   Version: $Revision: 1.2 $ %D%
    Module By: Jonathan Abbey
    Applied Research Laboratories, The University of Texas at Austin
 
@@ -99,4 +99,102 @@ public class personCustom extends DBEditObject implements SchemaConstants {
 
     return lastname + ", " + firstname;
   }
+
+  /**
+   *
+   * Customization method to allow this Ganymede object type to grant
+   * permissions above and beyond the default permissions mechanism
+   * for special purposes.<br><br>
+   *
+   * If this method returns null, the default permissions mechanism
+   * will be followed.  If not, the permissions system will grant
+   * the union of the permissions specified by this method for access to the
+   * given object.<br><br>
+   *
+   * This method is essentially different from permOverride() in that
+   * the permissions system will not just take the result of this
+   * method for an answer, but will grant additional permissions as
+   * appropriate.<br><br>
+   *
+   * To be overridden in DBEditObject subclasses.
+   *
+   */
+
+  public PermEntry permExpand(GanymedeSession session, DBObject object)
+  {
+    PermEntry result = PermEntry.noPerms;
+    Vector accounts;
+    Invid userInvid;
+    DBObject account;
+    DBSession dbSession;
+    Boolean privileged;
+
+    /* -- */
+
+    if ((object == null) || (object.getTypeID() != getTypeID()))
+      {
+	return null;
+      }
+
+    if (debug)
+      {
+	System.err.println("personCustom.permExpand(" + object.getLabel() + ")");
+      }
+
+    // ok.. we want to allow access to this person object if the admin
+    // is logged in as or has ownership of one of the privileged user
+    // accounts connected with this object.
+
+    dbSession = session.getSession();
+
+    accounts = object.getFieldValuesLocal(personSchema.ACCOUNTS);
+
+    for (int i = 0; i < accounts.size(); i++)
+      {
+	userInvid = (Invid) accounts.elementAt(i);
+	account = dbSession.viewDBObject(userInvid);
+
+	privileged = (Boolean) account.getFieldValueLocal(userSchema.PRIVILEGED);
+
+	if (privileged != null && privileged.booleanValue())
+	  {
+	    // ok, if the user is this account or owns this account,
+	    // give em permission
+
+	    if (userInvid.equals(session.getUserInvid()))
+	      {
+		if (debug)
+		  {
+		    System.err.println("personCustom.permExpand(): Returning full perms for viewing self");
+		  }
+
+		return PermEntry.fullPerms; // create doesn't really apply, but this is cheap.
+	      }
+	    else
+	      {
+		result = result.union(session.getPerm(account));
+	      }
+	    
+	    if (result.equals(PermEntry.fullPerms))
+	      {
+		// we've already got full perms, go ahead and return.
+
+		if (debug)
+		  {
+		    System.err.println("personCustom.permExpand(): Returning full perms for viewing owned person");
+		  }
+
+		return result;
+	      }
+	  }
+      }
+
+    if (debug)
+      {
+	System.err.println("personCustom.permExpand(" + object.getLabel() + ") returning " + result);
+      }
+
+    return result;
+  }
+
 }
