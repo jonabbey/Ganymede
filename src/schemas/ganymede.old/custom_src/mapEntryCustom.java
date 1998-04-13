baddc8b9377503db9ec5c9b0304e3be6664883bc
Jonathan Abbey <jonabbey@arlut.utexas.edu>
1998-04-13 21:33:58 +0000

    Initial revision

---
+++ src/schemas/ganymede.old/custom_src/mapEntryCustom.java
@@ -0,0 +1,116 @@
+/*
+
+   mapEntryCustom.java
+
+   This file is a management class for Automounter map entry objects in Ganymede.
+   
+   Created: 9 December 1997
+   Version: $Revision: 1.1 $ %D%
+   Module By: Jonathan Abbey
+   Applied Research Laboratories, The University of Texas at Austin
+
+*/
+
+package arlut.csd.ganymede.custom;
+
+import arlut.csd.ganymede.*;
+
+import java.util.*;
+import java.rmi.*;
+
+/*------------------------------------------------------------------------------
+                                                                           class
+                                                                  mapEntryCustom
+
+------------------------------------------------------------------------------*/
+
+public class mapEntryCustom extends DBEditObject implements SchemaConstants {
+
+  /**
+   *
+   * This method provides a hook that can be used to indicate that a
+   * particular field's value should be filtered by a particular
+   * subclass of DBEditObject.  This is intended to allow, for instance,
+   * that the Admin object's name field, if null, can have the owning
+   * user's name interposed.
+   *
+   */
+
+  /**
+   *
+   * Customization Constructor
+   *
+   */
+
+  public mapEntryCustom(DBObjectBase objectBase) throws RemoteException
+  {
+    super(objectBase);
+  }
+
+  /**
+   *
+   * Create new object constructor
+   *
+   */
+
+  public mapEntryCustom(DBObjectBase objectBase, Invid invid, DBEditSet editset) throws RemoteException
+  {
+    super(objectBase, invid, editset);
+  }
+
+  /**
+   *
+   * Check-out constructor, used by DBObject.createShadow()
+   * to pull out an object for editing.
+   *
+   */
+
+  public mapEntryCustom(DBObject original, DBEditSet editset) throws RemoteException
+  {
+    super(original, editset);
+  }
+
+  /**
+   *
+   * Hook to allow intelligent generation of labels for DBObjects
+   * of this type.  Subclasses of DBEditObject should override
+   * this method to provide for custom generation of the
+   * object's label type
+   *
+   */
+
+  public String getLabelHook(DBObject object)
+  {
+    DBField field, field2;
+    StringBuffer buff = new StringBuffer();
+
+    /* -- */
+
+    if ((object == null) || (object.getTypeID() != getTypeID()))
+      {
+	return null;
+      }
+
+    field = (DBField) object.getField((short) 256); // map name
+    field2 = (DBField) object.getField((short) 257); // volume
+
+    try
+      {
+	if (field != null)
+	  {
+	    buff.append(field.getValueString() + ":");
+	  }
+
+	if (field2 != null)
+	  {
+	    buff.append(field2.getValueString());
+	  }
+      }
+    catch (IllegalArgumentException ex)
+      {
+	buff.append("<?:?>");
+      }
+
+    return buff.toString();
+  }
+}
