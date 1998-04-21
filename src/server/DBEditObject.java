add795509d330b0d1c663173ab77923dcff52192
Jonathan Abbey <jonabbey@arlut.utexas.edu>
1998-04-21 12:53:17 +0000

    Made getStatus() public. -- Jon

--- src/server/DBEditObject.java
+++ src/server/DBEditObject.java
@@ -6,7 +6,7 @@
    The GANYMEDE object storage system.
 
    Created: 2 July 1996
-   Version: $Revision: 1.60 $ %D%
+   Version: $Revision: 1.61 $ %D%
    Module By: Jonathan Abbey
    Applied Research Laboratories, The University of Texas at Austin
 
@@ -406,7 +406,7 @@ public class DBEditObject extends DBObject implements ObjectStatus, FieldType {
    *
    */
 
-  byte getStatus()
+  public byte getStatus()
   {
     return status;
   }
