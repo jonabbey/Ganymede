6bd7309afc910ec7fef420e932f78273b89d9e34
Jonathan Abbey <jonabbey@arlut.utexas.edu>
1998-04-23 16:00:16 +0000

    Added fieldRequired(). -- Jon

--- src/server/eventCustom.java
+++ src/server/eventCustom.java
@@ -5,7 +5,7 @@
    This file is a management class for event-class records in Ganymede.
    
    Created: 9 December 1997
-   Version: $Revision: 1.1 $ %D%
+   Version: $Revision: 1.2 $ %D%
    Module By: Jonathan Abbey
    Applied Research Laboratories, The University of Texas at Austin
 
@@ -68,6 +68,22 @@ public class eventCustom extends DBEditObject implements SchemaConstants {
   public eventCustom(DBObject original, DBEditSet editset) throws RemoteException
   {
     super(original, editset);
+  }
+
+  /**
+   *
+   * Customization method to control whether a specified field
+   * is required to be defined at commit time for a given object.<br><br>
+   *
+   * To be overridden in DBEditObject subclasses.
+   *
+   */
+
+  public boolean fieldRequired(DBObject object, short fieldid)
+  {
+    // both fields defined in event are required
+
+    return true;
   }
 
   /**
