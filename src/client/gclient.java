6e78b3c3ffeaad9e4f87804114dc5afbd813aa49
Michael Mulvaney <mulvaney@arlut.utexas.edu>
1998-05-04 18:49:15 +0000

    Took out some debugging. Mike

--- src/client/gclient.java
+++ src/client/gclient.java
@@ -4,7 +4,7 @@
    Ganymede client main module
 
    Created: 24 Feb 1997
-   Version: $Revision: 1.72 $ %D%
+   Version: $Revision: 1.73 $ %D%
    Module By: Mike Mulvaney, Jonathan Abbey, and Navin Manohar
    Applied Research Laboratories, The University of Texas at Austin
 
@@ -1968,7 +1968,7 @@ public class gclient extends JFrame implements treeCallback,ActionListener, Jset
 
 	if (deleteHash.containsKey(invid))
 	  {
-	    if (true)
+	    if (debug)
 	      {
 		System.out.println("Setting icon to delete.");
 	      }
@@ -1976,7 +1976,7 @@ public class gclient extends JFrame implements treeCallback,ActionListener, Jset
 	  }
 	else if (createHash.containsKey(invid))
 	  {
-	    if (true)
+	    if (debug)
 	      {
 		System.out.println("Setting icon to create.");
 	      }
