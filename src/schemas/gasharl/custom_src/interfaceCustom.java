7407491245fb72b7c3896dacce5c755c49547f98
Jonathan Abbey <jonabbey@arlut.utexas.edu>
1998-05-15 18:25:41 +0000

    Added support for providing network choices based on the room the systm is in.  Still need to automatically provide IP addresses from the net chosen. -- Jon

--- src/schemas/gasharl/custom_src/interfaceCustom.java
+++ src/schemas/gasharl/custom_src/interfaceCustom.java
@@ -5,7 +5,7 @@
    This file is a management class for interface objects in Ganymede.
    
    Created: 15 October 1997
-   Version: $Revision: 1.5 $ %D%
+   Version: $Revision: 1.6 $ %D%
    Module By: Jonathan Abbey
    Applied Research Laboratories, The University of Texas at Austin
 
@@ -27,8 +27,6 @@ import java.rmi.*;
 public class interfaceCustom extends DBEditObject implements SchemaConstants {
   
   static final boolean debug = false;
-  static QueryResult shellChoices = new QueryResult();
-  static Date shellChoiceStamp = null;
 
   /**
    *
@@ -73,7 +71,6 @@ public class interfaceCustom extends DBEditObject implements SchemaConstants {
   {
     super(original, editset);
   }
-  
 
   /**
    *
@@ -86,155 +83,212 @@ public class interfaceCustom extends DBEditObject implements SchemaConstants {
 
   public String getLabelHook(DBObject object)
   {
-    DBField field, field2;
-    DBObject ipObj, dnsObj;
-    String result = null;
-    Invid invid = null;
+    StringBuffer result = new StringBuffer();
 
     /* -- */
 
-    if ((object == null) || (object.getTypeID() != getTypeID()))
+    String name = (String) object.getFieldValueLocal(interfaceSchema.NAME);
+
+    if (name != null)
+      {
+	result.append(name);
+      }
+
+    IPDBField ipfield = (IPDBField) object.getField(interfaceSchema.ADDRESS);
+
+    if (ipfield != null)
+      {
+	if (result.length() != 0)
+	  {
+	    result.append(":");
+	  }
+	
+	result.append(ipfield.getValueString(true));
+      }
+
+    if (result.length() == 0)
       {
 	return null;
       }
-
-    field = (DBField) object.getField((short) 257); // get the I.P. records field
-
-    if (field == null)
-      {
-	field = (DBField) object.getField((short) 256); // get the Ethernet address
-	
-	return field.getValueString();
-      }
-
-    if (field.size() > 0)
-      {
-	invid = (Invid) field.getElement(0); // get the primary I.P. address object
-      }
-
-    if (invid == null)
-      {
-	field = (DBField) object.getField((short) 256); // get the Ethernet address
-
-	if (field == null)
-	  {
-	    return "interface " + getInvid();
-	  }
-	
-	return field.getValueString();
-      }
-
-    // okay to use DBSession.viewDBObject() here, as we are basically just looking
-    // to get label information
-    //
-    // Note that we have to use Ganymede.internalSession here because we are
-    // getting label information for another object, and not for ourself
-
-    ipObj = Ganymede.internalSession.getSession().viewDBObject(invid);
-
-    if (ipObj == null)
-      {
-	field = (DBField) object.getField((short) 256); // get the Ethernet address
-	
-	return field.getValueString();
-      }
-
-    field = (DBField) ipObj.getField((short) 256); // vector of dns records
-
-    if (field == null)
-      {
-	field = (DBField) ipObj.getField((short) 257); // get the i.p. address
-	
-	return field.getValueString();
-      }
-
-    invid = (Invid) field.getElement(0); // get the primary dns record
-
-    if (invid == null)
-      {
-	field = (DBField) ipObj.getField((short) 257); // get the i.p. address
-	
-	return field.getValueString();
-      }
-
-    // okay to use DBSession.viewDBObject() here, as we are basically just looking
-    // to get label information
-    //
-    // Note that we have to use Ganymede.internalSession here because we are
-    // getting label information for another object, and not for ourself
-
-    dnsObj = (DBObject) Ganymede.internalSession.getSession().viewDBObject(invid);
-
-    if (dnsObj == null)
-      {
-	field = (DBField) ipObj.getField((short) 257); // get the i.p. address
-
-	return result + ":" + field.getValueString();
-      }
-
-    field = (DBField) dnsObj.getField((short) 257);
-
-    if (field == null)
-      {
-	field = (DBField) ipObj.getField((short) 257); // get the i.p. address
-	return field.getValueString();
-      }
     else
       {
-	result = field.getValueString();
-      }
-
-    if (result == null || result.equals(""))
-      {
-	field = (DBField) ipObj.getField((short) 257); // get the i.p. address
-	return field.getValueString();
-      }
-    else
-      {
-	field = (DBField) ipObj.getField((short) 257); // get the i.p. address
-
-	return result + ":" + field.getValueString();
+	return result.toString();
       }
   }
 
   /**
    *
-   * Hook to have this object create a new embedded object
-   * in the given field.  
+   * This method provides a hook that can be used to generate
+   * choice lists for invid and string fields that provide
+   * such.  String and Invid DBFields will call their owner's
+   * obtainChoiceList() method to get a list of valid choices.<br><br>
+   *
+   * This method will provide a reasonable default for targetted
+   * invid fields.
+   * 
+   */
+
+  public QueryResult obtainChoiceList(DBField field)
+  {
+    if (field.getID() != interfaceSchema.IPNET)
+      {
+	return super.obtainChoiceList(field);
+      }
+
+    // ok, we are returning the list of choices for what
+    // ipNet's to put this entry into.  We don't want it
+    // to include any ipNets that already have an entry
+    // for this system.
+
+    // first, get the list of map entries contained in this
+    // object.
+
+    Vector netsToSkip = new Vector();
+    DBObject entry;
+    Invid netInvid;
+
+    Vector entries = getSiblingInvids();
+
+    for (int i = 0; i < entries.size(); i++)
+      {
+	entry = getSession().viewDBObject((Invid) entries.elementAt(i));
+
+	netInvid = (Invid) entry.getFieldValueLocal(interfaceSchema.IPNET);
+
+	if (netInvid != null)
+	  {
+	    netsToSkip.addElement(netInvid);
+	  }
+      }
+    
+    // ok, netsToSkip has a list of invid's to skip in our choice list.
+
+    // now, we need to find the list of IPnet's available in the room
+    // that the system is registered in.
+
+    QueryResult netList = new QueryResult();
+
+    try
+      {
+	// find the system that contains us
+	
+	Invid sysInvid = (Invid) getFieldValueLocal(SchemaConstants.ContainerField);
+	entry = getSession().viewDBObject(sysInvid);
+	
+	// get the room object that the system is associated with
+	
+	Invid roomInvid = (Invid) entry.getFieldValueLocal(systemSchema.ROOM);
+	entry = getSession().viewDBObject(roomInvid);
+	
+	// and get the list of networks in that room
+	
+	Vector ipNetVec = entry.getFieldValuesLocal(roomSchema.NETWORKS);
+	String netName;
+	
+	for (int i = 0; i < ipNetVec.size(); i++)
+	  {
+	    netInvid = (Invid) ipNetVec.elementAt(i);
+	    netName = getGSession().viewObjectLabel(netInvid);
+	    
+	    netList.addRow(netInvid, netName, true);
+	  }
+      }
+    catch (NullPointerException ex)
+      {
+	// ok, no big deal
+      }
+
+    QueryResult result = new QueryResult();
+
+    // go through baseList, anything that we don't skip because
+    // other map entries in this user object have taken that
+    // map gets added to result.
+
+    for (int i = 0; i < netList.size(); i++)
+      {
+	netInvid = netList.getInvid(i);
+
+	if (!netsToSkip.contains(netInvid))
+	  {
+	    result.addRow(netList.getObjectHandle(i));
+	  }
+      }
+
+    if (debug)
+      {
+	System.err.println("interfaceCustom: net choice for invid " + getInvid() + ":\n" +
+			   result.getBuffer());
+      }
+    
+    return result;
+  }
+
+  /**
+   *
+   * This is the hook that DBEditObject subclasses use to interpose wizards when
+   * a field's value is being changed.
    *
    */
 
-  public Invid createNewEmbeddedObject(InvidDBField field)
+  public ReturnVal wizardHook(DBField field, int operation, Object param1, Object param2)
   {
-    DBEditObject newObject;
-    DBObjectBase targetBase;
-    DBObjectBaseField fieldDef;
-
-    /* -- */
-
-    if (field.getID() == 257)
+    if ((field.getID() != mapEntrySchema.MAP) ||
+	(operation != DBEditObject.SETVAL))
       {
-	fieldDef = field.getFieldDef();
-	
-	if (fieldDef.getTargetBase() > -1)
-	  {
-	    newObject = getSession().createDBObject(fieldDef.getTargetBase(), null, null);
-
-	    // link it in
-
-	    newObject.setFieldValue(SchemaConstants.ContainerField, getInvid());
-	    
-	    return newObject.getInvid();
-	  }
-	else
-	  {
-	    editset.getSession().setLastError("error in schema.. imbedded object type not restricted..");
-	    return null;
-	  }
+	return null;		// by default, we just ok whatever
       }
-    else
+
+    // ok, we want to go ahead and approve the operation,
+    // but we want to cause the client to rescan the IPNET
+    // field in all of our siblings so that their choice
+    // list gets updated to not show whatever net *we*
+    // just chose.
+
+    // First, we create a ReturnVal that will apply to
+    // our siblings
+
+    ReturnVal rescanPlease = new ReturnVal(true); // bool doesn't matter
+    rescanPlease.addRescanField(interfaceSchema.IPNET);
+
+    // second, we create a ReturnVal which will cause
+    // the field.setValue() call which triggered us
+    // to continue normal processing, and return
+    // our list of rescan preferences to the client.
+
+    ReturnVal result = new ReturnVal(true, true);
+    Vector entries = getSiblingInvids();
+
+    for (int i = 0; i < entries.size(); i++)
       {
-	return null;		// default
+	if (debug)
+	  {
+	    System.err.println("interfaceCustom.wizardHook(): adding object " + 
+			       entries.elementAt(i) + " for rescan");
+	  }
+	result.addRescanObject((Invid) entries.elementAt(i), rescanPlease);
       }
+
+    return result;
+  }
+
+  private Vector getSiblingInvids()
+  {
+    Vector result;
+    Invid sysInvid = (Invid) getFieldValueLocal(SchemaConstants.ContainerField);
+    DBObject sysObj = getSession().viewDBObject(sysInvid);
+
+    result = (Vector) sysObj.getFieldValuesLocal(systemSchema.INTERFACES).clone();
+    
+    // we are not our own sibling.
+
+    result.removeElement(getInvid());
+
+    if (debug)
+      {
+	System.err.println("interfaceCustom.getSiblingInvids(): " + getInvid() +
+			   " has return value: " + result);
+      }
+
+    return result;
   }
 }
