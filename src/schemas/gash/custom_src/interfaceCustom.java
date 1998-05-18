7914a739796a00df823e1a48765fbdeac96b4d0d
Jonathan Abbey <jonabbey@arlut.utexas.edu>
1998-05-18 13:45:17 +0000

    Added support for managing IP nets and IP addresses through methods provided by the systemCustom object. -- Jon

--- src/schemas/gash/custom_src/interfaceCustom.java
+++ src/schemas/gash/custom_src/interfaceCustom.java
@@ -5,7 +5,7 @@
    This file is a management class for interface objects in Ganymede.
    
    Created: 15 October 1997
-   Version: $Revision: 1.6 $ %D%
+   Version: $Revision: 1.7 $ %D%
    Module By: Jonathan Abbey
    Applied Research Laboratories, The University of Texas at Austin
 
@@ -27,6 +27,10 @@ import java.rmi.*;
 public class interfaceCustom extends DBEditObject implements SchemaConstants {
   
   static final boolean debug = false;
+
+  // ---
+
+  systemCustom sysObj = null;
 
   /**
    *
@@ -70,6 +74,7 @@ public class interfaceCustom extends DBEditObject implements SchemaConstants {
   public interfaceCustom(DBObject original, DBEditSet editset) throws RemoteException
   {
     super(original, editset);
+    getParentObj();
   }
 
   /**
@@ -135,83 +140,17 @@ public class interfaceCustom extends DBEditObject implements SchemaConstants {
 	return super.obtainChoiceList(field);
       }
 
-    // ok, we are returning the list of choices for what
-    // ipNet's to put this entry into.  We don't want it
-    // to include any ipNets that already have an entry
-    // for this system.
-
-    // first, get the list of map entries contained in this
-    // object.
-
-    Vector netsToSkip = new Vector();
-    DBObject entry;
-    Invid netInvid;
-
-    Vector entries = getSiblingInvids();
-
-    for (int i = 0; i < entries.size(); i++)
-      {
-	entry = getSession().viewDBObject((Invid) entries.elementAt(i));
-
-	netInvid = (Invid) entry.getFieldValueLocal(interfaceSchema.IPNET);
-
-	if (netInvid != null)
-	  {
-	    netsToSkip.addElement(netInvid);
-	  }
-      }
-    
-    // ok, netsToSkip has a list of invid's to skip in our choice list.
-
-    // now, we need to find the list of IPnet's available in the room
-    // that the system is registered in.
-
-    QueryResult netList = new QueryResult();
-
-    try
-      {
-	// find the system that contains us
-	
-	Invid sysInvid = (Invid) getFieldValueLocal(SchemaConstants.ContainerField);
-	entry = getSession().viewDBObject(sysInvid);
-	
-	// get the room object that the system is associated with
-	
-	Invid roomInvid = (Invid) entry.getFieldValueLocal(systemSchema.ROOM);
-	entry = getSession().viewDBObject(roomInvid);
-	
-	// and get the list of networks in that room
-	
-	Vector ipNetVec = entry.getFieldValuesLocal(roomSchema.NETWORKS);
-	String netName;
-	
-	for (int i = 0; i < ipNetVec.size(); i++)
-	  {
-	    netInvid = (Invid) ipNetVec.elementAt(i);
-	    netName = getGSession().viewObjectLabel(netInvid);
-	    
-	    netList.addRow(netInvid, netName, true);
-	  }
-      }
-    catch (NullPointerException ex)
-      {
-	// ok, no big deal
-      }
-
     QueryResult result = new QueryResult();
 
-    // go through baseList, anything that we don't skip because
-    // other map entries in this user object have taken that
-    // map gets added to result.
+    // get the vector of currently available nets from our containing
+    // System
 
-    for (int i = 0; i < netList.size(); i++)
+    sysObj = getParentObj();
+    Vector ipNetVec = sysObj.getAvailableNets();
+
+    for (int i = 0; i < ipNetVec.size(); i++)
       {
-	netInvid = netList.getInvid(i);
-
-	if (!netsToSkip.contains(netInvid))
-	  {
-	    result.addRow(netList.getObjectHandle(i));
-	  }
+	result.addRow((ObjectHandle) ipNetVec.elementAt(i));
       }
 
     if (debug)
@@ -225,6 +164,68 @@ public class interfaceCustom extends DBEditObject implements SchemaConstants {
 
   /**
    *
+   * This method allows the DBEditObject to have executive approval
+   * of any vector set operation, and to take any special actions
+   * in reaction to the set.. if this method returns true, the
+   * DBField that called us will proceed to make the change to
+   * it's vector.  If this method returns false, the DBField
+   * that called us will not make the change, and the field
+   * will be left unchanged.<br><br>
+   *
+   * The DBField that called us will take care of all possible
+   * checks on the operation (including vector bounds, etc.),
+   * acceptable values as appropriate (including a call to our
+   * own verifyNewValue() method.  Under normal circumstances,
+   * we won't need to do anything here.<br><br>
+   *
+   * If we do return false, we should set editset.setLastError to
+   * provide feedback to the client about what we disapproved of.
+   * 
+   */
+
+  public boolean finalizeSetElement(DBField field, int index, Object value)
+  {
+    if (field.getID() == interfaceSchema.IPNET)
+      {
+	// we're changing the IP net.. make sure our parent is ok with us
+	// taking the new net.
+	
+	sysObj = getParentObj();
+	
+	// free the old net for others to use.. note that at this point, 
+	// field.getOldValue() holds the field's old value and value is the
+	// proposed new value
+	
+	if (!sysObj.freeNet((Invid) field.getOldValue()))
+	  {
+	    return false;
+	  }
+	else if (!sysObj.allocNet((Invid) value))
+	  {
+	    sysObj.allocNet((Invid) field.getOldValue()); // take it back
+	    return false;
+	  }
+
+	// set our address.. the wizardHook will have instructed the
+	// client to rescan the address field.
+
+	IPDBField ipfield = (IPDBField) getField(interfaceSchema.ADDRESS);
+	Byte[] address = getParentObj().getAddress((Invid) value);
+	
+	if (address != null)
+	  {
+	    // this will work or not, the client's rescan will show the
+	    // result regardless
+
+	    ipfield.setValueLocal(address);
+	  }
+      }
+
+    return true;
+  }
+
+  /**
+   *
    * This is the hook that DBEditObject subclasses use to interpose wizards when
    * a field's value is being changed.
    *
@@ -232,43 +233,57 @@ public class interfaceCustom extends DBEditObject implements SchemaConstants {
 
   public ReturnVal wizardHook(DBField field, int operation, Object param1, Object param2)
   {
-    if ((field.getID() != mapEntrySchema.MAP) ||
-	(operation != DBEditObject.SETVAL))
+    if (field.getID() == interfaceSchema.IPNET && operation == SETVAL)
       {
-	return null;		// by default, we just ok whatever
-      }
+	// ok, we want to go ahead and approve the operation,
+	// but we want to cause the client to rescan the IPNET
+	// field in all of our siblings so that their choice
+	// list gets updated to not show whatever net *we*
+	// just chose.
 
-    // ok, we want to go ahead and approve the operation,
-    // but we want to cause the client to rescan the IPNET
-    // field in all of our siblings so that their choice
-    // list gets updated to not show whatever net *we*
-    // just chose.
+	// First, we create a ReturnVal that will apply to
+	// our siblings
+	
+	ReturnVal rescanPlease = new ReturnVal(true); // bool doesn't matter
+	rescanPlease.addRescanField(interfaceSchema.IPNET);
 
-    // First, we create a ReturnVal that will apply to
-    // our siblings
+	// second, we create a ReturnVal which will cause
+	// the field.setValue() call which triggered us
+	// to continue normal processing, and return
+	// our list of rescan preferences to the client.
 
-    ReturnVal rescanPlease = new ReturnVal(true); // bool doesn't matter
-    rescanPlease.addRescanField(interfaceSchema.IPNET);
+	ReturnVal result = new ReturnVal(true, true);
+	result.addRescanField(interfaceSchema.ADDRESS);
 
-    // second, we create a ReturnVal which will cause
-    // the field.setValue() call which triggered us
-    // to continue normal processing, and return
-    // our list of rescan preferences to the client.
+	Vector entries = getSiblingInvids();
 
-    ReturnVal result = new ReturnVal(true, true);
-    Vector entries = getSiblingInvids();
-
-    for (int i = 0; i < entries.size(); i++)
-      {
-	if (debug)
+	for (int i = 0; i < entries.size(); i++)
 	  {
-	    System.err.println("interfaceCustom.wizardHook(): adding object " + 
-			       entries.elementAt(i) + " for rescan");
+	    if (debug)
+	      {
+		System.err.println("interfaceCustom.wizardHook(): adding object " + 
+				   entries.elementAt(i) + " for rescan");
+	      }
+	    result.addRescanObject((Invid) entries.elementAt(i), rescanPlease);
 	  }
-	result.addRescanObject((Invid) entries.elementAt(i), rescanPlease);
+
+
+
+	return result;
       }
 
-    return result;
+    return null;
+  }
+
+  private systemCustom getParentObj()
+  {
+    if (sysObj == null)
+      {    
+	Invid sysInvid = (Invid) getFieldValueLocal(SchemaConstants.ContainerField);
+	sysObj = (systemCustom) getSession().viewDBObject(sysInvid);
+      }
+
+    return sysObj;
   }
 
   private Vector getSiblingInvids()
