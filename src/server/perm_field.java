815debf571df13438ae1ce78d6e74ffc2d2ec957
Jonathan Abbey <jonabbey@arlut.utexas.edu>
1998-05-21 15:30:40 +0000

    Added setDefaultFieldsPerm(). -- Jon

--- src/server/perm_field.java
+++ src/server/perm_field.java
@@ -5,7 +5,7 @@
    Client-side interface to the PermissionMatrixDBField class.
    
    Created: 27 June 1997
-   Version: $Revision: 1.3 $ %D%
+   Version: $Revision: 1.4 $ %D%
    Module By: Jonathan Abbey
    Applied Research Laboratories, The University of Texas at Austin
 
@@ -29,6 +29,8 @@ public interface perm_field extends db_field {
   public PermEntry getPerm(Base base) throws RemoteException;
   public void setPerm(short baseID, short fieldID, PermEntry entry) throws RemoteException;
   public void setPerm(short baseID, PermEntry entry) throws RemoteException;
+  public void setDefaultFieldsPerm(short baseID, PermEntry entry) throws RemoteException;
   public void setPerm(Base base, BaseField field, PermEntry entry) throws RemoteException;
   public void setPerm(Base base, PermEntry entry) throws RemoteException;
+  public void setDefaultFieldsPerm(Base base, PermEntry entry) throws RemoteException;
 }
