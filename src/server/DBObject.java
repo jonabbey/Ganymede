10d56ab6d792a5776c0e21596a4726a6c0d922da
Jonathan Abbey <jonabbey@arlut.utexas.edu>
1998-06-10 17:07:42 +0000

    Optimized getField() a bit, since it is so commonly used.  The compiler may have already done this optimization, so there may not be a difference here. -- Jon

--- src/server/DBObject.java
+++ src/server/DBObject.java
@@ -6,7 +6,7 @@
    The GANYMEDE object storage system.
 
    Created: 2 July 1996
-   Version: $Revision: 1.52 $ %D%
+   Version: $Revision: 1.53 $ %D%
    Module By: Jonathan Abbey
    Applied Research Laboratories, The University of Texas at Austin
 
@@ -53,7 +53,7 @@ import arlut.csd.JDialog.*;
  * <p>The constructors of this object can throw RemoteException because of the
  * UnicastRemoteObject superclass' constructor.</p>
  *
- * @version $Revision: 1.52 $ %D% (Created 2 July 1996)
+ * @version $Revision: 1.53 $ %D% (Created 2 July 1996)
  * @author Jonathan Abbey, jonabbey@arlut.utexas.edu, ARL:UT
  *
  */
@@ -1046,13 +1046,9 @@ public class DBObject extends UnicastRemoteObject implements db_object, FieldTyp
    * @see arlut.csd.ganymede.db_object
    */
 
-  public db_field getField(short id)
+  public final db_field getField(short id)
   {
-    db_field f;
-
-    f = fields.get(id);
-
-    return f;
+    return fields.get(id);
   }
 
   /**
