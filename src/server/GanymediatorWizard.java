6568e0e363a2532a3ba05046d4509f21c2a2d946
Jonathan Abbey <jonabbey@arlut.utexas.edu>
1998-06-26 20:27:28 +0000

    Fixed constructor to set state to 1 on construction. -- Jon

--- src/server/GanymediatorWizard.java
+++ src/server/GanymediatorWizard.java
@@ -17,7 +17,7 @@
    call back this GanymediatorWizard to continue along the process.
    
    Created: 29 January 1998
-   Version: $Revision: 1.4 $ %D%
+   Version: $Revision: 1.5 $ %D%
    Module By: Jonathan Abbey
    Applied Research Laboratories, The University of Texas at Austin
 
@@ -25,9 +25,11 @@
 
 package arlut.csd.ganymede;
 
+import java.lang.reflect.*;
 import java.rmi.*;
 import java.rmi.server.*;
 import java.util.Hashtable;
+import java.util.Enumeration;
 
 import arlut.csd.JDialog.JDialogBuff;
 
@@ -64,8 +66,12 @@ public abstract class GanymediatorWizard extends UnicastRemoteObject implements 
   // ---
 
   protected boolean active = false;
-  public int state = 0;
+  public int state = 1;
   protected GanymedeSession session = null;
+
+  protected Hashtable returnHash;
+  boolean stateSet = false;
+  ReturnVal result = new ReturnVal(false);
 
   /* -- */
 
@@ -93,7 +99,7 @@ public abstract class GanymediatorWizard extends UnicastRemoteObject implements 
 	throw new RuntimeException("error, couldn't register wizard with session");
       }
 
-    state = STARTUP;
+    state = 1;
   }
 
   /**
@@ -111,7 +117,221 @@ public abstract class GanymediatorWizard extends UnicastRemoteObject implements 
    *
    */
 
-  public abstract ReturnVal respond(Hashtable returnHash);
+  public ReturnVal respond(Hashtable returnHash)
+  {
+    this.returnHash = returnHash;
+
+    /* - */
+
+    ReturnVal result;
+
+    /* -- */
+
+    if (returnHash != null)
+      {
+	result = callDialog(state);
+
+	if (result == null || 
+	    (!result.didSucceed() &&
+	     result.getCallback() == null) ||
+	    result.didSucceed())
+	  {
+	    this.unregister();
+	    state = DONE;
+	  }
+	else if (!stateSet)
+	  {
+	    state++;
+	  }
+
+	stateSet = false;
+	
+	return result;
+      }
+    else
+      {	
+	result = cancel();
+
+	if (result == null || 
+	    (!result.didSucceed() &&
+	     result.getCallback() == null) ||
+	    result.didSucceed())
+	  {
+	    this.unregister();
+	    state = DONE;
+	  }
+
+	return result;
+      }
+  }
+
+  /**
+   *
+   * This method provides a default response if a user
+   * hits cancel on a wizard dialog.  This should be
+   * subclassed if a wizard wants to provide a more
+   * detailed cancel response.
+   *
+   */
+
+  public ReturnVal cancel()
+  {
+    return fail("Operation Canceled",
+		"Operation Canceled",
+		"OK", null, "ok.gif");
+  }
+
+  /**
+   *
+   * This method should be called by a processDialog*()
+   * method that does not intend to proceed to the next
+   * state.  By default, if a processDialog() returns
+   * a ReturnVal that does not indicate completion
+   * or failure _without_ the callback being set,
+   * respond() will call the next processDialog() method
+   * in the ordinal sequence.
+   *
+   */
+
+  protected void setNextState(int state)
+  {
+    this.state = state;
+    
+    stateSet = true;
+  }
+
+  /**
+   *
+   * This method returns a ReturnVal that indicates that the 
+   * wizard sequence has finished without success.
+   *
+   */
+
+  protected ReturnVal fail(String title, String body,
+			   String ok, String cancel, String image)
+  {
+    result.clear();
+    result.success = false;
+    result.setDialog(new JDialogBuff(title, body, ok, cancel, image));
+    result.setCallback(null);
+
+    return result;
+  }
+
+
+  /**
+   *
+   * This method returns a ReturnVal that indicates that the 
+   * wizard sequence has not yet finished.
+   *
+   */
+
+  protected ReturnVal continueOn(String title, String body,
+				 String ok, String cancel, String image)
+  {
+    result.clear();
+    result.success = false;
+    result.setDialog(new JDialogBuff(title, body, ok, cancel, image));
+    result.setCallback(this);
+
+    return result;
+  }
+
+  /**
+   *
+   * This method returns a ReturnVal that indicates that the 
+   * wizard sequence has terminated successfully.
+   *
+   */
+
+  protected ReturnVal success(String title, String body,
+			      String ok, String cancel, String image)
+  {
+    result.clear();
+    result.success = true;
+    result.setDialog(new JDialogBuff(title, body, ok, cancel, image));
+
+    return result;
+  }
+
+  /**
+   *
+   * This method allows a processDialog*() method
+   * in a GanymediatorWizard subclass to get access
+   * to a value returned to the wizard from a 
+   * previous dialog.
+   *
+   */
+
+  protected Object getParam(Object key)
+  {
+    return returnHash.get(key);
+  }
+
+  /**
+   *
+   * This method allows a processDialog*() method
+   * in a GanymediatorWizard subclass to get access
+   * to an enum of keys returned to the wizard from a 
+   * previous dialog.
+   *
+   */
+
+  protected Enumeration getKeys()
+  {
+    return returnHash.keys();
+  }
+
+  /**
+   *
+   * This method allows a processDialog*() method
+   * in a GanymediatorWizard subclass to get access
+   * to an enum of values returned to the wizard from a 
+   * previous dialog.
+   *
+   */
+
+  protected Enumeration getElements()
+  {
+    return returnHash.elements();
+  }
+
+  public ReturnVal callDialog(int state)
+  {
+    Method dialogMethod = null;
+    ReturnVal localResult;
+
+    /* -- */
+
+    try
+      {
+	dialogMethod = this.getClass().getDeclaredMethod("processDialog" + state, null);
+      }
+    catch (NoSuchMethodException ex)
+      {
+	return Ganymede.createErrorDialog("Ganymede Wizard Error",
+					  "GanymediatorWizard.callDialog(): Couldn't find processDialog" + state);
+      }
+
+    try
+      {
+	localResult = (ReturnVal) dialogMethod.invoke(this, null);
+      }
+    catch (InvocationTargetException ex)
+      {
+	return Ganymede.createErrorDialog("Ganymede Wizard Error",
+					  "GanymediatorWizard.callDialog(): Invocation error in state " + state +
+					  "\n" + ex.getMessage());
+      }
+    catch (IllegalAccessException ex)
+      {
+	return Ganymede.createErrorDialog("Ganymede Wizard Error",
+					  "GanymediatorWizard.callDialog(): Illegal Access error in state " + state +
+					  "\n" + ex.getMessage());
+      }
+
+    return localResult;
+  }
 
   /**
    *
