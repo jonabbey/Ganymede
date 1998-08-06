008c19369f03c6393dc40bbb8f492c305fcdf3fe
Jonathan Abbey <jonabbey@arlut.utexas.edu>
1998-08-06 20:08:19 +0000

    Converted over to using arlut.csd.Util.Parser. -- Jon

--- src/schemas/gasharl/loader/source/User.java
+++ src/schemas/gasharl/loader/source/User.java
@@ -6,7 +6,7 @@
    GASH user_info file
    
    Created: 22 August 1997
-   Version: $Revision: 1.1 $ %D%
+   Version: $Revision: 1.2 $ %D%
    Module By: Jonathan Abbey
    Applied Research Laboratories, The University of Texas at Austin
 
@@ -15,6 +15,7 @@
 package arlut.csd.ganymede.loader;
 
 import java.io.*;
+import arlut.csd.Util.Parser;
 
 /*------------------------------------------------------------------------------
                                                                            class
@@ -34,6 +35,14 @@ public class User {
     tokens.ordinaryChar('\n');
   }
 
+  /**
+   *
+   * Our (extra-super-simple) parser.
+   *
+   */
+
+  Parser parser;
+
   // member fields
 
   String name;
@@ -48,6 +57,12 @@ public class User {
   String directory;
   String shell;
 
+  // ARL specific
+
+  String socialsecurity;
+  String category;
+  String expiration;
+
   // instance constructor
 
   public User()
@@ -56,42 +71,23 @@ public class User {
 
   public boolean loadLine(StreamTokenizer tokens) throws IOException, EOFException
   {
-    // read username
+    parser = new Parser(tokens);
 
-    tokens.nextToken();
-
-    if (tokens.ttype == StreamTokenizer.TT_EOF)
+    if (parser.EOFnext())
       {
 	return true;
       }
-    else
-      {
-	tokens.pushBack();
-      }
 
-    name = getNextBit(tokens);
+    name = parser.getNextBit();
+    password = parser.getNextBit();
+    uid = parser.getNextInt();
+    gid = parser.getNextInt();
+    fullname = parser.getNextBit();
 
-    //    System.out.println("name = '" + name + "'");
+    /* now we need to get the next bit..  in the ARL user_info format,
+       it's "Room Division", separated by a space.  */
 
-    password = getNextBit(tokens); 
-
-    // System.out.println("password = '" + password + "'");
-
-    String uidString = getNextBit(tokens);
-    uid = new Integer(uidString).intValue();
-
-    // System.out.println("uid = '" + uid + "'");
-
-    String gidString = getNextBit(tokens);
-    gid = new Integer(gidString).intValue();
-
-    // System.out.println("gid = '" + gid + "'");
-
-    fullname = getNextBit(tokens);
-
-    // System.out.println("fullname = '" + fullname + "'");
-
-    String tmp = getNextBit(tokens);
+    String tmp = parser.getNextBit();
     int index = tmp.indexOf(' ');
 
     if (index == -1)
@@ -99,69 +95,45 @@ public class User {
 	// no space in the string.. maybe we got
 	// a comma where we shouldn't have?
 
-	room = getNextBit(tokens);
+	room = parser.getNextBit();
       }
     else
       {
 	room = tmp.substring(0, index); 
       }
 
-    // System.out.println("room = '" + room + "'");
     division = tmp.substring(index+1); 
 
-    // System.out.println("division = '" + division + "'");
+    /* and some more : or , separated fields... */
 
-    officePhone = getNextBit(tokens);
+    officePhone = parser.getNextBit();
 
-    // System.out.println("officePhone = '" + officePhone + "'");
+    /* home phone is optional in the ARL user_info file.. */
 
-    int token = tokens.nextToken();
-
-    // should be a comma
-
-    if (token != ',')
+    if (parser.checkNextToken() == ',')
       {
-	System.err.println("No comma after office phone..");
-      }
-    else
-      {
-	token = tokens.nextToken();
+	homePhone = parser.getNextBit();
       }
 
-    if (token == ':')
-      {
-	// no home phone
+    directory = parser.getNextBit();
+    shell = parser.getNextBit();
 
-	homePhone = "";
-      }
-    else
-      {
-	// System.err.println("XXX");
-	tokens.pushBack();
-	homePhone = getNextBit(tokens);
+    /* Ok, we've got all the mandatory felds.. check for the
+      trailing 'optional' fields. */
 
-	// System.out.println("homePhone = '" + homePhone + "'");
+    if (parser.checkNextToken() == ':')
+      {
+	socialsecurity = parser.getNextBit();
       }
 
-    directory = getNextBit(tokens);
-
-    // System.out.println("directory = '" + directory + "'");
-
-    shell = getNextBit(tokens);
-
-    // System.out.println("shell = '" + shell + "'");
-
-    // get to the end of line
-
-    // System.err.println("HEY! Token = " + token + ", ttype = " + tokens.ttype);
-
-    while ((tokens.ttype != StreamTokenizer.TT_EOL) && (tokens.ttype != StreamTokenizer.TT_EOF))
+    if (parser.checkNextToken() == ':')
       {
-	// System.err.print(".");
-	token = tokens.nextToken();
+	category = parser.getNextBit();
       }
 
-    return (tokens.ttype == StreamTokenizer.TT_EOF);
+    parser.skipToEndLine();
+
+    return parser.atEOF();
   }
 
   public void display()
@@ -170,48 +142,6 @@ public class User {
     System.out.println("\tfullname: " + fullname + ", room: " + room + ", division: " + division +
 		       ", officePhone: " + officePhone + ", homePhone: " + homePhone);
     System.out.println("Directory: " + directory + ", shell: " + shell);
+    System.out.println("SS#: " + socialsecurity + ", category: " + category);
   }
-  
-  private String getNextBit(StreamTokenizer tokens) throws IOException
-  {
-    int token;
-    String result;
-
-    token = tokens.nextToken();
-
-    while (tokens.ttype == ':' || tokens.ttype == ',')
-      {
-	//	System.err.println("*");
-	token = tokens.nextToken();
-      }
-
-    if (tokens.ttype == StreamTokenizer.TT_WORD)
-      {
-	//	System.err.println("returning native word");
-	return tokens.sval;
-      }
-
-    if (tokens.ttype == StreamTokenizer.TT_NUMBER)
-      {
-	// System.err.println("returning converted word");
-	result = Integer.toString(new Double(tokens.nval).intValue());
-
-	while (tokens.ttype != ':' && tokens.ttype != ',' && tokens.ttype != StreamTokenizer.TT_EOF &&
-	       tokens.ttype != StreamTokenizer.TT_EOL)
-	  {
-	    token = tokens.nextToken();
-	    
-	    if (tokens.ttype == StreamTokenizer.TT_WORD)
-	      {
-		result += tokens.sval;
-	      }
-	  }
-
-	return result;
-      }
-
-
-    return null;
-  }
-
 }
