e5ed002e864ede30f13482c5409c8942edcdb4d7
Jonathan Abbey <jonabbey@arlut.utexas.edu>
1998-09-11 17:25:27 +0000

    Comment header added to the constructor. -- Jon

--- src/client/openObjectDialog.java
+++ src/client/openObjectDialog.java
@@ -75,6 +75,24 @@ public class openObjectDialog extends JDialog implements ActionListener, MouseLi
 
   /* -- */
 
+  /**
+   *
+   * This is the constructor for openObjectDialog.  The thing worth noting
+   * about this is that when this object is first constructed, it checks
+   * gclient to see if an object node is selected in the gclient's tree.
+   * If so, it will set the object's type and name in the appropriate
+   * GUI fields as they are constructed.<BR><BR>
+   *
+   * Note that this field-setting only occurs when this dialog is first
+   * constructed.  I would have had to rewrite a lot of this class to
+   * make it so that the base/object name fields could have been set
+   * after the fact, so I just didn't.  gclient is set up to dispose
+   * of an old instance of this and recreate one as necessary.  It's
+   * a shameful hack, but it works, and it's not worth investing much
+   * more time here to avoid the redundant dialog creation.
+   *
+   */
+
   public openObjectDialog(gclient client)
   {
     super(client, "Open object", true);
