03d4d123d7e4b7ec12c84ed6e8f90479b2aa222d
Jonathan Abbey <jonabbey@arlut.utexas.edu>
1998-09-21 16:22:19 +0000

    Made the tree not print out  "Hey!  received out of bounds exception, row = " when someone clicks below the last row displayed. -- Jon

--- src/JTree/treeControl.java
+++ src/JTree/treeControl.java
@@ -28,7 +28,7 @@
   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
   
   Created: 3 March 1997
-  Version: $Revision: 1.10 $ %D%
+  Version: $Revision: 1.11 $ %D%
   Module By: Jonathan Abbey	         jonabbey@arlut.utexas.edu
   Applied Research Laboratories, The University of Texas at Austin
 
@@ -57,7 +57,7 @@ import com.sun.java.swing.*;
  * both 'drag-tween' and 'drag on' drag supported.</p>
  *
  * @author Jonathan Abbey
- * @version $Revision: 1.10 $ %D%
+ * @version $Revision: 1.11 $ %D%
  *
  * @see arlut.csd.Tree.treeCallback
  * @see arlut.csd.Tree.treeNode
@@ -2193,7 +2193,10 @@ class treeCanvas extends JComponent implements MouseListener, MouseMotionListene
       {
 	// out of range, deselect all rows
 
-	System.err.println("Hey!  received out of bounds exception, row = " + row);
+	if (debug)
+	  {
+	    System.err.println("Hey!  received out of bounds exception, row = " + row);
+	  }
 
 	ctrl.unselectAllNodes(false);
 	render();
