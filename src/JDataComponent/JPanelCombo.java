673a6f789645d1159a7f3f50001a6cf24aa3a54e
Jonathan Abbey <jonabbey@arlut.utexas.edu>
1998-09-24 03:24:29 +0000

    Made setVectorContents() use DefaultComboBoxModel. -- Jon

--- src/JDataComponent/JPanelCombo.java
+++ src/JDataComponent/JPanelCombo.java
@@ -9,7 +9,7 @@
    But you should extend this, or it doesn't make much sense.
    
    Created: ? April 1998
-   Version: $Revision: 1.3 $ %D%
+   Version: $Revision: 1.4 $ %D%
    Module By: Mike Mulvaney
    Applied Research Laboratories, The University of Texas at Austin
 
@@ -164,7 +164,7 @@ public class JPanelCombo extends JPanel {
 
   public void setVectorContents(Vector vect, Object selected)
   {
-    combo.setModel(new csdComboBoxModel(vect, selected));
+    combo.setModel(new DefaultComboBoxModel(vect));
     setSelectedItem(selected);
   }
 }
