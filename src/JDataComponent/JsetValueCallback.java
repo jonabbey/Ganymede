c7af9d30c38f187a5e54e0d0bb077cdd92c8c5e9
Jonathan Abbey <jonabbey@arlut.utexas.edu>
1998-10-05 13:14:30 +0000

    Formatting. -- Jon

--- src/JDataComponent/JsetValueCallback.java
+++ src/JDataComponent/JsetValueCallback.java
@@ -3,26 +3,28 @@
    JsetValueCallback.java
 
    Created: 18 June 1996
-   Version: $Revision: 1.2 $ %D%
+   Version: $Revision: 1.3 $ %D%
    Module By: Navin Manohar
    Applied Research Laboratories, The University of Texas at Austin
 */
+
 package arlut.csd.JDataComponent;
 
+/*------------------------------------------------------------------------------
+                                                                       interface
+                                                               JsetValueCallback
+
+------------------------------------------------------------------------------*/
+
 /**
- *  
  *  This interface is used to allow callback to be done from the
  *  components in the package to the container which contains them.
  *  The conatiner that is to contain the components needs to provide
  *  implementations for the methods defined in this interface.
  */
+
 public interface JsetValueCallback
- {
-
-  // Variables
-
-  // Interface methods
-  
+{
   public boolean setValuePerformed(JValueObject v) throws java.rmi.RemoteException;
 }
 
