c5cd7767bda25a20b11d84d5f0f964e868a45764
Jonathan Abbey <jonabbey@arlut.utexas.edu>
1998-10-13 02:33:43 +0000

    Made booleanThing serializable, added formatting -- Jon

--- src/JDataComponent/booleanThing.java
+++ src/JDataComponent/booleanThing.java
@@ -1,41 +1,55 @@
-/* 
+/*
+
+   choiceThing.java
+
    Resource class for use with StringDialog.java
+   
+   Created: 16 June 1997
+   Version: $Revision: 1.5 $ %D%
+   Module By: Jonathan Abbey, jonabbey@arlut.utexas.edu
+   Applied Research Laboratories, The University of Texas at Austin
+
 */
 
 package arlut.csd.JDialog;
 
-import java.lang.String;
+/*------------------------------------------------------------------------------
+                                                                           class
+                                                                     choiceThing
 
-public class booleanThing {
+------------------------------------------------------------------------------*/
+
+public class booleanThing implements java.io.Serializable {
 
   String booleanLabel;
   boolean Default;
 
+  /* -- */
+
   public booleanThing(String label)
-    {
-      this(label, false);
-    }
+  {
+    this(label, false);
+  }
 
   public booleanThing(String label, boolean Default)
-    {
-       this.booleanLabel = label;
-       this.Default = Default;
-    }
-
+  {
+    this.booleanLabel = label;
+    this.Default = Default;
+  }
 
   public String getLabel()
-    {
-      return booleanLabel;
-    }
+  {
+    return booleanLabel;
+  }
 
   public Boolean getDefault()
-    {
-      Boolean answer = new Boolean(Default);
-      return answer;
-    }
+  {
+    Boolean answer = new Boolean(Default);
+    return answer;
+  }
 
   public boolean getValue()
-    {
-      return Default;
-    }
+  {
+    return Default;
+  }
 }
