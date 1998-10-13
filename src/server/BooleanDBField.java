81754e13a25ae995fc3a5414b9e9a9c93d2b30fe
Jonathan Abbey <jonabbey@arlut.utexas.edu>
1998-10-13 18:18:24 +0000

    Fixed value() to handle null value. -- Jon

--- src/server/BooleanDBField.java
+++ src/server/BooleanDBField.java
@@ -6,7 +6,7 @@
    The GANYMEDE object storage system.
 
    Created: 2 July 1996
-   Version: $Revision: 1.15 $ %D%
+   Version: $Revision: 1.16 $ %D%
    Module By: Jonathan Abbey
    Applied Research Laboratories, The University of Texas at Austin
 
@@ -164,6 +164,11 @@ public class BooleanDBField extends DBField implements boolean_field {
 
   public boolean value()
   {
+    if (value == null)
+      {
+	return false;
+      }
+    
     return ((Boolean) value).booleanValue();
   }
 
