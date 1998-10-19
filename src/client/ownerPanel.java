d066e46d7cd8675fcca5184abbb084100b4a904a
Jonathan Abbey <jonabbey@arlut.utexas.edu>
1998-10-19 22:11:05 +0000

    Some cleanups, made the 'no owner' label more informative and more attractive. -- Jon

--- src/client/ownerPanel.java
+++ src/client/ownerPanel.java
@@ -5,7 +5,7 @@
    The individual frames in the windowPanel.
    
    Created: 9 September 1997
-   Version: $Revision: 1.20 $ %D%
+   Version: $Revision: 1.21 $ %D%
    Module By: Michael Mulvaney
    Applied Research Laboratories, The University of Texas at Austin
 
@@ -66,11 +66,14 @@ public class ownerPanel extends JPanel implements JsetValueCallback, Runnable {
 
     holdOnPanel = new JPanel();
     holdOnPanel.add(new JLabel("Loading ownerPanel, please wait.", 
-			       new ImageIcon(fp.getWaitImage()), SwingConstants.CENTER));
+			       new ImageIcon(fp.getWaitImage()), 
+			       SwingConstants.CENTER));
       
     add("Center", holdOnPanel);
     invalidate();
     fp.validate();
+
+    // spin off a thread to load the panel.
     
     Thread thread = new Thread(this);
     thread.start();
@@ -85,12 +88,18 @@ public class ownerPanel extends JPanel implements JsetValueCallback, Runnable {
 
     if (field == null)
       {
+	// we'll only get this if we are view-only and there are no owners
+	// for this object registered.. in this case, the object is
+	// effectively owned by supergash.
+
 	if (debug)
 	  {
 	    System.out.println("ownerPanel: field is null, there is no owner for this object.");
 	  }
 
-	JLabel l = new JLabel("There is no owner for this object.");
+	JLabel l = new JLabel("Owned by supergash",
+			      SwingConstants.CENTER);
+
 	remove(holdOnPanel);
 	add("Center", l);
       }
@@ -218,17 +227,19 @@ public class ownerPanel extends JPanel implements JsetValueCallback, Runnable {
     if (availableOwners != null)
       {
 	Invid supergash = new Invid((short)0, 1); // This is supergash
+
 	for (int i = 0; i < availableOwners.size(); i++)
 	  {
 	    listHandle l = (listHandle)availableOwners.elementAt(i);
+
 	    if (supergash.equals(l.getObject()))
 	      {
 		availableOwners.removeElementAt(i);
 		break;
 	      }
-	    
 	  }
       }
+
     if (debug)
       {
 	System.out.println("ownerPanel: creating string selector");
@@ -256,75 +267,89 @@ public class ownerPanel extends JPanel implements JsetValueCallback, Runnable {
 
     /* -- */
 
-    if (o.getSource() instanceof StringSelector) {
-	if (o.getOperationType() == JValueObject.ERROR)
-	  {
-	    fp.getgclient().setStatus((String)o.getValue());
-	  }
-	else if (o.getOperationType() == JValueObject.PARAMETER) {  // From the popup menu
-	  JValueObject v = o; // because this code was originally used with a v
-	  String command = (String)v.getParameter();
-	  
-	  if (command.equals("Edit object"))
-	    {
-	      if (debug)
-		{
-		  System.out.println("Edit object: " + v.getValue());
-		}
-	      
-	      if (v.getValue() instanceof listHandle)
-		{
-		  Invid invid = (Invid)((listHandle)v.getValue()).getObject();
-		  
-		  gc.editObject(invid);
-		}
-	      else if (v.getValue() instanceof Invid)
-		{
-		  if (debug)
-		    {
-		      System.out.println("It's an invid!");
-		    }
+    if (!(o.getSource() instanceof StringSelector))
+      {
+	System.out.println("Where did this setValuePerformed come from?");
+	return false;
+      }
 
-		  Invid invid = (Invid)v.getValue();
-		  
-		  gc.editObject(invid);
-		}
-	      
-	      retVal = null;
-	    }
-	  else if (command.equals("View object"))
-	    {
-	      if (debug)
-		{
-		  System.out.println("View object: " + v.getValue());
-		}
-	      
-	      if (v.getValue() instanceof Invid)
-		{
-		  Invid invid = (Invid)v.getValue();
-		  
-		  gc.viewObject(invid);
-		}
-	      
-	      retVal = null;
-	    }
-	  else if (command.equals("Create new Object")){
+    if (o.getOperationType() == JValueObject.ERROR)
+      {
+	fp.getgclient().setStatus((String)o.getValue());
+      }
+    else if (o.getOperationType() == JValueObject.PARAMETER) 
+      {  // From the popup menu
+	
+	JValueObject v = o; // because this code was originally used with a v
+	String command = (String)v.getParameter();
+	  
+	if (command.equals("Edit object"))
+	  {
+	    if (debug)
+	      {
+		System.out.println("Edit object: " + v.getValue());
+	      }
+		
+	    if (v.getValue() instanceof listHandle)
+	      {
+		Invid invid = (Invid)((listHandle)v.getValue()).getObject();
+		    
+		gc.editObject(invid);
+	      }
+	    else if (v.getValue() instanceof Invid)
+	      {
+		if (debug)
+		  {
+		    System.out.println("It's an invid!");
+		  }
+		    
+		Invid invid = (Invid)v.getValue();
+		    
+		gc.editObject(invid);
+	      }
+		
+	    retVal = null;
+	  }
+	else if (command.equals("View object"))
+	  {
+	    if (debug)
+	      {
+		System.out.println("View object: " + v.getValue());
+	      }
+		
+	    if (v.getValue() instanceof Invid)
+	      {
+		Invid invid = (Invid)v.getValue();
+		    
+		gc.viewObject(invid);
+	      }
+		
+	    retVal = null;
+	  }
+	else if (command.equals("Create new Object"))
+	  {
 	    String label = null;
+
 	    try
 	      {
 		short type = field.getTargetBase();
 		db_object obj = gc.createObject(type, false);
-		
+		    
 		// Find the label field.
 		db_field f = obj.getLabelField();
+
 		if ((f != null) && (f instanceof string_field))
 		  {
 		    if (debug)
 		      {
 			System.out.println("Going to get label for this object.");
 		      }
+
 		    // Set up a label for this object.
-		    DialogRsrc r = new DialogRsrc(gc, "Choose Label for Object", "What would you like to name this object?", "Ok", null);
+
+		    DialogRsrc r = new DialogRsrc(gc, "Choose Label for Object", 
+						  "What would you like to name this object?",
+						  "Ok", null);
 		    r.addString("Label:");
 		    
 		    StringDialog d = new StringDialog(r);
@@ -360,16 +385,16 @@ public class ownerPanel extends JPanel implements JsetValueCallback, Runnable {
 		  {
 		    gc.handleReturnVal(retVal);
 		  }
-
+		    
 		gc.showNewlyCreatedObject(obj, invid, new Short(type));
-		
+		    
 		if ((retVal == null) || retVal.didSucceed())
 		  {
 		    if (debug)
 		      {
 			System.out.println("--Adding it to the StringSelector");
 		      }
-
+			
 		    if (label == null)
 		      {
 			((StringSelector)v.getSource()).addNewItem(new listHandle("New item", invid), true);
@@ -389,52 +414,46 @@ public class ownerPanel extends JPanel implements JsetValueCallback, Runnable {
 		throw new RuntimeException("Exception creating new object from SS menu: " + rx);
 	      }
 	  }	    
-	  else
-	    {
-	      System.out.println("Unknown action command from popup: " + command);
-	    }
-	} // end of popup processing, now it's just an add or remove kind of thing.	  
+	else
+	  {
+	    System.out.println("Unknown action command from popup: " + command);
+	  }
+      } // end of popup processing, now it's just an add or remove kind of thing.
     else if (o.getValue() instanceof Invid)
       {
 	Invid invid = (Invid)o.getValue();
-	    int index = o.getIndex();
-
-	    try
+	int index = o.getIndex();
+	
+	try
+	  {
+	    if (o.getOperationType() == JValueObject.ADD)
 	      {
-		if (o.getOperationType() == JValueObject.ADD)
+		retVal = field.addElement(invid);
+
+		if (retVal != null)
 		  {
-		    retVal = field.addElement(invid);
-
-		    if (retVal != null)
-		      {
-			gc.handleReturnVal(retVal);
-		      }
-
-		    succeeded = (retVal == null) ? true : retVal.didSucceed();
-
+		    gc.handleReturnVal(retVal);
 		  }
-		else if (o.getOperationType() == JValueObject.DELETE)
-		  {
-		    retVal = field.deleteElement(invid);
 
-		    if (retVal != null)
-		      {
-			gc.handleReturnVal(retVal);
-		      }
+		succeeded = (retVal == null) ? true : retVal.didSucceed();
 
-		    succeeded = (retVal == null) ? true : retVal.didSucceed();
-
-		  }
 	      }
-	    catch (RemoteException rx)
+	    else if (o.getOperationType() == JValueObject.DELETE)
 	      {
-		throw new RuntimeException("Could not change owner field: " + rx);
+		retVal = field.deleteElement(invid);
+
+		if (retVal != null)
+		  {
+		    gc.handleReturnVal(retVal);
+		  }
+
+		succeeded = (retVal == null) ? true : retVal.didSucceed();
 	      }
 	  }
-      }
-    else
-      {
-	System.out.println("Where did this setValuePerformed come from?");
+	catch (RemoteException rx)
+	  {
+	    throw new RuntimeException("Could not change owner field: " + rx);
+	  }
       }
     
     if (succeeded)
