03a085440f9a3705ce945722eb0c0a23669eadd2
Jonathan Abbey <jonabbey@arlut.utexas.edu>
1998-10-20 16:37:59 +0000

    Bumped up the number of concurrent connections allowed to 50. -- Jon

--- src/server/Ganymede.java
+++ src/server/Ganymede.java
@@ -12,7 +12,7 @@
    start method.
 
    Created: 17 January 1997
-   Version: $Revision: 1.55 $ %D%
+   Version: $Revision: 1.56 $ %D%
    Module By: Jonathan Abbey
    Applied Research Laboratories, The University of Texas at Austin
 
@@ -283,7 +283,7 @@ public class Ganymede {
       {
 	debug("Creating GanymedeServer object");
 
-	server = new GanymedeServer(10);
+	server = new GanymedeServer(50);
 
 	debug("Binding GanymedeServer in RMI Registry");
 
