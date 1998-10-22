cc8ab12514f3d5545252dc1d5a1a3f053f1cdc09
Jonathan Abbey <jonabbey@arlut.utexas.edu>
1998-10-22 14:13:17 +0000

    Made addWindow() ask an object for its type name if objectType is null. -- Jon

--- src/client/windowPanel.java
+++ src/client/windowPanel.java
@@ -5,7 +5,7 @@
    The window that holds the frames in the client.
    
    Created: 11 July 1997
-   Version: $Revision: 1.49 $ %D%
+   Version: $Revision: 1.50 $ %D%
    Module By: Michael Mulvaney
    Applied Research Laboratories, The University of Texas at Austin
 
@@ -274,7 +274,7 @@ public class windowPanel extends JDesktopPane implements InternalFrameListener, 
       {
 	if (objectType == null)
 	  {
-	    title = object.getLabel();
+	    title = object.getTypeName() + " - " + object.getLabel();
 	  }
 	else
 	  {
