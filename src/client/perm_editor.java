3a11d4ecc8eab9848ae95a82db822e4313df9833
Brian OMara <omara@arlut.utexas.edu>
1998-11-18 15:10:38 +0000

    Initial revision

---
+++ src/client/perm_editor.java
@@ -0,0 +1,14 @@
+/*
+
+   brian_editor.java
+
+   Description.
+   
+   Created: 18 November 1998
+   Version: $Revision: 1.1 $ %D%
+   Module By: Brian O'Mara omara@arlut.utexas.edu
+   Applied Research Laboratories, The University of Texas at Austin
+
+*/
+
+package arlut.csd.ganymede.client;
