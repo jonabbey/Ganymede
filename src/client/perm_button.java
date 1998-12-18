c2a4bdf53e848cfee257ee52db756be3b4820a56
Brian OMara <omara@arlut.utexas.edu>
1998-12-18 15:52:51 +0000

    added checking to see if editor already exists to avoid inadvertent double-clicks from creating more than one editor before the modal state becomes active. -Brian

--- src/client/perm_button.java
+++ src/client/perm_button.java
@@ -5,7 +5,7 @@
    Description.
    
    Created: 20 January 1997
-   Version: $Revision: 1.8 $ %D%
+   Version: $Revision: 1.9 $ %D%
    Module By: Erik Grostic
               Jonathan Abbey
    Applied Research Laboratories, The University of Texas at Austin
@@ -31,16 +31,17 @@ import javax.swing.border.*;
 ------------------------------------------------------------------------------*/
 
 class perm_button extends JButton implements ActionListener {
-
+  
   perm_field field;
   boolean enabled;
   Hashtable basehash;
   gclient gc;
   String title;
   JCheckBox tableView;
-
+  perm_editor editor = null;
+  boolean isActiveAlready = false;
   /* -- */
-
+  
   /**
    *
    * perm_button constructor
@@ -57,66 +58,75 @@ class perm_button extends JButton implements ActionListener {
 		      gclient gc,
 		      boolean justShowUser,
 		      String title)
-  {
-
-
-    // Commented out stuff is to allow access to "good" editor while testing another
-
-    //    JLabel label = new JLabel("myLabel");
-    //    tableView = new JCheckBox("Table View",false);
-    //    label.setBorder(new BevelBorder(BevelBorder.RAISED));
-
-    if (enabled)
-      {
-	setText("Edit Permissions");
-      }
-    else
-      {
-	setText("View Permissions");
-      }
-
-//     this.setLayout(new BorderLayout());
-//     this.add(label, "North");
-//     this.add(tableView, "South");
-
-    this.field = field;
-    this.enabled = enabled;
-    this.gc = gc;
-    this.title = title;
-    
-    addActionListener(this);
-  }
-
+    {
+      
+      
+      // Commented out stuff is to allow access to "good" editor while testing another
+      
+      //    JLabel label = new JLabel("myLabel");
+      //    tableView = new JCheckBox("Table View",false);
+      //    label.setBorder(new BevelBorder(BevelBorder.RAISED));
+      
+      if (enabled)
+	{
+	  setText("Edit Permissions");
+	}
+      else
+	{
+	  setText("View Permissions");
+	}
+      
+      //     this.setLayout(new BorderLayout());
+      //     this.add(label, "North");
+      //     this.add(tableView, "South");
+      
+      this.field = field;
+      this.enabled = enabled;
+      this.gc = gc;
+      this.title = title;
+      
+      addActionListener(this);
+    }
+  
   /**
    * When clicked, this button invokes an instance of the permissions
    * editor class.
    */
   
   public void actionPerformed(ActionEvent e)
-  {
-    if ((e.getSource() == this)) // && (tableView.isSelected()))
-      {
-// 	System.out.println("Edit Button was pushed- table selected");
-	
-// 	Frame parent = new Frame();
-// 	perm_editor editor = new perm_editor(field, 
-// 					     enabled, gc, 
-// 					     parent, "Permissions Editor: " + title);
-
-// 	System.out.println("Editor Created by perm button");
-//       }
+    {
+      if ((e.getSource() == this)) // && (tableView.isSelected()))
+	{
+	  // 	System.out.println("Edit Button was pushed- table selected");
+	  
+	  // 	Frame parent = new Frame();
+	  // 	perm_editor editor = new perm_editor(field, 
+	  // 					     enabled, gc, 
+	  // 					     parent, "Permissions Editor: " + title);
+	  
+	  // 	System.out.println("Editor Created by perm button");
+	  //       }
  
-//     else {
+	  //     else {
+	  
+	  System.out.println("Edit Button was pushed- table not selected");
+	  
 
-      System.out.println("Edit Button was pushed- table not selected");
+	  // Need to take care of accidental double clicks resulting in two or more instances 
+	  // of perm_editor being created. Even though perm_editor is modal, there is a small 
+	  // gap in time between clicking the perm button and the modal state taking effect- 
+	  // enough time for multiple clicks on the button to create multiple editors. 
+	  if ((editor == null) || (!editor.isActiveEditor())) { 
+	    Frame parent = new Frame();
+	    editor = new perm_editor(field, 
+				     enabled, gc, 
+				     parent, "Permissions Editor: " + title);
+	    
+	    System.out.println("Editor Created by perm button");
+	  } else {
+	    System.out.println("An editor already exists- new one not created");
+	  }
+	}
       
-      Frame parent = new Frame();
-      perm_editor editor = new perm_editor(field, 
-					   enabled, gc, 
-					   parent, "Permissions Editor: " + title);
-      
-      System.out.println("Editor Created by perm button");
     }
-    
-  }
 }
