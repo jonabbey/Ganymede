f5596dc92418b17b500df8ce147327feb9c12013
Jonathan Abbey <jonabbey@arlut.utexas.edu>
1999-01-13 22:26:20 +0000

    Build and run script files

---
+++ src/password/config.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#
+# Ganymede password client build configure variables
+#
+####################################################
+
+JAVAC=/opt/depot/java-1.2b3/bin/javac
+RMIC=/opt/depot/java-1.2b3/bin/rmic
+JAR=/opt/bin/jar
+SOURCE=/home/broccol/gash2/code/arlut/csd/ganymede/client/password
+CLASSDIR=/home/broccol/gash2/code/arlut/csd/ganymede/client/password/classes
+JARDIR=/home/broccol/gash2/classes
+TARGETJAR=/home/broccol/gash2/code/arlut/csd/ganymede/client/password/password.jar
