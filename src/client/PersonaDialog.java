65b6e083d95801d7bd54b8b2c501d1d93ac3fc79
Brian OMara <omara@arlut.utexas.edu>
1999-02-26 20:48:04 +0000

    Fixed browser dialog display problem and rearranged gui code to a slightly more logical structure. Moved updatePersonaMenu() from gclient to PersonaDialog. Its only function is to update which button on the persona dialog is shown as selected, so it seemed more logical to make it part of PersonaDialog (so you can say something like personaDialog.updatePersonaMenu() )

--- src/client/PersonaDialog.java
+++ src/client/PersonaDialog.java
@@ -6,8 +6,8 @@
    
    Created: 17 February 1999
    Release: $Name:  $
-   Version: $Revision: 1.1 $
-   Last Mod Date: $Date: 1999/02/23 23:50:45 $
+   Version: $Revision: 1.2 $
+   Last Mod Date: $Date: 1999/02/26 20:48:04 $
    Module By: Brian O'Mara
 
    -----------------------------------------------------------------------
@@ -70,19 +70,9 @@ public class PersonaDialog extends JCenterDialog implements ActionListener{
   gclient 
     gc;
 
-  JLabel 
-    passLabel;
-
-  JPasswordField
+  private JPasswordField
     password;
 
-  JPanel 
-    passPanel,
-    personaPanel;
-
-  Box 
-    personaBox;
-  
   PersonaListener 
     personaListener;
 
@@ -104,83 +94,93 @@ public class PersonaDialog extends JCenterDialog implements ActionListener{
     this.my_username = gc.my_username;
     this.currentPersonaString = gc.currentPersonaString;
 
-    personaPanel = new JPanel(new BorderLayout());
+
+    // Main dialog container
+
+    JPanel pane = new JPanel(new BorderLayout());
+    this.setContentPane(pane);
+
+    JPanel topPanel = new JPanel(new BorderLayout()); 
+    JPanel buttonPanel = new JPanel();
+
+    pane.add("Center", topPanel); // Personae go here.
+    pane.add("South", buttonPanel); // "OK" button.
+    
+
+    // "OK" button
+
+    login = new JButton("OK");
+    buttonPanel.add(login);
+    login.addActionListener(personaListener);
+    login.addActionListener(this);
+
+
+    // Panel to hold persona radiobuttons and pass field
+
+    JPanel personaPanel = new JPanel(new BorderLayout());
     personaPanel.setBorder(new TitledBorder(new EtchedBorder(),
 					"Select Persona",
 					TitledBorder.LEFT,
 					TitledBorder.TOP));
 
-    passLabel = new JLabel("Password:");
-    password = new JPasswordField(10);
-    password.addActionListener(this);
-
-    passPanel = new JPanel(new BorderLayout());
-    passPanel.add("West",passLabel);
-    passPanel.add("Center",password);
-    personaBox = Box.createVerticalBox();
-    personaGroupRB = new ButtonGroup();
-
-    if ((personae != null)  && personae.size() > 1)
-      {
-	if (debug)
-	  {
-	    System.out.println("Adding persona stuff");
-	  }
-	
-	for (int i= personae.size()-1;i >=0; i--)
-	  {
-	    String p = (String)personae.elementAt(i);
-	    JRadioButton rb = new JRadioButton(p);
-	    if (p.equals(my_username))
-	      {
-		currentPersonaString = p;
-		rb.doClick(); // note that only selects initial persona "username".
-		              // Does no action b/c actionlisterer not added yet. 
-	      }
-
-	    rb.addActionListener(personaListener);
-	    personaGroupRB.add(rb);
-	    personaBox.add(rb);
-	  }
-	personaBox.add(Box.createVerticalStrut(25));
-	personaBox.add(passPanel);
-	personaPanel.add("North", personaBox);
-      }
-    else if (debug)
-      {
-	System.out.println("No personas.");
-      }
-
-    JPanel topPanel = new JPanel(new BorderLayout()); 
-    JPanel buttonPanel = new JPanel();
-    JPanel pane = new JPanel(new BorderLayout());
-
     topPanel.add("Center", personaPanel);
     topPanel.add("South", new JSeparator());
 
-    // Create the button Panel for the bottom
-    login = new JButton("OK");
-    login.addActionListener(personaListener);
-    login.addActionListener(this);
-    buttonPanel.add(login);
+    password = new JPasswordField(10);
+    password.addActionListener(this);
 
-    pane.add("Center", topPanel);
-    pane.add("South", buttonPanel);
-    this.setContentPane(pane);
-    pack();
-    updatePassField(currentPersonaString);
+    JPanel passPanel = new JPanel(new BorderLayout());
+    passPanel.add("West",new JLabel("Password: "));
+    passPanel.add("Center",password);
+    
+    Box personaBox = Box.createVerticalBox();
+    personaGroupRB = new ButtonGroup();
+    
+    if (debug) { System.out.println("Adding persona stuff"); }
+    
+    
+    // Build up persona list as radiobutton group
+    
+    for (int i= personae.size()-1;i >=0; i--)
+      {
+	String p = (String)personae.elementAt(i);
+	JRadioButton rb = new JRadioButton(p);
+	if (p.equals(currentPersonaString))
+	  {
+	    rb.doClick(); 
+	    // No actionListener yet, so just selects current username
+	  }
 	
+	rb.addActionListener(personaListener);
+	personaGroupRB.add(rb);
+	personaBox.add(rb);
+
+      }
+
+    // Give a little space b/t buttons and pass field
+    personaBox.add(Box.createVerticalStrut(25));
+
+    personaBox.add(passPanel);
+    personaPanel.add("North", personaBox);
+    
+
+    pack();
+    pane.revalidate(); // Win95 browser fix??
+    updatePassField(currentPersonaString);
   }
-
-
+  
+  
   public void actionPerformed(ActionEvent e)
     {
       // Hitting enter after typing pass is like clicking OK.
+
       if (e.getSource() instanceof JPasswordField) {
 	login.doClick();
       }
+
       else {
 	// Clicking OK hides the dialog
+
 	setVisible(false);
       }
     }
@@ -188,6 +188,7 @@ public class PersonaDialog extends JCenterDialog implements ActionListener{
   public void layout(int width, int height)
   {
     // Uses a special pack in JCenterDialog
+
     pack(width, height);
   }
 
@@ -199,29 +200,65 @@ public class PersonaDialog extends JCenterDialog implements ActionListener{
     return new String(password.getPassword());
   }
 
-  // Called when RadioButton is toggled.
-  // Updates focus and testbox appropriately.
+
+  // updatePassField called when RadioButton is toggled.
+  // Updates focus and textfield appropriately.
+
   void updatePassField(String newPersona) {
     this.newPersona = newPersona;
+
     if (newPersona.equals(my_username)) {
       password.setText("");
-      password.setEnabled(false);
       password.setEditable(false);
       password.setBackground(Color.lightGray);
       login.requestFocus();
     }
     else {
       password.setText("");
-      password.setEnabled(true);
       password.setEditable(true);
       password.setBackground(Color.white);
       password.requestFocus();
     }
   }
 
+  public void updatePersonaMenu()
+  {
+    if (debug) { System.out.println("--Updating persona menu"); }
+
+    if (personaGroupRB != null)
+      {
+	Enumeration RButtons = personaGroupRB.getElements();
+	
+	while (RButtons.hasMoreElements())
+	  {
+	    // Weed out nonRadioButtons if exist
+	    Object next = RButtons.nextElement();
+	    if (!(next instanceof JRadioButton)) {
+	      break;
+	    }
+	    else {
+	      JRadioButton rb = (JRadioButton)next;
+	      if (rb.getActionCommand().equals(currentPersonaString))
+		{
+		  if (debug) { System.out.println("Calling setState(true)"); }
+		  
+		  rb.removeActionListener(personaListener);
+		  rb.doClick();
+		  rb.requestFocus();
+		  rb.addActionListener(personaListener);
+		  updatePassField(currentPersonaString);
+		  break; // Selecting this one sets others to false
+		}
+	    }
+	  }
+      }
+  }
+
+
   // Access to most recent RButton selection. 
   String getNewPersona(){
     return newPersona;
   }
+
 }
 
