8d71c3c7f0fcca59d0b3a344fb02d4ab42e01359
Jonathan Abbey <jonabbey@arlut.utexas.edu>
2001-05-31 05:39:50 +0000

    Made the client give better differentiation when reporting commit errors

--- src/client/gclient.java
+++ src/client/gclient.java
@@ -4,8 +4,8 @@
    Ganymede client main module
 
    Created: 24 Feb 1997
-   Version: $Revision: 1.185 $
-   Last Mod Date: $Date: 2001/04/12 10:22:59 $
+   Version: $Revision: 1.186 $
+   Last Mod Date: $Date: 2001/05/31 05:39:50 $
    Release: $Name:  $
 
    Module By: Mike Mulvaney, Jonathan Abbey, and Navin Manohar
@@ -59,6 +59,7 @@ import java.applet.*;
 import java.net.*;
 import java.rmi.*;
 import java.rmi.server.*;
+import java.io.*;
 import java.util.*;
 import java.beans.PropertyVetoException;
 
@@ -91,7 +92,7 @@ import javax.swing.plaf.basic.BasicToolBarUI;
  * treeControl} GUI component displaying object categories, types, and instances
  * for the user to browse and edit.</p>
  *
- * @version $Revision: 1.185 $ $Date: 2001/04/12 10:22:59 $ $Name:  $
+ * @version $Revision: 1.186 $ $Date: 2001/05/31 05:39:50 $ $Name:  $
  * @author Mike Mulvaney, Jonathan Abbey, and Navin Manohar
  */
 
@@ -131,8 +132,24 @@ public class gclient extends JFrame implements treeCallback, ActionListener, Jse
   static final int OBJECTNOWRITE = 16;
 
   static String release_name = "$Name:  $";
-  static String release_date = "$Date: 2001/04/12 10:22:59 $";
+  static String release_date = "$Date: 2001/05/31 05:39:50 $";
   static String release_number = null;
+
+  /**
+   * This is a convenience method used by the server to get a
+   * stack trace from a throwable object in String form.
+   */
+
+  static public String stackTrace(Throwable thing)
+  {
+    StringWriter stringTarget = new StringWriter();
+    PrintWriter writer = new PrintWriter(stringTarget);
+    
+    thing.printStackTrace(writer);
+    writer.close();
+
+    return stringTarget.toString();
+  }
 
   // ---
 
@@ -3996,7 +4013,7 @@ public class gclient extends JFrame implements treeCallback, ActionListener, Jse
 	    retVal = handleReturnVal(retVal);
 	  }
 
-	succeeded = (retVal == null) ? true : retVal.didSucceed();
+	succeeded = ((retVal == null) || retVal.didSucceed());
 
 	// if we succeed, we clean up.  If we don't,
 	// retVal.doNormalProcessing can be false, in which case the
@@ -4039,13 +4056,13 @@ public class gclient extends JFrame implements treeCallback, ActionListener, Jse
       }
     catch (RemoteException rx)
       {
-	throw new RuntimeException("Could not commit transaction" + rx);
+	rx.printStackTrace();
+	showErrorMessage("Remote Exception during commit:\n\n" + stackTrace(rx));
       }
     catch (Exception e)
       {
 	e.printStackTrace();
-	showErrorMessage("Exception during commit: " + e.getMessage());
-	throw new RuntimeException("Exception during commit: " + e.getMessage());
+	showErrorMessage("Local Exception during commit:\n\n" + stackTrace(e));
       }
     finally
       {
