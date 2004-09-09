477b4342259e3ce7f655427fe6cb61fe88ec167f
Jonathan Abbey <jonabbey@arlut.utexas.edu>
2004-09-09 00:06:16 +0000

    Comment fix.
    -- Jon

--- ddroid/src/ddroid/arlut/csd/ddroid/admin/GASHAdmin.java
+++ ddroid/src/ddroid/arlut/csd/ddroid/admin/GASHAdmin.java
@@ -101,7 +101,7 @@ import arlut.csd.ddroid.rmi.Server;
  *
  * <P>GASHAdmin connects to a running
  * {@link arlut.csd.ddroid.server.GanymedeServer GanymedeServer} using the 
- * {@link arlut.csd.ddroid.server.GanymedeServer#admin(arlut.csd.ganymede.Admin) admin()}
+ * {@link arlut.csd.ddroid.server.GanymedeServer#admin(arlut.csd.ddroid.common.Admin) admin()}
  * method.</P>
  */
 
