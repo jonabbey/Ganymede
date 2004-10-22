fae44f4dc7fe467970fe043f9135133d1d5f1110
Jonathan Abbey <jonabbey@arlut.utexas.edu>
2004-10-22 22:15:54 +0000

    Minor variation, email fix test.
    -- Jon

--- src/ganymede/arlut/csd/JDataComponent/JPropertyPanel.java
+++ src/ganymede/arlut/csd/JDataComponent/JPropertyPanel.java
@@ -290,11 +290,6 @@ public class JPropertyPanel extends JPanel implements ActionListener {
    */
 
   public static void main(String[] args) {
-    /*try {
-      UIManager.setLookAndFeel( UIManager.getCrossPlatformLookAndFeelClassName() );
-    } 
-    catch (Exception e) { }*/
-
     System.out.println("main");
 
     JFrame frame = new JFrame("SwingApplication");
