498239d04d1b8817aa515217b573b81c2a20bf63
Jonathan Abbey <jonabbey@arlut.utexas.edu>
2005-02-03 02:26:25 +0000

    Added SyncRunner, which is pretty close to done, and syncChannelCustom, which needs a good bit more work.
    -- Jon

---
+++ src/ganymede/arlut/csd/ganymede/server/SyncRunner.java
@@ -0,0 +1,209 @@
+/*
+
+   SyncRunner.java
+
+   This class is used in the Ganymede server to handle sync channel
+   synchronization.  It is responsible for acting as a Runnable in the
+   Ganymede scheduler (when run, it executes the external service program for
+   a given sync channel queue) and for tracking the data associated with the
+   sync channel.
+   
+   Created: 2 February 2005
+   Last Mod Date: $Date$
+   Last Revision Changed: $Rev$
+   Last Changed By: $Author$
+   SVN URL: $HeadURL$
+
+   Module By: Jonathan Abbey, jonabbey@arlut.utexas.edu
+
+   -----------------------------------------------------------------------
+	    
+   Ganymede Directory Management System
+ 
+   Copyright (C) 1996-2005
+   The University of Texas at Austin
+
+   Contact information
+
+   Author Email: ganymede_author@arlut.utexas.edu
+   Email mailing list: ganymede@arlut.utexas.edu
+
+   US Mail:
+
+   Computer Science Division
+   Applied Research Laboratories
+   The University of Texas at Austin
+   PO Box 8029, Austin TX 78713-8029
+
+   Telephone: (512) 835-3200
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+   02111-1307, USA
+
+*/
+
+package arlut.csd.ganymede.server;
+
+import arlut.csd.ganymede.common.Invid;
+import arlut.csd.ganymede.common.SchemaConstants;
+
+import arlut.csd.Util.FileOps;
+import arlut.csd.Util.TranslationService;
+
+/*------------------------------------------------------------------------------
+                                                                           class
+                                                                      SyncRunner
+
+------------------------------------------------------------------------------*/
+
+/**
+ * <p>This class is used in the Ganymede server to handle sync channel
+ * synchronization.  It is responsible for acting as a Runnable in the
+ * Ganymede scheduler (when run, it executes the external service program for
+ * a given sync channel queue) and for tracking the data associated with the
+ * sync channel.</p>
+ */
+
+public class SyncRunner implements Runnable {
+
+  /**
+   * <p>TranslationService object for handling string localization in
+   * the Ganymede server.</p>
+   */
+
+  static final TranslationService ts = TranslationService.getTranslationService("arlut.csd.ganymede.server.SyncRunner");
+
+  // ---
+
+  private Invid syncChannelInvid;
+  private String name;
+  private String directory;
+  private String serviceProgram;
+  private int transactionNumber;
+
+  /* -- */
+
+  public SyncRunner(DBObject syncChannel)
+  {
+    updateInfo(syncChannel);
+  }
+
+  public void updateInfo(DBObject syncChannel)
+  {
+    if (syncChannel.getID() != SchemaConstants.SyncChannelBase)
+      {
+	// "Error, passed the wrong kind of DBObject."
+	throw new IllegalArgumentException(ts.l("updateInfo.typeError"));
+      }
+
+    this.syncChannelInvid = syncChannel.getInvid();
+    this.name = (String) syncChannel.getFieldValueLocal(SchemaConstants.SyncChannelName);
+    this.directory = (String) syncChannel.getFieldValueLocal(SchemaConstants.SyncChannelDirectory);
+    this.serviceProgram = (String) syncChannel.getFieldValueLocal(SchemaConstants.SyncChannelServicer);
+  }
+
+  /**
+   * <p>Used to set the number of the last transaction known to have been
+   * safely persisted to disk (journal and sync channels) at the time this
+   * method is called.</p>
+   */
+
+  public void setTransactionNumber(int trans)
+  {
+    if (trans >= this.transactionNumber)
+      {
+	this.transactionNumber = trans;
+      }
+    else
+      {
+	// "Error, can''t set the persisted transaction number to a lower number than previously seen."
+	throw new IllegalArgumentException(ts.l("setTransactionNumber.badNumber"));
+      }
+  }
+
+  /**
+   * <p>Returns the number of the last transaction known to have been
+   * safely persisted to disk (journal and sync channels) at the time this
+   * method is called.</p>
+   */
+
+  public int getTransactionNumber()
+  {
+    return this.transactionNumber;
+  }
+
+  public String getName()
+  {
+    return name;
+  }
+
+  public String getDirectory()
+  {
+    return directory;
+  }
+
+  public String getServiceProgram()
+  {
+    return serviceProgram;
+  }
+
+  public void run()
+  {
+    int myTransactionNumber = getTransactionNumber();
+    String myServiceProgram = getServiceProgram();
+    String invocation = myServiceProgram + " " + String.valueOf(myTransactionNumber);
+
+    File
+      file;
+
+    /* -- */
+
+    // "SyncChannel {0} running"
+    Ganymede.debug(ts.l("run.running", getName()));
+
+    file = new File(myServiceProgram);
+
+    if (file.exists())
+      {
+	if (runtime == null)
+	  {
+	    runtime = Runtime.getRuntime();
+	  }
+
+	try
+	  {
+	    FileOps.runProcess(invocation);
+	  }
+	catch (IOException ex)
+	  {
+	    // "Couldn''t exec SyncChannel {0}''s service program "{1}" due to IOException: {2}"
+	    Ganymede.debug(ts.l("run.ioException", getName(), myServiceProgram, ex));
+	  }
+	catch (InterruptedException ex)
+	  {
+	    // "Failure during exec of SyncChannel {0}''s service program "{1}""
+	    Ganymede.debug(ts.l("run.interrupted", getName(), myServiceProgram, ex));
+	  }
+      }
+    else
+      {
+	// ""{0}" doesn''t exist, not running external service program for SyncChannel {1}"
+	Ganymede.debug(ts.l("run.nonesuch", myServiceProgram, getName()));
+      }
+
+    // "SyncChannel {1} finished"
+    Ganymede.debug(ts.l("run.done", getName()));
+  }
+}
---
+++ src/ganymede/arlut/csd/ganymede/server/syncChannelCustom.java
@@ -0,0 +1,239 @@
+/*
+
+   syncChannelCustom.java
+
+   This file is a management class for sync channel definitions in Ganymede.
+   
+   Created: 2 February 2005
+   Last Mod Date: $Date$
+   Last Revision Changed: $Rev$
+   Last Changed By: $Author$
+   SVN URL: $HeadURL$
+
+   Module By: Jonathan Abbey, jonabbey@arlut.utexas.edu
+
+   -----------------------------------------------------------------------
+	    
+   Ganymede Directory Management System
+ 
+   Copyright (C) 1996-2005
+   The University of Texas at Austin
+
+   Contact information
+
+   Author Email: ganymede_author@arlut.utexas.edu
+   Email mailing list: ganymede@arlut.utexas.edu
+
+   US Mail:
+
+   Computer Science Division
+   Applied Research Laboratories
+   The University of Texas at Austin
+   PO Box 8029, Austin TX 78713-8029
+
+   Telephone: (512) 835-3200
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+   02111-1307, USA
+
+*/
+
+package arlut.csd.ganymede.server;
+
+import java.rmi.RemoteException;
+
+import arlut.csd.ganymede.common.Invid;
+import arlut.csd.ganymede.common.NotLoggedInException;
+import arlut.csd.ganymede.common.QueryResult;
+import arlut.csd.ganymede.common.ReturnVal;
+import arlut.csd.ganymede.common.SchemaConstants;
+
+/*------------------------------------------------------------------------------
+                                                                           class
+                                                               syncChannelCustom
+
+------------------------------------------------------------------------------*/
+
+/**
+ *
+ * This class is a DBEditObject subclass for handling fields in the Ganymede
+ * server's sync channel object type.
+ *
+ */
+
+public class syncChannelCustom extends DBEditObject implements SchemaConstants {
+
+  /**
+   *
+   * Customization Constructor
+   *
+   */
+
+  public syncChannelCustom(DBObjectBase objectBase) throws RemoteException
+  {
+    super(objectBase);
+  }
+
+  /**
+   *
+   * Create new object constructor
+   *
+   */
+
+  public syncChannelCustom(DBObjectBase objectBase, Invid invid, DBEditSet editset) throws RemoteException
+  {
+    super(objectBase, invid, editset);
+  }
+
+  /**
+   *
+   * Check-out constructor, used by DBObject.createShadow()
+   * to pull out an object for editing.
+   *
+   */
+
+  public syncChannelCustom(DBObject original, DBEditSet editset) throws RemoteException
+  {
+    super(original, editset);
+  }
+
+  /**
+   *
+   * Customization method to control whether a specified field
+   * is required to be defined at commit time for a given object.<br><br>
+   *
+   * To be overridden in DBEditObject subclasses.<br><br>
+   *
+   * Note that this method will not be called if the controlling
+   * GanymedeSession's enableOversight is turned off, as in
+   * bulk loading.<br><br>
+   *
+   * <b>*PSEUDOSTATIC*</b>
+   *
+   */
+
+  public boolean fieldRequired(DBObject object, short fieldid)
+  {
+    switch (fieldid)
+      {
+      case SchemaConstants.SyncChannelName:
+      case SchemaConstants.SyncChannelDirectory:
+	return true;
+      }
+
+    // We'll allow SyncChannelServicer, SyncChannelFields, and
+    // SyncChannelPlaintextOK to be false or undefined
+
+    return false;
+  }
+
+  /**
+   *
+   * This method is a hook for subclasses to override to
+   * pass the phase-two commit command to external processes.<br><br>
+   *
+   * For normal usage this method would not be overridden.  For
+   * cases in which change to an object would result in an external
+   * process being initiated whose <b>success or failure would not
+   * affect the successful commit of this DBEditObject in the
+   * Ganymede server</b>, the process invokation should be placed here,
+   * rather than in commitPhase1().<br><br>
+   *
+   * Subclasses that override this method may wish to make this method 
+   * synchronized.
+   *
+   * @see arlut.csd.ganymede.server.DBEditSet
+   */
+
+  public void commitPhase2()
+  {
+    String origName = null, channelName;
+
+    /* -- */
+
+    if (original != null)
+      {
+	origName = (String) original.getFieldValueLocal(SchemaConstants.SyncChannelName);
+      }
+
+    channelName = (String) getFieldValueLocal(SchemaConstants.SyncChannelName);
+
+    switch (getStatus())
+      {
+      case DROPPING:
+	return;
+
+      case DELETING:
+	Ganymede.scheduler.unregisterTask(origName);
+
+	if (original.isSet(SchemaConstants.TaskRunOnCommit))
+	  {
+	    Ganymede.unregisterBuilderTask(origName);
+	  }
+
+	break;
+
+      case EDITING:
+	if (!origName.equals(taskName))
+	  {
+	    // we changed our task name.. ditch the old record
+
+	    Ganymede.scheduler.unregisterTask(origName);
+
+	    if (original.isSet(SchemaConstants.TaskRunOnCommit))
+	      {
+		Ganymede.unregisterBuilderTask(origName);
+	      }
+
+	    // and re-register ourselves appropriately
+
+	    Ganymede.scheduler.registerTaskObject(this);
+
+	    if (isSet(SchemaConstants.TaskRunOnCommit))
+	      {
+		Ganymede.registerBuilderTask(taskName);
+	      }
+	  }
+	else
+	  {
+	    // no name change, go ahead and reschedule ourselves
+
+	    Ganymede.scheduler.registerTaskObject(this);
+
+	    // get the buildertask list updated to handle this task
+	    // on transaction commit appropriately
+
+	    if (original.isSet(SchemaConstants.TaskRunOnCommit) && !isSet(SchemaConstants.TaskRunOnCommit))
+	      {
+		Ganymede.unregisterBuilderTask(origName);
+	      }
+	    else if (isSet(SchemaConstants.TaskRunOnCommit) && !original.isSet(SchemaConstants.TaskRunOnCommit))
+	      {
+		Ganymede.registerBuilderTask(taskName);
+	      }
+	  }
+	break;
+
+      case CREATING:
+	Ganymede.scheduler.registerTaskObject(this);
+
+	if (isSet(SchemaConstants.TaskRunOnCommit))
+	  {
+	    Ganymede.registerBuilderTask(taskName);
+	  }
+      }
+  }
+
+}
