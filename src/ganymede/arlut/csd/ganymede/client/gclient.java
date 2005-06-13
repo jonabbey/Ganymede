5498eb7670ae89f7729000308eb404eced802808
Jonathan Abbey <jonabbey@arlut.utexas.edu>
2005-06-13 16:25:16 +0000

    Make the Ganymede client do a case-insensitive sort.
    -- Jon

--- src/ganymede/arlut/csd/ganymede/client/gclient.java
+++ src/ganymede/arlut/csd/ganymede/client/gclient.java
@@ -5424,7 +5424,7 @@ public class gclient extends JFrame implements treeCallback, ActionListener, Jse
 	  bF = (listHandle) b;
 	  int comp = 0;
 	  
-	  comp =  aF.toString().compareTo(bF.toString());
+	  comp =  aF.toString().toLowerCase().compareTo(bF.toString().toLowerCase());
 	  
 	  if (comp < 0)
 	    {
