5519cf9073fad9dbf014674bf2681553d877cb92
Jonathan Abbey <jonabbey@arlut.utexas.edu>
2009-01-07 19:11:52 -0600

    Added arlut.csd.Util.BrowserLauncher.
    BrowserLauncher is a utility class for launching the web browser on the
    user's desktop, provided that Java 6's java.awt.Desktop class is available
    on the system running this class.
    
    Despite currently working only on Java 6, this class is designed to compile
    cleanly under earlier versions of Java and simply fail at run time if the
    platform support is not there for launching a web browser to view a given
    URL.
    
    -- Jon

---
+++ src/ganymede/arlut/csd/Util/BrowserLauncher.java
@@ -0,0 +1,130 @@
+/*
+
+   BrowserLauncher.java
+
+   Created: 7 January 2009
+   Last Commit: $Format:%cd$
+
+   Module By: Jonathan Abbey, jonabbey@arlut.utexas.edu
+
+   -----------------------------------------------------------------------
+	    
+   Ganymede Directory Management System
+ 
+   Copyright (C) 1996 - 2009
+   The University of Texas at Austin
+
+   Contact information
+
+   Web site: http://www.arlut.utexas.edu/gash2
+   Author Email: ganymede_author@arlut.utexas.edu
+   Email mailing list: ganymede@arlut.utexas.edu
+
+   US Mail:
+
+   Computer Science Division
+   Applied Research Laboratories
+   The University of Texas at Austin
+   PO Box 8029, Austin TX 78713-8029
+
+   Telephone: (512) 835-3200
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+   02111-1307, USA
+
+*/
+
+package arlut.csd.Util;
+
+import java.net.URI;
+import java.lang.reflect.Method;
+
+/*------------------------------------------------------------------------------
+                                                                           class
+                                                                 BrowserLauncher
+
+------------------------------------------------------------------------------*/
+
+/**
+ * This utility class provides methods for displaying a URL in a web
+ * browser on the user's desktop.
+ */
+
+public class BrowserLauncher {
+
+  private static Class desktopClass = null;
+  private static Object desktopObject = null;
+
+  /**
+   * Returns true if we are running under Java 6 or later with support
+   * for the java.awt.Desktop class.
+   *
+   * Under Java 5, we don't have access to the java.awt.Desktop class,
+   * so we will return false.
+   *
+   * N.B. this method could be crafted to take advantage of the
+   * java.applet.AppletContext and/or javax.jnlp.BasicService classes,
+   * if we are being as an applet or a Java Web Start application
+   * under Java 5, but we are not yet providing this support.
+   */
+
+  public static synchronized boolean isWebBrowserSupported()
+  {
+    if (desktopObject == null)
+      {
+	try
+	  {
+	    desktopClass = Class.forName("java.awt.Desktop");
+
+	    Method getDesktopMethod = desktopClass.getMethod("getDesktop", new Class [] {});
+
+	    desktopObject = getDesktopMethod.invoke(null, (Object[]) null);
+	  }
+	catch (Exception ex)
+	  {
+	  }
+      }
+
+    return desktopObject != null;
+  }
+
+  /**
+   * Invokes a browser to bring up the given URL.
+   *
+   * May throw an IllegalArgumentException if the url parameter is not
+   * well-formed.
+   */
+
+  public static void browse(String url)
+  {
+    if (!isWebBrowserSupported())
+      {
+	return;
+      }
+
+    URI urlObj = URI.create(url);
+
+    try
+      {
+	Method browseMethod = desktopClass.getMethod("browse", new Class [] {java.net.URI.class});
+
+	browseMethod.invoke(urlObj, (Object []) null);
+      }
+    catch (Exception ex)
+      {
+      }
+  }
+}
+
