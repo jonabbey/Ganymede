394c3cf08dc17c336e66188fc7fe731687c8d9ef
Jonathan Abbey <jonabbey@arlut.utexas.edu>
2009-02-10 14:32:51 -0600

    Put bars across the server output to accentuate the server ready message.
    -- Jon

--- src/ganymede/arlut/csd/ganymede/server/Ganymede.java
+++ src/ganymede/arlut/csd/ganymede/server/Ganymede.java
@@ -913,8 +913,12 @@ public class Ganymede {
 
     java.lang.Runtime.getRuntime().addShutdownHook(Ganymede.signalHandlingThread);
 
-    // "Ganymede Server Ready."    
+    debug("--------------------------------------------------------------------------------");
+
+    // "Ganymede Server Ready."
     debug(ts.l("main.info_ready"));
+
+    debug("--------------------------------------------------------------------------------\n");
   }
 
   // debug routine
