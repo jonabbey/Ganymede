cf7fdd8639352bfda3e257ed3ed278ef42318e82
Jonathan Abbey <jonabbey@arlut.utexas.edu>
2009-10-15 15:39:43 -0500

    Code clean up.
    Cleaned up trailing whitespace, put constants at top,
    made sure method comments are JavaDoc formatted, reworked
    indentation to our code standards, made the test routine be
    runnable from the command line as a test rig.
    
    -- Jon

--- src/ganymede/arlut/csd/Util/RandomUtils.java
+++ src/ganymede/arlut/csd/Util/RandomUtils.java
@@ -2,16 +2,16 @@
 
    RandomUtils.java
 
-   Created: 7 February 2008, 
+   Created: 7 February 2008,
    Last Modified: 15 October 2009
 
    Module By: Jonathan Abbey, jonabbey@arlut.utexas.edu
               James Ratcliff, falazar@arlut.utexas.edu
 
    -----------------------------------------------------------------------
-	    
+
    Ganymede Directory Management System
- 
+
    Copyright (C) 1996 - 2009
    The University of Texas at Austin
 
@@ -66,6 +66,10 @@ public class RandomUtils {
 
   private static Random randomizer = new Random();
 
+  private static final String charsetA = "abcdefghijklmnopqrstuvwxyz";
+  private static final String charsetAN = "0123456789abcdefghijklmnopqrstuvwxyz";
+  private static final String charsetC = "!@#$%^&*()0123456789abcdefghijklmnopqrstuvwxyz";
+
   /**
    * This method returns a random hex string, used to ensure that a
    * string including it is random.
@@ -86,66 +90,73 @@ public class RandomUtils {
     return in + " " + getRandomHex();
   }
 
-  private static final String charsetA = "abcdefghijklmnopqrstuvwxyz";
-  private static final String charsetAN = "0123456789abcdefghijklmnopqrstuvwxyz";
-  private static final String charsetC = "!@#$%^&*()0123456789abcdefghijklmnopqrstuvwxyz";
- 
+  /**
+   * Get a random string, using all characters from given char set, length chars long
+   */
 
-
-  /* Get a random string, using all characters from given char set, length chars long */ 
-  public static String getRandomString(int length, String charset) 
+  public static String getRandomString(int length, String charset)
   {
     Random rand = new Random(System.currentTimeMillis());
     StringBuffer sb = new StringBuffer();
-    for (int i = 0; i < length; i++) 
-    {
-      int pos = rand.nextInt(charset.length());
-      sb.append(charset.charAt(pos));
-    }
+
+    for (int i = 0; i < length; i++)
+      {
+	int pos = rand.nextInt(charset.length());
+	sb.append(charset.charAt(pos));
+      }
+
     return sb.toString();
   }
 
-  /* Get a random password, using all characters, length chars long */ 
-  public static String getRandomPassword(int length) 
+  /**
+   * Get a random password, using all characters, length chars long
+   */
+
+  public static String getRandomPassword(int length)
   {
-      String password = getRandomString(length, charsetC);
-      return password;
+    String password = getRandomString(length, charsetC);
+
+    return password;
   }
 
-  /* Get a random username, 8 alpha-numeric, starts with alhpa */ 
-  public static String getRandomUsername() 
+  /**
+   * Get a random username, 8 alpha-numeric, starts with alpha
+   */
+
+  public static String getRandomUsername()
   {
-      String onechar = getRandomString(1, charsetA);
-      String username = onechar + getRandomString(7, charsetAN);
-      return username;
+    String onechar = getRandomString(1, charsetA);
+    String username = onechar + getRandomString(7, charsetAN);
+
+    return username;
   }
 
+  /**
+   * Test rig
+   */
 
-
-  // Test out the passwords now.
-  public static void test() 
+  public static void main(String args[])
   {
-    for (int i = 0; i < 10; i++) 
-    {
-      String username = getRandomUsername();
-      String password = getRandomPassword(20);
+    for (int i = 0; i < 10; i++)
+      {
+	String username = getRandomUsername();
+	String password = getRandomPassword(20);
 
-      System.out.println(username);
-      System.out.println(password);
-      System.out.println("");
- 
-      try 
-      {
-	// if you generate more than 1 time, you must
-	// put the process to sleep for awhile
-	// otherwise it will return the same random string
-	Thread.sleep(100);
-      } 
-      catch (InterruptedException e) 
-      {
-	e.printStackTrace();
+	System.out.println(username);
+	System.out.println(password);
+	System.out.println("");
+
+	try
+	  {
+	    // if you generate more than 1 time, you must
+	    // put the process to sleep for awhile
+	    // otherwise it will return the same random string
+	    Thread.sleep(100);
+	  }
+	catch (InterruptedException e)
+	  {
+	    e.printStackTrace();
+	  }
       }
-    }
   }
-
 }
