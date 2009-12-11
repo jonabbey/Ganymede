36d441ca22d71836c7514b2c2f5ff95e1503d72f
Jonathan Abbey <jonabbey@arlut.utexas.edu>
2009-12-11 16:07:28 -0600

    Implemented name checking against the XML 1.1 / XML 1.0 (version 5) specifications.

--- src/ganymede/arlut/csd/Util/XMLNameValidator.java
+++ src/ganymede/arlut/csd/Util/XMLNameValidator.java
@@ -8,9 +8,9 @@
    Module By: Jonathan Abbey, jonabbey@arlut.utexas.edu
 
    -----------------------------------------------------------------------
-	    
+
    Ganymede Directory Management System
- 
+
    Copyright (C) 1996 - 2009
    The University of Texas at Austin
 
@@ -58,40 +58,133 @@ package arlut.csd.Util;
  * This is a dirt-simple XML Name validator for Ganymede, used to
  * verify that proposed field names are acceptable for Ganymede's use.
  *
- * It is intended to validate whether a given string is usable as an
- * XML identifier, but it doesn't fully do the job correctly, as it is
- * using the Java Unicode methods, which differ slightly from those of
- * XML.
+ * From the XML 1.1 (and XML 1.0 Version 5) standards:
  *
- * However, I think you'd have to go to some effort to find cases
- * where they don't overlap, and for our current purposes this will
- * have to be good enough.  Certainly far better than limiting
- * Ganymede field and object type names to the American alphabet.
+ * NameStartChar ::= ":" | [A-Z] | "_" | [a-z] | [#xC0-#xD6] |
+ * [#xD8-#xF6] | [#xF8-#x2FF] | [#x370-#x37D] | [#x37F-#x1FFF] |
+ * [#x200C-#x200D] | [#x2070-#x218F] | [#x2C00-#x2FEF] |
+ * [#x3001-#xD7FF] | [#xF900-#xFDCF] | [#xFDF0-#xFFFD] |
+ * [#x10000-#xEFFFF]
  *
- * Sun actually uses the Xerces code from Apache to do this, but that
- * code is packaged under com.sun.org.apache.xerces, and we are meant
- * not to rely on that as it is not part of the official JDK API.
+ * NameChar ::= NameStartChar | "-" | "." | [0-9] | #xB7 | [#x0300-#x036F] | [#x203F-#x2040]
  *
- * We can't use the Xerces code ourselves, because it is not
- * compatible with GPL 2.0, but Red Hat's IcedTea distribution
- * considers it okay because it is licensed under GPL+Linking
- * exception.
- *
- * Since Ganymede's use of the GPL does not extend to the licensing of
- * the JDK, we'll use reflection to use the Xerces XName validation if
- * we can, otherwise we'll back down to using the
- * java.lang.Character.isUnicodeIdentifer* methods.
  */
 
 public class XMLNameValidator {
 
+  /**
+   * Returns true if codePoint is a valid character to start a name
+   * (element name or attribute name) in XML 1.1 or XML 1.0 (revision
+   * 5).
+   */
+
+  public static boolean isValidNameStartChar(int codePoint)
+  {
+    return (codePoint == ':' || codePoint == '_' ||
+	    (codePoint >= 'A' && codePoint <= 'Z') ||
+	    (codePoint >= 'a' && codePoint <= 'z') ||
+	    (codePoint >= 0xC0 && codePoint <= 0xD6) ||
+	    (codePoint >= 0xD8 && codePoint <= 0xF6) ||
+	    (codePoint >= 0xF8 && codePoint <= 0x2FF) ||
+	    (codePoint >= 0x370 && codePoint <= 0x37D) ||
+	    (codePoint >= 0x37F && codePoint <= 0x1FFF) ||
+	    (codePoint >= 0x200C && codePoint <= 0x200D) ||
+	    (codePoint >= 0x2070 && codePoint <= 0x218F) ||
+	    (codePoint >= 0x2C00 && codePoint <= 0x2FEF) ||
+	    (codePoint >= 0x3001 && codePoint <= 0xD7FF) ||
+	    (codePoint >= 0xF900 && codePoint <= 0xFDCF) ||
+	    (codePoint >= 0xFDF0 && codePoint <= 0xFFFD) ||
+	    (codePoint >= 0x10000 && codePoint <= 0xEFFFF));
+  }
+
+  /**
+   * Returns true if codePoint is a valid character for a character
+   * after the first in a name (element name or attribute name) in XML
+   * 1.1 or XML 1.0 (revision 5).
+   */
+
+  public static boolean isValidNameChar(int codePoint)
+  {
+    return (isValidNameStartChar(codePoint) ||
+	    codePoint == '-' || codePoint == '.' ||
+	    (codePoint >= '0' && codePoint <= '9') ||
+	    codePoint == 0xB7 ||
+	    (codePoint >= 0x300 && codePoint <= 0x36F) ||
+	    (codePoint >= 0x203F && codePoint <= 0x2040));
+  }
+
+  /**
+   * Returns true if text is a valid name for element name or
+   * attribute name in XML 1.1 or XML 1.0 (revision 5)
+   */
+
   public static boolean isValidXMLName(String text)
   {
+    if (text == null || text.isEmpty())
+      {
+	return false;
+      }
+
+    if (!XMLNameValidator.isValidNameStartChar(text.codePointAt(0)))
+      {
+	return false;
+      }
+
+    for (int i = 1; i < text.codePointCount(0, text.length()); i++)
+      {
+	if (!XMLNameValidator.isValidNameChar(text.codePointAt(i)))
+	  {
+	    return false;
+	  }
+      }
+
     return true;
   }
 
-  public static boolean isValidGanymedeFieldName(String text)
+  /**
+   * Returns true if text can be used as a Ganymede object type or
+   * field name.
+   *
+   * Ganymede names differ from XML names in that underscores are not
+   * allowed (because they are used as the representation of spaces),
+   * and spaces are.
+   *
+   * We're also not going to use colons to avoid
+   */
+
+  public static boolean isValidGanymedeName(String text)
   {
+    if (text == null || text.isEmpty())
+      {
+	return false;
+      }
+
+    int firstCodePoint = text.codePointAt(0);
+
+    if (firstCodePoint == ' ' || firstCodePoint == '_' || firstCodePoint == ':' ||
+	!XMLNameValidator.isValidNameStartChar(text.codePointAt(0)))
+      {
+	return false;
+      }
+
+    for (int i = 1; i < text.codePointCount(0, text.length()); i++)
+      {
+	int codePoint = text.codePointAt(i);
+
+	if (codePoint == '_' || (codePoint != ' ' && codePoint != ':' && !XMLNameValidator.isValidNameChar(text.codePointAt(i))))
+	  {
+	    return false;
+	  }
+      }
+
+    // XML names that begin with xml are reserved, so we don't want to
+    // allow Ganymede names to start that way.
+
+    if (text.startsWith("xml"))
+      {
+	return false;
+      }
+
     return true;
   }
 }
