d02af45407cc3f4a8f78710250a2afaebad5d0c3
Jonathan Abbey <jonabbey@arlut.utexas.edu>
2010-01-25 18:12:41 -0600

    Header, FSF address, Copyright

--- src/ganymede/arlut/csd/JTable/gridTable.java
+++ src/ganymede/arlut/csd/JTable/gridTable.java
@@ -1,36 +1,48 @@
 /*
 
-  gridTable.java
+   gridTable.java
 
-  A JDK 1.1 table Swing component.
+   A JDK 1.1 table Swing component.
 
-  Copyright (C) 1996 - 2008
-  The University of Texas at Austin
+   The GANYMEDE object storage system.
 
-  This program is free software; you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation; either version 2 of the License, or
-  (at your option) any later version.
+   Created: 29 May 1996
 
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
+   Module By: Jonathan Abbey, jonabbey@arlut.utexas.edu
 
-  You should have received a copy of the GNU General Public License
-  along with this program; if not, write to the Free Software
-  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
-  02111-1307, USA
+   -----------------------------------------------------------------------
 
-  Created: 17 June 1996
+   Ganymede Directory Management System
 
-  Last Mod Date: $Date$
-  Last Revision Changed: $Rev$
-  Last Changed By: $Author$
-  SVN URL: $HeadURL$
+   Copyright (C) 1996-2010
+   The University of Texas at Austin
 
-  Module By: Jonathan Abbey -- jonabbey@arlut.utexas.edu
-  Applied Research Laboratories, The University of Texas at Austin
+   Contact information
+
+   Author Email: ganymede_author@arlut.utexas.edu
+   Email mailing list: ganymede@arlut.utexas.edu
+
+   US Mail:
+
+   Computer Science Division
+   Applied Research Laboratories
+   The University of Texas at Austin
+   PO Box 8029, Austin TX 78713-8029
+
+   Telephone: (512) 835-3200
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 */
 
--- src/ganymede/arlut/csd/JTable/tableRow.java
+++ src/ganymede/arlut/csd/JTable/tableRow.java
@@ -1,36 +1,45 @@
 /*
+   tableRow.java
 
-  tableRow.java
+   A JDK 1.1 table Swing component.
 
-  A JDK 1.1 table AWT component.
+   Created: 15 January 1999
 
-  Copyright (C) 1996 - 2004
-  The University of Texas at Austin
+   Module By: Jonathan Abbey, jonabbey@arlut.utexas.edu
 
-  This program is free software; you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation; either version 2 of the License, or
-  (at your option) any later version.
+   -----------------------------------------------------------------------
 
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
+   Ganymede Directory Management System
 
-  You should have received a copy of the GNU General Public License
-  along with this program; if not, write to the Free Software
-  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
-  02111-1307, USA
+   Copyright (C) 1996-2010
+   The University of Texas at Austin
 
-  Created: 15 January 1999
+   Contact information
 
-  Last Mod Date: $Date$
-  Last Revision Changed: $Rev$
-  Last Changed By: $Author$
-  SVN URL: $HeadURL$
+   Author Email: ganymede_author@arlut.utexas.edu
+   Email mailing list: ganymede@arlut.utexas.edu
 
-  Module By: Jonathan Abbey -- jonabbey@arlut.utexas.edu
-  Applied Research Laboratories, The University of Texas at Austin
+   US Mail:
+
+   Computer Science Division
+   Applied Research Laboratories
+   The University of Texas at Austin
+   PO Box 8029, Austin TX 78713-8029
+
+   Telephone: (512) 835-3200
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 */
 
