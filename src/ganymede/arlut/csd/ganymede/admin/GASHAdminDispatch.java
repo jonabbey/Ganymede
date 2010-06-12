19898fd21ab0918796f5d2e34a3c01cd8fd264f8
Jonathan Abbey <jonabbey@arlut.utexas.edu>
2010-06-11 19:48:10 -0500

    Setting the tab background is not actually the right thing to do.

--- src/ganymede/arlut/csd/ganymede/admin/GASHAdminDispatch.java
+++ src/ganymede/arlut/csd/ganymede/admin/GASHAdminDispatch.java
@@ -904,13 +904,11 @@ class GASHAdminDispatch implements Runnable {
 	  case NONEMPTYQUEUE:
 	    table.setCellColor(handle.name, 2, Color.black, false);
 	    table.setCellBackColor(handle.name, 2, Color.white, false);
-	    frame.tabPane.setBackgroundAt(2, null);
 	    break;
 	    
 	  default:
 	    table.setCellColor(handle.name, 2, Color.white, false);
 	    table.setCellBackColor(handle.name, 2, Color.red, false);
-	    frame.tabPane.setBackgroundAt(2, Color.red);
 	  }
 
 	if (handle.lastTime != null)
