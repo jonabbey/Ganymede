a897b7b55c777239bfb04cceb7c805de40c39c05
Jonathan Abbey <jonabbey@arlut.utexas.edu>
2010-09-22 00:13:33 -0500

    Added outline of Exchange Store Task

---
+++ src/schemas/gasharl/src/arlut/csd/ganymede/gasharl/ExchangeStoreTask.java
@@ -0,0 +1,199 @@
+/*
+
+   ExchangeStoreTask.java
+
+   This task is designed to query our Active Directory server to
+   obtain a list of defined Exchange Mail Store volumes.  We use the
+   results of this query to keep our Exchange Store object list up to
+   date.
+
+   Created: 22 September 2010
+
+   Module By: Jonathan Abbey, jonabbey@arlut.utexas.edu
+
+   -----------------------------------------------------------------------
+	    
+   Ganymede Directory Management System
+ 
+   Copyright (C) 1996-2010
+   The University of Texas at Austin
+
+   Contact information
+
+   Author Email: ganymede_author@arlut.utexas.edu
+   Email mailing list: ganymede@arlut.utexas.edu
+
+   US Mail:
+
+   Computer Science Division
+   Applied Research Laboratories
+   The University of Texas at Austin
+   PO Box 8029, Austin TX 78713-8029
+
+   Telephone: (512) 835-3200
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+package arlut.csd.ganymede.gasharl;
+
+import java.rmi.RemoteException;
+
+import arlut.csd.ganymede.common.Invid;
+import arlut.csd.ganymede.common.NotLoggedInException;
+import arlut.csd.ganymede.common.ReturnVal;
+import arlut.csd.ganymede.common.SchemaConstants;
+import arlut.csd.ganymede.server.DBEditObject;
+import arlut.csd.ganymede.server.DBObject;
+import arlut.csd.ganymede.server.Ganymede;
+import arlut.csd.ganymede.server.GanymedeServer;
+import arlut.csd.ganymede.server.GanymedeSession;
+import arlut.csd.ganymede.server.StringDBField;
+
+/*------------------------------------------------------------------------------
+                                                                           class
+                                                               ExchangeStoreTask
+
+------------------------------------------------------------------------------*/
+
+/**
+ * This task is designed to query our Active Directory server to
+ * obtain a list of defined Exchange Mail Store volumes.  We use the
+ * results of this query to keep our Exchange Store object list up to
+ * date.
+ *
+ * @author Jonathan Abbey jonabbey@arlut.utexas.edu
+ */
+
+public class ExchangeStoreTask implements Runnable {
+
+  static final boolean debug = true;
+
+  /* -- */
+
+  GanymedeSession mySession = null;
+
+  /**
+   *
+   * Just Do It (tm)
+   *
+   * @see java.lang.Runnable
+   *
+   */
+
+  public void run()
+  {
+    boolean transactionOpen = false;
+
+    /* -- */
+
+    Ganymede.debug("Exchange Store Task: Starting");
+
+    String error = GanymedeServer.checkEnabled();
+	
+    if (error != null)
+      {
+	Ganymede.debug("Deferring Exchange Store Task - semaphore disabled: " + error);
+	return;
+      }
+
+    try
+      {
+	try
+	  {
+	    mySession = new GanymedeSession("ExchangeStoreTask");
+	  }
+	catch (RemoteException ex)
+	  {
+	    Ganymede.debug("Exchange Store Task: Couldn't establish session");
+	    return;
+	  }
+
+	// we don't want interactive handholding
+
+	mySession.enableWizards(false);
+
+	// and we want forced required fields oversight..
+
+	mySession.enableOversight(true);
+	
+	ReturnVal retVal = mySession.openTransaction("Exchange Store Task");
+
+	if (retVal != null && !retVal.didSucceed())
+	  {
+	    Ganymede.debug("Exchange Store Task: Couldn't open transaction");
+	    return;
+	  }
+
+	transactionOpen = true;
+	
+	// do the stuff
+
+	if (!doTask())
+	  {
+	    Ganymede.debug("Exchange Store Query / Update bailed");
+
+	    mySession.abortTransaction();
+	    return;
+	  }
+
+	retVal = mySession.commitTransaction();
+
+	if (retVal != null && !retVal.didSucceed())
+	  {
+	    // if doNormalProcessing is true, the
+	    // transaction was not cleared, but was
+	    // left open for a re-try.  Abort it.
+
+	    if (retVal.doNormalProcessing)
+	      {
+		Ganymede.debug("Exchange Store Task: couldn't fully commit, trying to abort.");
+
+		mySession.abortTransaction();
+	      }
+
+	    Ganymede.debug("Exchange Store Task: Couldn't successfully commit transaction");
+	  }
+	else
+	  {
+	    Ganymede.debug("Exchange Store Task: Transaction committed");
+	  }
+
+	transactionOpen = false;
+      }
+    catch (NotLoggedInException ex)
+      {
+      }
+    catch (Throwable ex)
+      {
+	Ganymede.debug("Caught " + ex.getMessage());
+      }
+    finally
+      {
+	if (transactionOpen)
+	  {
+	    Ganymede.debug("Exchange Store Task: Forced to terminate early, aborting transaction");
+	  }
+
+	mySession.logout();
+      }
+  }
+
+  public boolean doTask() throws NotLoggedInException
+  {
+    return false;
+  }
+}
+
