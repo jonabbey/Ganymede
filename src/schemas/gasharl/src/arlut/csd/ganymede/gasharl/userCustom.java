99b0a989a0ba56b70bcf695009fc628ef7c6f501
Jonathan Abbey <jonabbey@arlut.utexas.edu>
2010-12-22 17:13:13 -0600

    Put in an appropriate notice about hitting commit again to proceed in IRIS warning case.

--- src/schemas/gasharl/src/arlut/csd/ganymede/gasharl/userCustom.java
+++ src/schemas/gasharl/src/arlut/csd/ganymede/gasharl/userCustom.java
@@ -505,7 +505,8 @@ public class userCustom extends DBEditObject implements SchemaConstants, userSch
 									     "'.\n\n" +
 									     "You must change the username or badge id in the '" + username + "' object in order " +
 									     "to resolve this conflict.\n\n" +
-									     "You could also change this user object's user category to something other than 'normal'."));
+									     "You could also change this user object's user category to something other than 'normal'.\n\n" +
+									      "If you feel this message is in error, hit commit again to proceed."));
 		  }
 	      }
 	  }
