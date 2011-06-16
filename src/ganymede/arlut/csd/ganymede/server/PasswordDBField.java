2ebe641f8c68065cebff17b4f6642d8d7fb14e3e
Jonathan Abbey <jonabbey@arlut.utexas.edu>
2011-06-16 12:41:06 -0500

    Marked the pass_field RMI interface's crypted() API as deprecated.

--- src/ganymede/arlut/csd/ganymede/server/PasswordDBField.java
+++ src/ganymede/arlut/csd/ganymede/server/PasswordDBField.java
@@ -1343,7 +1343,15 @@ public class PasswordDBField extends DBField implements pass_field {
   }
 
   /**
-   * Returns true if the password stored in this field is hash-crypted.
+   * <p>Returns true if the password stored in this field is hash-crypted.</p>
+   *
+   * <p>NB: This method is part of the pass_field client API for the
+   * PasswordDBField class, but it only returns true if the server is
+   * holding a traditional Unix crypt hash.  We don't have comparable
+   * methods for other hash formats, and the client could always query
+   * the field definition using the DBObjectBase base_field remote
+   * API, in any case.  We should probably ditch this method from the
+   * pass_field remote interface.</p>
    *
    * @see arlut.csd.ganymede.rmi.pass_field
    */
