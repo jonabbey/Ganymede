jonabbey/Ganymede

commit f8a4e79bcdb78d17e0c5f42c39fc74f972c455dc

Made PasswordDBField do the full, expensive ShaCrypt validation on xml load.

src/ganymede/arlut/csd/ganymede/server/PasswordDBField.java
@@ -2724,7 +2724,7 @@ public class PasswordDBField extends DBField implements pass_field {
 
     if (settingShaUnixCrypt)
       {
-	if (!ShaUnixCryptText.startsWith("$5$") && !ShaUnixCryptText.startsWith("$6$"))
+	if (!Sha256Crypt.verifyHashTextFormat(shaUnixCryptText) && !Sha512Crypt.verifyHashTextFormat(shaUnixCryptText))
 	  {
 	    // "Server: Error in PasswordDBField.setAllHashes()"
 	    // "The hash text passed to setShaUnixCryptPass(), "{0}", is not a well-formed, SHA Unix Crypt hash text"

commit 9e9ed49384d76960825b5a1e7cd5435f176e7de5

Fixed a compile regression in PasswordDBField from commit f8a4e79bcdb78d17e0c5f42c39fc74f972c455dc

src/ganymede/arlut/csd/ganymede/server/PasswordDBField.java
@@ -2735,7 +2735,7 @@ public class PasswordDBField extends DBField implements pass_field {
 
     if (settingShaUnixCrypt)
       {
-	if (!Sha256Crypt.verifyHashTextFormat(shaUnixCryptText) && !Sha512Crypt.verifyHashTextFormat(shaUnixCryptText))
+	if (!Sha256Crypt.verifyHashTextFormat(ShaUnixCryptText) && !Sha512Crypt.verifyHashTextFormat(ShaUnixCryptText))
 	  {
 	    // "Server: Error in PasswordDBField.setAllHashes()"
 	    // "The hash text passed to setShaUnixCryptPass(), "{0}", is not a well-formed, SHA Unix Crypt hash text"