d87f148c0fc41d27445a0eb6c8424181cc8dfab9
Jonathan Abbey <jonabbey@arlut.utexas.edu>
2012-05-01 13:42:00 -0500

    Moved public fields to top of GanymedeSession.

--- src/ganymede/arlut/csd/ganymede/server/GanymedeSession.java
+++ src/ganymede/arlut/csd/ganymede/server/GanymedeSession.java
@@ -183,6 +183,35 @@ final public class GanymedeSession implements Session, Unreferenced {
   // ---
 
   /**
+   * <p>This variable tracks whether or not the client desires to have
+   * wizards presented.  If this is false, custom plug-in code
+   * for the object types stored in the
+   * {@link arlut.csd.ganymede.server.DBStore DBStore} may either
+   * refuse certain operations or will resort to taking a default action.</p>
+   *
+   * <p>Note: this variable should be private with an accessor, but
+   * end-user custom code has been written which uses this, so we have
+   * to keep it public.</p>
+   */
+
+  public boolean enableWizards = true;
+
+  /**
+   * <p>If this variable is set to false, no custom wizard code will ever
+   * be invoked, and required fields will not be forced.  This is
+   * intended primarily for direct database loading.</p>
+   *
+   * <p>This variable is not intended ever to be available to the client,
+   * but should only be set by local server code.</p>
+   *
+   * <p>Note: this variable should be private with an accessor, but
+   * end-user custom code has been written which uses this, so we have
+   * to keep it public.</p>
+   */
+
+  public boolean enableOversight = true;
+
+  /**
    * Async responder for sending async messages to the client.
    */
 
@@ -227,35 +256,6 @@ final public class GanymedeSession implements Session, Unreferenced {
    */
 
   private int objectsCheckedOut = 0;
-
-  /**
-   * <p>This variable tracks whether or not the client desires to have
-   * wizards presented.  If this is false, custom plug-in code
-   * for the object types stored in the
-   * {@link arlut.csd.ganymede.server.DBStore DBStore} may either
-   * refuse certain operations or will resort to taking a default action.</p>
-   *
-   * <p>Note: this variable should be private with an accessor, but
-   * end-user custom code has been written which uses this, so we have
-   * to keep it public.</p>
-   */
-  
-  public boolean enableWizards = true;
-
-  /**
-   * <p>If this variable is set to false, no custom wizard code will ever
-   * be invoked, and required fields will not be forced.  This is
-   * intended primarily for direct database loading.</p>
-   *
-   * <p>This variable is not intended ever to be available to the client,
-   * but should only be set by local server code.</p>
-   *
-   * <p>Note: this variable should be private with an accessor, but
-   * end-user custom code has been written which uses this, so we have
-   * to keep it public.</p>
-   */
-  
-  public boolean enableOversight = true;
 
   /**
    * The time that this client initially connected to the server.  Used
