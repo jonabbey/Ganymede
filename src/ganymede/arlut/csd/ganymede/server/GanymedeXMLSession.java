3adc0377980ca9057f2f9d78642f2fbf177965e2
Jonathan Abbey <jonabbey@arlut.utexas.edu>
2013-02-27 12:59:53 -0600

    Updated the Javadoc headers for XMLSession/GanymedeXMLSession xmlEnd() and getNextErrChunk()

--- src/ganymede/arlut/csd/ganymede/server/GanymedeXMLSession.java
+++ src/ganymede/arlut/csd/ganymede/server/GanymedeXMLSession.java
@@ -588,7 +588,13 @@ public final class GanymedeXMLSession extends java.lang.Thread implements XMLSes
    * <p>This method is called by the XML client once the end of the
    * XML stream has been transmitted, whereupon the server will
    * attempt to finalize the XML transaction and return an overall
-   * success or failure message in the ReturnVal.</p>
+   * success or failure indication in the ReturnVal.</p>
+   *
+   * <p>xmlEnd() only returns a success / failure indication in the
+   * returned ReturnVal.  In order to get all diagnostic / progress
+   * messages explaining the success or failure, the client is obliged
+   * to maintain a thread calling getNextErrChunk() until
+   * getNextErrChunk() returns null.</p>
    *
    * @see arlut.csd.ganymede.rmi.XMLSession
    */
@@ -606,16 +612,24 @@ public final class GanymedeXMLSession extends java.lang.Thread implements XMLSes
   }
 
   /**
-   * <p>This method is called by the XML client on a dedicated thread
-   * to pull stderr messages from the server.</p>
+   * <p>Returns chunks of diagnostic / progress messages produced on
+   * the server during the processing of XML submitted with
+   * xmlSubmit().</p>
    *
-   * <p>This call will block on the server until err stream data is
-   * available and will block for at least a tenth of a second while
-   * the XML is still being processed so that the client doesn't loop
-   * on getNextErrChunk() too fast.</p>
+   * <p>This call will block on the server until more message data is
+   * available and will for at least a tenth of a second while the XML
+   * is still being processed so that the client doesn't loop on
+   * getNextErrChunk() too fast.</p>
    *
-   * <p>This method will return null after the server closes its error
-   * stream.</p>
+   * <p>Once this XMLSession has finished processing the submitted XML
+   * and everything in the diagnostic / progress message stream has
+   * been retrieved by calls to getNextErrChunk(), getNextErrChunk()
+   * will return null.</p>
+   *
+   * <p>The XML client is meant to run a dedicated thread to
+   * repeatedly call this method to collect diagnostic / progress data
+   * until getNextErrChunk() returns null.  This thread will generally
+   * last beyond the time of the XML client's xmlEnd() call.</p>
    *
    * @see arlut.csd.ganymede.rmi.XMLSession
    */
