6420846d5ea96a9a50156a7fee7f7776d7e66db6
Jonathan Abbey <jonabbey@arlut.utexas.edu>
2013-04-02 18:29:44 -0500

    Tweaked the comments describing problems with loopback binding in startup.

--- src/ganymede/arlut/csd/ganymede/server/Ganymede.java
+++ src/ganymede/arlut/csd/ganymede/server/Ganymede.java
@@ -1362,16 +1362,16 @@ public final class Ganymede {
                 debug("\n** " + ts.l("main.error") + " **\n");
               }
 
-            // "Both the system hostname ({0}) and the
-            // ganymede.serverhost definition ({1}) resolve to
-            // the 127.0.0.1 loopback address"
+            // "The system hostname ({0}) and/or the
+            // ganymede.serverhost definition ({1}) resolve to the
+            // 127.0.0.1 loopback address"
             debug(ts.l("main.error_loopback",
                        java.net.InetAddress.getLocalHost().getHostName(),
                        serverHostProperty));
             debug("\n");
 
             // "The Ganymede server must have an externally
-            // accessible IP address or else clients            \
+            // accessible IP address or else clients
             // will not be able to communicate with the
             // Ganymede server from other than localhost."
             debug(ts.l("main.error_loopback_explain"));
