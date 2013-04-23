9988b912b1cef917df183e0fa0e1efb95a6e9bb1
Jonathan Abbey <jonabbey@arlut.utexas.edu>
2013-04-22 22:51:34 -0500

    Modernized Vector API usage, Override annotations, tabs to spaces in gasharl emailRedirectCustom

--- src/schemas/gasharl/src/arlut/csd/ganymede/gasharl/emailRedirectCustom.java
+++ src/schemas/gasharl/src/arlut/csd/ganymede/gasharl/emailRedirectCustom.java
@@ -3,16 +3,16 @@
    emailRedirectCustom.java
 
    Custom plug-in for managing fields in the email redirect object type.
-   
+
    Created: 25 June 1999
 
    Module By: Jonathan Abbey, jonabbey@arlut.utexas.edu
 
    -----------------------------------------------------------------------
-	    
+
    Ganymede Directory Management System
- 
-   Copyright (C) 1996-2010
+
+   Copyright (C) 1996-2013
    The University of Texas at Austin
 
    Contact information
@@ -108,55 +108,74 @@ public class emailRedirectCustom extends DBEditObject implements SchemaConstants
 
   /**
    * <p>This method is used to control whether or not it is acceptable to
-   * make a link to the given field in this 
+   * make a link to the given field in this
    * {@link arlut.csd.ganymede.server.DBObject DBObject} type when the
-   * user only has editing access for the source 
+   * user only has editing access for the source
    * {@link arlut.csd.ganymede.server.InvidDBField InvidDBField} and not
    * the target.</p>
    *
    * <p>This version of anonymousLinkOK takes additional parameters
    * to allow an object type to decide that it does or does not want
    * to allow a link based on what field of what object wants to link
-   * to it.</P>
+   * to it.</p>
    *
-   * <p>By default, the 3 variants of the DBEditObject anonymousLinkOK() 
-   * method are chained together, so that the customizer can choose
-   * which level of detail he is interested in.
+   * <p>By default, the 3 variants of the DBEditObject
+   * anonymousLinkOK() method are chained together, so that the
+   * customizer can choose which level of detail he is interested in.
    * {@link arlut.csd.ganymede.server.InvidDBField InvidDBField}'s
-   * {@link arlut.csd.ganymede.server.InvidDBField#bind(arlut.csd.ganymede.common.Invid,arlut.csd.ganymede.common.Invid,boolean) bind()}
-   * method calls this version.  This version calls the three parameter
-   * version, which calls the two parameter version, which returns
-   * false by default.  Customizers can implement any of the three
-   * versions, but unless you maintain the version chaining yourself,
-   * there's no point to implementing more than one of them.</P>
+   * {@link
+   * arlut.csd.ganymede.server.InvidDBField#bind(arlut.csd.ganymede.common.Invid,arlut.csd.ganymede.common.Invid,boolean)
+   * bind()} method calls this version.  This version calls the three
+   * parameter version, which calls the two parameter version, which
+   * returns false by default.  Customizers can implement any of the
+   * three versions, but unless you maintain the version chaining
+   * yourself, there's no point to implementing more than one of
+   * them.</p>
+   *
+   * <p>Note that the {@link
+   * arlut.csd.ganymede.server.DBEditObject#choiceListHasExceptions(arlut.csd.ganymede.server.DBField)
+   * choiceListHasExceptions()} method will call this version of
+   * anonymousLinkOK() with a null targetObject, to determine that the
+   * client should not use its cache for an InvidDBField's choices.
+   * Any overriding done of this method must be able to handle a null
+   * targetObject, or else an exception will be thrown
+   * inappropriately.</p>
+   *
+   * <p>The only reason to consult targetObject in any case is to
+   * allow or disallow anonymous object linking to a field based on
+   * the current state of the target object.  If you are just writing
+   * generic anonymous linking rules for a field in this object type,
+   * targetObject won't concern you anyway.  If you do care about the
+   * targetObject's state, though, you have to be prepared to handle
+   * a null valued targetObject.</p>
    *
    * <p><b>*PSEUDOSTATIC*</b></p>
    *
-   * @param targetObject The object that the link is to be created in
+   * @param targetObject The object that the link is to be created in (may be null)
    * @param targetFieldID The field that the link is to be created in
    * @param sourceObject The object on the other side of the proposed link
    * @param sourceFieldID  The field on the other side of the proposed link
    * @param gsession Who is trying to do this linking?
    */
 
-  public boolean anonymousLinkOK(DBObject targetObject, short targetFieldID,
-				 DBObject sourceObject, short sourceFieldID,
-				 GanymedeSession gsession)
+  @Override public boolean anonymousLinkOK(DBObject targetObject, short targetFieldID,
+                                           DBObject sourceObject, short sourceFieldID,
+                                           GanymedeSession gsession)
   {
     // if someone tries to put this redirect in a email list,
     // let them.
 
     if ((targetFieldID == SchemaConstants.BackLinksField) &&
-	(sourceObject.getTypeID() == 274) && // email list
-	(sourceFieldID == 257))	// email list members
+        (sourceObject.getTypeID() == emailListSchema.BASE) && // email list
+        (sourceFieldID == emailListSchema.MEMBERS)) // email list members
       {
-	return true;
+        return true;
       }
 
     // the default anonymousLinkOK() method returns false
 
     return super.anonymousLinkOK(targetObject, targetFieldID,
-				 sourceObject, sourceFieldID, gsession);
+                                 sourceObject, sourceFieldID, gsession);
   }
 
   /**
@@ -177,25 +196,29 @@ public class emailRedirectCustom extends DBEditObject implements SchemaConstants
    * checks on the operation (including vector bounds, etc.) before
    * calling this method.  Under normal circumstances, we won't need
    * to do anything here.</p>
+   *
+   * @return A ReturnVal indicating success or failure.  May
+   * be simply 'null' to indicate success if no feedback need
+   * be provided.
    */
 
-  public ReturnVal finalizeAddElement(DBField field, Object value)
+  @Override public ReturnVal finalizeAddElement(DBField field, Object value)
   {
     if (field.getID() != emailRedirectSchema.TARGETS)
       {
-	return null;
+        return null;
       }
 
-    Vector newItemVect = new Vector();
-    
-    newItemVect.addElement(value);
+    Vector<String> newItemVect = new Vector<String>();
+
+    newItemVect.add((String)value);
 
     if (!fitsInNIS(newItemVect))
       {
-	return Ganymede.createErrorDialog("Overflow error",
-					  "The address that you are attempting to add to the " + getTypeName() + 
-					  " email redirection cannot all fit.  No NIS email alias definition in the laboratory's " +
-					  "network can be longer than 1024 characters.");
+        return Ganymede.createErrorDialog("Overflow error",
+                                          "The address that you are attempting to add to the " + getTypeName() +
+                                          " email redirection cannot all fit.  No NIS email alias definition in the laboratory's " +
+                                          "network can be longer than 1024 characters.");
       }
 
     return null;
@@ -219,23 +242,27 @@ public class emailRedirectCustom extends DBEditObject implements SchemaConstants
    * checks on the operation (including vector bounds, etc.) before
    * calling this method.  Under normal circumstances, we won't need
    * to do anything here.</p>
+   *
+   * @return A ReturnVal indicating success or failure.  May
+   * be simply 'null' to indicate success if no feedback need
+   * be provided.
    */
 
-  public ReturnVal finalizeAddElements(DBField field, Vector submittedValues)
+  @Override public ReturnVal finalizeAddElements(DBField field, Vector submittedValues)
   {
     if (field.getID() != emailRedirectSchema.TARGETS)
       {
-	return null;
+        return null;
       }
 
-    if (!fitsInNIS(submittedValues))
+    if (!fitsInNIS((Vector<String>)submittedValues))
       {
-	return Ganymede.createErrorDialog("Overflow error",
-					  "The " + submittedValues.size() + 
-					  " addresses that you are attempting to add to the " + getTypeName() + 
-					  " email redirection cannot all fit.  No NIS email list in the laboratory's " +
-					  "network can be longer than 1024 characters when converted to an " +
-					  "NIS email alias definition.");
+        return Ganymede.createErrorDialog("Overflow error",
+                                          "The " + submittedValues.size() +
+                                          " addresses that you are attempting to add to the " + getTypeName() +
+                                          " email redirection cannot all fit.  No NIS email list in the laboratory's " +
+                                          "network can be longer than 1024 characters when converted to an " +
+                                          "NIS email alias definition.");
       }
 
     return null;
@@ -246,22 +273,20 @@ public class emailRedirectCustom extends DBEditObject implements SchemaConstants
    * be able to fit in the NIS line built from this emailList object.</p>
    */
 
-  private boolean fitsInNIS(Vector newItemVect)
+  private boolean fitsInNIS(Vector<String> newItemVect)
   {
     StringDBField targets = (StringDBField) getField(emailRedirectSchema.TARGETS);
 
     int totalLength = targets.getValueString().length();
 
-    for (int i = 0; i < newItemVect.size(); i++)
+    for (String label: newItemVect)
       {
-	Object value = newItemVect.elementAt(i);
+        totalLength += label.length() + 2; // need a comma and space
 
-	totalLength += (((String) value).length() + 2); // need a comma and space
-
-	if (totalLength >= 1024)
-	  {
-	    return false;
-	  }
+        if (totalLength >= 1024)
+          {
+            return false;
+          }
       }
 
     return true;
