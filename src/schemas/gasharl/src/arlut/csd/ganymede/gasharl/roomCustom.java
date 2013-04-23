90d35a5443d572165a1b9694b1859e480702a009
Jonathan Abbey <jonabbey@arlut.utexas.edu>
2013-04-22 23:06:18 -0500

    Modernized Vector API usage, Override annotations, tabs to spaces in gasharl roomCustom

--- src/schemas/gasharl/src/arlut/csd/ganymede/gasharl/roomCustom.java
+++ src/schemas/gasharl/src/arlut/csd/ganymede/gasharl/roomCustom.java
@@ -3,17 +3,19 @@
    roomCustom.java
 
    This file is a management class for room objects in Ganymede.
-   
+
    Created: 21 May 1998
 
    Module By: Jonathan Abbey, jonabbey@arlut.utexas.edu
 
    -----------------------------------------------------------------------
-	    
+
    Ganymede Directory Management System
- 
-   Copyright (C) 1996-2010
+
+   Copyright (C) 1996-2013
    The University of Texas at Austin
+
+   Ganymede is a registered trademark of The University of Texas at Austin
 
    Contact information
 
@@ -96,38 +98,68 @@ public class roomCustom extends DBEditObject {
   }
 
   /**
+   * <p>Customization method to control whether a specified field
+   * is required to be defined at commit time for a given object.</p>
    *
-   * Customization method to control whether a specified field
-   * is required to be defined at commit time for a given object.<br><br>
+   * <p>To be overridden in DBEditObject subclasses.</p>
    *
-   * To be overridden in DBEditObject subclasses.
+   * <p>Note that this method will not be called if the controlling
+   * GanymedeSession's enableOversight is turned off, as in
+   * bulk loading.</p>
    *
+   * <p><b>*PSEUDOSTATIC*</b></p>
    */
 
-  public boolean fieldRequired(DBObject object, short fieldid)
+  @Override public boolean fieldRequired(DBObject object, short fieldid)
   {
     if (fieldid == roomSchema.ROOMNUMBER)
       {
-	return true;
+        return true;
       }
 
     return false;
   }
 
   /**
+   * <p>This method is used to control whether or not it is acceptable to
+   * make a link to the given field in this
+   * {@link arlut.csd.ganymede.server.DBObject DBObject} type when the
+   * user only has editing access for the source
+   * {@link arlut.csd.ganymede.server.InvidDBField InvidDBField} and not
+   * the target.</p>
    *
-   * This method is used to control whether or not it is acceptable to
-   * make a link to the given field in this DBObject type when the
-   * user only has editing access for the source InvidDBField and not
-   * the target.
+   * <p>See {@link arlut.csd.ganymede.server.DBEditObject#anonymousLinkOK(arlut.csd.ganymede.server.DBObject,short,
+   * arlut.csd.ganymede.server.DBObject,short,arlut.csd.ganymede.server.GanymedeSession)
+   * anonymousLinkOK(obj,short,obj,short,GanymedeSession)} for details on
+   * anonymousLinkOK() method chaining.</p>
    *
+   * <p>Note that the {@link
+   * arlut.csd.ganymede.server.DBEditObject#choiceListHasExceptions(arlut.csd.ganymede.server.DBField)
+   * choiceListHasExceptions()} method will call this version of anonymousLinkOK()
+   * with a null targetObject, to determine that the client should not
+   * use its cache for an InvidDBField's choices.  Any overriding done
+   * of this method must be able to handle a null targetObject, or else
+   * an exception will be thrown inappropriately.</p>
+   *
+   * <p>The only reason to consult targetObject in any case is to
+   * allow or disallow anonymous object linking to a field based on
+   * the current state of the target object.  If you are just writing
+   * generic anonymous linking rules for a field in this object type,
+   * targetObject won't concern you anyway.  If you do care about the
+   * targetObject's state, though, you have to be prepared to handle
+   * a null valued targetObject.</p>
+   *
+   * <p><b>*PSEUDOSTATIC*</b></p>
+   *
+   * @param targetObject The object that the link is to be created in (may be null)
+   * @param targetFieldID The field that the link is to be created in
    */
 
-  public boolean anonymousLinkOK(DBObject object, short fieldID)
+  @Override public boolean anonymousLinkOK(DBObject object, short fieldID)
   {
     if (fieldID == roomSchema.SYSTEMS)
       {
-	return true;
+        return true;
       }
 
     return false;
