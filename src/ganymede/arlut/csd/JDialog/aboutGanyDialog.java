92e2570a80d2eb40c21ad57a8c60563fd1cd1503
Jonathan Abbey <jonabbey@arlut.utexas.edu>
2013-06-22 14:01:07 -0500

    Make sure text views in the aboutGanyDialog are displayed with the scroll position set to the top of the text

--- src/ganymede/arlut/csd/JDialog/aboutGanyDialog.java
+++ src/ganymede/arlut/csd/JDialog/aboutGanyDialog.java
@@ -169,48 +169,49 @@ public class aboutGanyDialog extends JDialog {
 
   private void addTab(String title, String text)
   {
-    try {
-      JEditorPane textbox = new JEditorPane("text/html",text);
-      textbox.setOpaque(true);
-      textbox.setEditable(false);
-      textbox.addHyperlinkListener(new HyperlinkListener()
-        {
-          @Override
-          public void hyperlinkUpdate(HyperlinkEvent e)
+    try
+      {
+        JEditorPane textbox = new JEditorPane("text/html",text);
+        textbox.setCaretPosition(0);
+        textbox.setOpaque(true);
+        textbox.setEditable(false);
+        textbox.addHyperlinkListener(new HyperlinkListener()
           {
-            if (e.getEventType() == HyperlinkEvent.EventType.ACTIVATED)
-              {
-                try {
-                  Desktop.getDesktop().browse(e.getURL().toURI());
-                } catch (Exception e1) {
-
-                  new StringDialog(new JFrame(),
-                                   ts.l("global.linkErrorTitle"), // "Error Opening Link"
-                                   ts.l("global.linkErrorMsg"), // "Error Opening Link"
-                                   ts.l("global.linkErrorOKButton"), // "Ok"
-                                   null,
-                                   StandardDialog.ModalityType.DOCUMENT_MODAL).showDialog();
+            @Override
+            public void hyperlinkUpdate(HyperlinkEvent e)
+            {
+              if (e.getEventType() == HyperlinkEvent.EventType.ACTIVATED)
+                {
+                  try
+                    {
+                      Desktop.getDesktop().browse(e.getURL().toURI());
+                    }
+                  catch (Exception e1)
+                    {
+                      new StringDialog(new JFrame(),
+                                       ts.l("global.linkErrorTitle"), // "Error Opening Link"
+                                       ts.l("global.linkErrorMsg"), // "Error Opening Link"
+                                       ts.l("global.linkErrorOKButton"), // "Ok"
+                                       null,
+                                       StandardDialog.ModalityType.DOCUMENT_MODAL).showDialog();
+                    }
                 }
-              }
-          }
-        });
+            }
+          });
 
-      JScrollPane scrollpane = new JScrollPane(textbox,
-                                               JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
-                                               JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
-      scrollpane.setBorder(null);
-      scrollpane.setViewportBorder(null);
-      scrollpane.getViewport().setOpaque(true);
+        JScrollPane scrollpane = new JScrollPane(textbox,
+                                                 JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
+                                                 JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        scrollpane.setBorder(null);
+        scrollpane.setViewportBorder(null);
+        scrollpane.getViewport().setOpaque(true);
 
-      tabPane.addTab(title, null, scrollpane);
-
-
-    }
+        tabPane.addTab(title, null, scrollpane);
+      }
     catch (Exception ex)
       {
         throw new RuntimeException("Error creating addTab" + ex);
       }
-
   }
 
   public void setVisible(boolean state)
