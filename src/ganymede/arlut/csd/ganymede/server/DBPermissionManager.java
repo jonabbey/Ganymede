fee6e6717c27496cf40ac5c7eb08d32faa06f8f9
Jonathan Abbey <jonabbey@arlut.utexas.edu>
2014-02-10 00:11:38 -0600

    Revised comments describing delegatableUnownedObjectPerms and delegatableOwnedObjectPerms in DBPermissionManager

--- src/ganymede/arlut/csd/ganymede/server/DBPermissionManager.java
+++ src/ganymede/arlut/csd/ganymede/server/DBPermissionManager.java
@@ -263,10 +263,9 @@ public final class DBPermissionManager {
    * String} is called or if the relevant Role Objects are changed in
    * the database .</p>
    *
-   * <p>Used by code in {@link arlut.csd.ganymede.server.ownerCustom}
-   * and {@link arlut.csd.ganymede.server.PermissionMatrixDBField} to
-   * control what privileges personae are able to grant to new
-   * personae.</p>
+   * <p>Used by code in {@link
+   * arlut.csd.ganymede.server.PermissionMatrixDBField} to control
+   * what privileges personae are able to grant to new personae.</p>
    */
 
   private PermMatrix delegatableOwnedObjectPerms;
@@ -282,10 +281,9 @@ public final class DBPermissionManager {
    * String} is called or if the relevant Role Objects are changed in
    * the database .</p>
    *
-   * <p>Used by code in {@link arlut.csd.ganymede.server.ownerCustom}
-   * and {@link arlut.csd.ganymede.server.PermissionMatrixDBField} to
-   * control what privileges personae are able to grant to new
-   * personae.</p>
+   * <p>Used by code in {@link
+   * arlut.csd.ganymede.server.PermissionMatrixDBField} to control
+   * what privileges personae are able to grant to new personae.</p>
    */
 
   private PermMatrix delegatableUnownedObjectPerms;
